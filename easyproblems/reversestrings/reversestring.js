b98f07c354a38719d46d106abed67e536dcb315b
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2022-06-29 22:28:53 -0400

    Create reversestring.js

---
+++ easyproblems/reversestrings/reversestring.js
@@ -0,0 +1,18 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function(array) {
+
+        let index=0
+    let backIndex=array.length-1
+
+    while(index<=backIndex){
+
+                [array[index],array[backIndex]] = [array[backIndex],array[index]]
+
+        index++
+        backIndex--
+    }
+    return array
+}
