33b3de5fe38c8cc2258d028a95708cd887d061e0
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2022-07-08 18:49:31 -0400

    Time: 967 ms (15.10%), Space: 48.9 MB (14.13%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.js
+++ 724-find-pivot-index/724-find-pivot-index.js
@@ -10,10 +10,7 @@
     const sumFn = array=> array.reduce((sum,curr)=>sum+curr,0)
 
     for (let index=0; index<array.length; index++){
-        let array1=array.slice(index + 1)
-        let array2= array.slice(0, index)
-
-        if (sumFn(array1) === sumFn(array2)){
+        if (sumFn(array.slice(index + 1)) === sumFn(array.slice(0, index))){
             return index
         }
     }
