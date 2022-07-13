1a922faf8687e4dc7d16f09991d65b8fc80fec58
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2022-07-12 22:01:11 -0400

    Time: 78 ms (77.67%), Space: 46.4 MB (14.41%) - LeetHub

---
+++ 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.js
@@ -0,0 +1,48 @@
+/**
+ * @param {number} target
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+var minSubArrayLen = function (targetNum, array) {
+    let left = 0
+    let right = 0
+
+    let sum = 0
+    let minArrayLength = Infinity
+    array
+
+    while (right < array.length) {
+
+        left
+        right
+
+        array[right]
+
+        sum
+        sum += array[right]
+
+        while (sum >= targetNum) {
+            let length = (right - left) + 1
+        
+            minArrayLength
+
+            if (length < minArrayLength) {
+                minArrayLength = length
+            }
+            
+            array[right]
+            array[left]
+            sum
+
+            sum -= array[left]
+            left++
+        } // end of nested while
+
+        right++
+    }
+    left
+    right
+    
+    return minArrayLength === Infinity ? 0 : minArrayLength
+}
