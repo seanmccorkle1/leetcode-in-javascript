50ed4814bff2a38433365dbbbd0ae194c85b3d11
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2022-07-19 20:09:14 -0400

    Time: 64 ms (91.16%), Space: 41.8 MB (72.09%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.js
+++ 28-implement-strstr/28-implement-strstr.js
@@ -1,4 +1,31 @@
-var strStr = function (bigString, substring) {
+const strStr = function (bigString, substring) {
 
- return bigString.indexOf(substring)   
+    if (substring.length > bigString.length) {
+        return -1
+    }
+    if (substring === "") {
+        return 0 // "" starts at index 0 
+    }
+
+    for (let savedIndex = 0; savedIndex < bigString.length; savedIndex++) {
+
+        // reset
+        let index = savedIndex
+        let index2=0
+
+        savedIndex
+        index
+        index2
+
+        // they have to be the same letter or the while loop wont work
+        while (bigString[index] == substring[index2] && index2 < substring.length){
+                index++
+                index2++
+
+                if (index2 === substring.length){
+                    return savedIndex
+                }
+        }
+    }
+    return -1
 }
