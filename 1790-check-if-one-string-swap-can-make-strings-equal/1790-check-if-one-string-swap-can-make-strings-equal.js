7d451c8d8da64bc5a7e0d048570a5214d5b019ef
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2022-11-01 18:26:56 -0400

    Time: 86 ms (75.12%), Space: 42 MB (68.23%) - LeetHub

---
+++ 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.js
@@ -0,0 +1,48 @@
+var areAlmostEqual= function(string1, string2){
+
+    let diffCount = 0
+    let twoLettersArray = []
+    
+    if (string1 === string2){
+        return true
+    }
+    
+    for (let index = 0; index < string1.length; index++) {
+ 
+        let currString1Letter = string1[index]
+        let currString2Letter = string2[index]
+
+        if (currString1Letter === currString2Letter) {
+          continue // no difference, dont do diffCount++
+        }
+ 
+        diffCount++
+ 
+        if (diffCount >= 3) {
+            return false
+        }
+      
+        if (twoLettersArray.length === 0) {
+            twoLettersArray = [currString1Letter, currString2Letter]
+            continue
+    }
+ 
+    let prevString1Letter = twoLettersArray[0]
+    let prevString2Letter = twoLettersArray[1]
+ 
+ 
+    if (prevString1Letter === currString2Letter && prevString2Letter === currString1Letter) {
+        continue   // dont return false
+    }
+    return false
+}
+  
+    // diffCount 0 means theyre the same
+    // need 2 letters to swap
+  
+   if (diffCount === 2){
+        return true
+    }
+    else return false
+}
+ 
