291b6456d989d6f18cf1792fccece3b793d200e6
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2022-11-01 19:31:58 -0400

    Time: 110 ms (88.46%), Space: 47.9 MB (54.41%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -0,0 +1,49 @@
+
+var lengthOfLongestSubstring = function (string) {
+
+    const obj = {}
+
+    let leftIndex = 0
+    let rightIndex = 0
+
+    // base case is 0 for "" which is length 0
+    let maxUniqueSubstringLength = 0
+    
+    while (rightIndex < string.length) {
+        let letter = string[rightIndex]
+
+
+        // if the object already has it, move left part of the window up
+
+        if (obj[letter] !== undefined) {
+
+           leftIndex=
+           Math.max(obj[letter] + 1,
+                    leftIndex)
+                }
+
+        // letter is the key, rightIndex is the value
+        // key = value
+
+        obj[letter] = rightIndex
+
+        leftIndex
+        rightIndex
+
+        obj
+
+        // when rightIndex and leftIndex are both 0, its still "n", which is length 1
+
+        maxUniqueSubstringLength = 
+        Math.max(
+            (rightIndex - leftIndex) + 1, 
+            maxUniqueSubstringLength)
+                        
+        rightIndex++
+    }
+    return maxUniqueSubstringLength
+}
+
+console.log(lengthOfLongestSubstring("abcabcbb"), 3)
+// console.log(lengthOfLongestSubstring("bbbbbb"), 1)
+// console.log(lengthOfLongestSubstring("leetcode"), 5, "etcod")
