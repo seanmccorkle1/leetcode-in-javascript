4c1f629d038bb562dc2a5340659abeb5b0867586
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2022-11-02 22:06:18 -0400

    Time: 290 ms (96.06%), Space: 68.2 MB (94.20%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.js
@@ -0,0 +1,67 @@
+
+
+
+var countPrimes = function (inputNum) {
+
+    // this is the (inputNumber -= 1) part
+    let array = new Uint8Array(inputNum)
+    let primeCount = 0
+
+
+    // dont cover 10 itself, use <
+
+    for (let increment = 2; increment < inputNum; increment++) {
+
+        increment
+
+        // if its been seen before, its a multiple (ie. not a prime)
+        // so dont increase primeCount
+
+        if (array[increment] == 1) {
+            continue // back to top
+        }
+
+        // [2, 3, 5, 7, 11]
+        ++primeCount
+
+        // mark every multiple of 2 up until 10, not including 10 itself
+        // so [2,4,6,8]
+        // then mark every multiple of 3
+
+        // use mult += 2 to only mark the multiples of 2
+        // mult++ would mark [2,3,4,5] without increasing primeCount
+
+        // for multiples of 3, go straight to 9
+        // 6 was already seen because its a multiple of 2
+
+        // (let index = 0) only happens once, it doesnt refresh
+
+        let power = Math.pow(increment, 2)
+        
+        while (power < inputNum){
+            array[power] = 1
+            power += increment
+
+            array
+        }
+
+        // for (let mult = Math.pow(increment, 2); mult < inputNum; mult += increment) {
+
+        //     increment
+        //     mult
+
+        //     array[mult] = 1
+
+        //     array
+        // }
+    }
+    array
+
+    return primeCount
+}
+
+// console.log(countPrimes(10), 4, " [2, 3, 5, 7], 4 primes")
+console.log(countPrimes(12), 5, "[2, 3, 5, 7, 11] - 5 numbers")
+
+// console.log(countPrimes(32), 11, "11 primes")
+// console.log(countPrimes(5), 2, "[2,3]")
