9f5b6e0a6829d3e228ad05705cb7f05312459b99
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2022-11-27 14:49:32 -0500

    Time: 118 ms (15.88%), Space: 41.9 MB (71.71%) - LeetHub

--- 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.js
+++ 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.js
@@ -1,80 +1,105 @@
+/*
+ * @lc app=leetcode id=1790 lang=javascript
+ *
+ * [1790] Check if One String Swap Can Make Strings Equal
+ */
 
 
-var areAlmostEqual = function(string1, string2) {
+// @lc code=start
 
-    let differenceCount = 0
+var areAlmostEqual = function (string1, string2) {
+
+    // not number of different letters, numOfDifferentSpots
+    // "k" and "b" are 2 different letters
+    // but they are different on the same 1 spot
+
+    let differentSpotCount = 0
+
     let twoLetterArray = []
+
+
+    if (string1.length !== string2.length) {
+        return false
+    }
 
     if (string1 === string2) {
         return true
     }
 
-    // no way to make "asd" and "zxcv" equal to each other with a swap
-    // you have to add a letter
-
-    if (string1.length != string2.length) {
-        return false
-    }
+    // string1 and string2 must be the same length
+    // "index < string1.length" is the same as "index < string2.length"
 
     for (let index = 0; index < string1.length; index += 1) {
 
-        index
+        string1
+        string2
 
         const currString1Letter = string1[index]
         const currString2Letter = string2[index]
 
+        // this `if` statement executes everything
+        // if the letters are equal, just continue
+
         if (currString1Letter === currString2Letter) {
-            continue // no need to swap the letters if theyre already equal
-        } 
-
-        // if it skips the `if` statement,
-        // that means the letters are different
-
-        differenceCount += 1
-
+            continue // skip all the code after, go next letters
+        }
         
-        // you can only swap exactly 2 letters
-        if (differenceCount >= 3) {
-            return false 
-        } 
-        twoLetterArray
+        
+        differentSpotCount += 1
 
+        // check this after the count is modified
+        if (differentSpotCount >= 3) {
+            return false
+        }
 
         if (twoLetterArray.length === 0) {
-            twoLetterArray = [currString1Letter, currString2Letter] 
-            continue // go to the next letter
+            twoLetterArray = [currString1Letter, currString2Letter]
+            continue // go next letters
         }
-        
-        
-        // when twoLetterArray gets ["a", "b"], the loop goes back to the top
-        // now ["a", "b"] are the previous letters
+
+        // the left letter is the one that came first
+        // and was stored in the array ["b", "k"]
+
         twoLetterArray
 
-        const prevString1Letter = twoLetterArray[0]
-        const prevString2Letter = twoLetterArray[1]
+        let leftString1Letter = twoLetterArray[0]
+        let leftString2Letter = twoLetterArray[1]
 
-        prevString1Letter
+        leftString1Letter
+        currString1Letter
+
+        leftString2Letter
         currString2Letter
 
-        prevString2Letter
-        currString1Letter
-        
-        // dont return false, but dont return true yet
-        // the differenceCount has to be 2
+        // since the strings are the same length,
+        // `b` in `kanb` is implicitly getting swapped to the first position like in `bank`
 
-        if (prevString1Letter === currString2Letter && prevString2Letter === currString1Letter) {
-            continue 
+        checkIfTheStringSwapIsValid:
+        if (leftString1Letter === currString2Letter && leftString2Letter === currString1Letter) {
+            continue // keep going in case differenceCount goes above 2
         }
 
-        else return false
-        // return false
-    }
+        // once a string swap attempt fails, return false
+        // cause that means the strings cant be entirely equal when swapped
 
-    differenceCount
+        else {
+            return false
+        }
 
-    if (differenceCount === 2) {
+    } // end for loop
+
+    // the two strings have to be unequal at exactly 2 spots
+    // for the swap to work
+
+    if (differentSpotCount === 2) {
         return true
-    }
-    else return false    
-    
+    } 
+    else return false
 }
+
+console.log(areAlmostEqual("bank", "kanb"), true, "swap `k` and `b` in string2 to make both equal to `bank` ")
+
+// console.log(areAlmostEqual("equal", "equal"), true, "return true if they're the same string")
+// console.log(areAlmostEqual("asd", "zxc"), false, "not the same length, a string swap cant make both equal")
+
+// @lc code=end
