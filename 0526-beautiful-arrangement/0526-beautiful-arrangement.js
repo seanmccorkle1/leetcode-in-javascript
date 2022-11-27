2d00230ec71f93be606b41d4627ed2d81aac5105
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2022-11-26 19:23:33 -0500

    Time: 257 ms (78.69%), Space: 42.5 MB (63.93%) - LeetHub

---
+++ 0526-beautiful-arrangement/0526-beautiful-arrangement.js
@@ -0,0 +1,53 @@
+var countArrangement = function(num) {
+
+    if (num <= 2) {
+        return num
+    }
+    
+    // array[3] is valid because it breaks at GREATER than 3, not equal to 3
+    // so there needs to be a fourth element
+    const visitedArray = Array(num + 1).fill(false)
+    let permutationCount = 0
+
+    var dfsFn = (element) => {
+
+        // if it made it here, it means all numbers in [1,2,3] are valid
+        if (element > num) {
+            permutationCount += 1
+            return // base case
+        }
+
+        // dfs(1) gets pushed to the bottom of the call stack at index = 1 in the for loop
+        // so it still has to loop 2 more times to get all the permutations
+
+
+        for (let index = 1; index <= num; index += 1) {
+
+            visitedArray
+            index
+
+            // avoid double counting
+
+            if ((visitedArray[index] === false) && (index % element === 0 || element % index === 0)) {
+
+                visitedArray[index] = true
+                dfsFn(element + 1)
+                visitedArray[index] = false
+
+                visitedArray
+            }
+            
+            visitedArray
+        } // end for loop
+
+        return
+    }
+
+    visitedArray
+
+    dfsFn(1)
+    return permutationCount
+}
+
+console.log(countArrangement(3), 3, "[1,2,3] [3,2,1] [2,1,3]")
+// console.log(countArrangement(2), 2, "[1, 2] and [2, 1]")
