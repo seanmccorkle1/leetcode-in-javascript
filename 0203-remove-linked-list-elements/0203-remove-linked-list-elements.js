282b54db7ed4e14a0da0c1f95507e854e00e1d5f
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2022-11-28 18:56:17 -0500

    Time: 148 ms (15.80%), Space: 46 MB (94.14%) - LeetHub

---
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.js
@@ -0,0 +1,23 @@
+var removeElements = function(head, val) {
+    if (!head) return head;
+	
+	// if head is the value were deleting and if there is any repetition lets handle that case first otherwise we'll just break out of the loop
+    while(head) {
+        if(head.val === val) {
+            head = head.next;
+        } else {
+            break;
+        }
+    }
+    
+	// skip any nodes whos values match the parameters and set it to the node after
+	// if the node is found, set curr.next to the node after it then try again
+	// otherwise iterate forward
+    let curr = head;
+    while(curr && curr.next) {
+        if (curr.next.val === val) curr.next = curr.next.next;
+        else curr = curr.next;
+    }
+    
+    return head;
+};
