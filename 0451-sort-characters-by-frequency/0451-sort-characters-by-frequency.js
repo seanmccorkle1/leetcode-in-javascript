12daa7902f206b25ef8f542fdfc297c9c3822444
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2022-12-03 11:11:55 -0500

    Time: 159 ms (34.95%), Space: 44.7 MB (74.57%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.js
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.js
@@ -1,42 +1,12 @@
-
-
-
-function frequencySort(string){
-
+var frequencySort = function(string){
+    
     const obj = {}
     let currString = ""
 
     for (let letter of string){
         obj[letter] ?obj[letter]++ : obj[letter] = 1
     }
-
-    // highest frequency first
-    //  2 - 1
-
-    obj
-    Object.keys(obj)
-
-    obj["e"]
-
-    const a=Object.keys(obj)
-    a
-
- // "e" is represented as 2, "r" is represented as 1
- // object keys are unique, so "e" will always be 2
- // .sort(a,b) with numbers
-
- const letterArray =
-    Object.keys
-    (obj).sort((a, b) => obj[b] - obj[a])
-
-
-    for (let letter of letterArray){
-        currString
-
-        // "ee" += "t".repeat(1) is "eet"
-        currString += letter.repeat(obj[letter])
-    }
+    
+    Object.keys(obj).sort((a, b) => obj[b] - obj[a]).forEach(letter => currString+=letter.repeat(obj[letter]))
     return currString
 }
-
-console.log(frequencySort("tree"), "eert")
