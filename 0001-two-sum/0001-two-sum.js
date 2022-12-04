e0f10e0d23ae0df9c5fd0f07619ade75a8c5db4e
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2022-12-03 19:10:19 -0500

    Time: 186 ms (28.15%), Space: 42.5 MB (64.29%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.js
@@ -0,0 +1,17 @@
+var twoSum = function(array, targetNumber) {
+
+    // const returnArray = [...array]
+    let returnArray = []
+
+    for (let index=0; index < array.length; index += 1){
+        let number=array[index]
+
+        let difference = targetNumber - number
+        let indexOfDifference = array.indexOf(difference)
+
+        if (index !== indexOfDifference && indexOfDifference > -1){
+            return [index, indexOfDifference]
+        }
+    }
+
+}
