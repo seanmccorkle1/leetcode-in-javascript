ed19ec8107a1d7376e77a5b9fcda9f7f60d20b1b
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2022-12-03 19:44:05 -0500

    Time: 65 ms (92.17%), Space: 15.1 MB (69.95%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, array: List[int], targetNum: int) -> List[int]:
+        obj = {}
+        for currentIndex, num in enumerate(array): 
+            
+            remainder = targetNum - num
+            if remainder in obj: 
+                pastIndex = obj[remainder]
+                return [pastIndex, currentIndex]  # [indexOfRemainder, currentIndex]
+            else: 
+                obj[num] = currentIndex
