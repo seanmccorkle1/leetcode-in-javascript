4cbfbf71d5f5a269aa14138aed23aeed25ec5ec6
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2022-12-04 18:50:16 -0500

    Time: 100 ms (98.18%), Space: 52.4 MB (87.27%) - LeetHub

--- 2256-minimum-average-difference/2256-minimum-average-difference.js
+++ 2256-minimum-average-difference/2256-minimum-average-difference.js
@@ -1,18 +1,59 @@
-var minimumAverageDifference = function(nums) {
-    let nL=nums.length;
-    let leftSum = 0;
-    let rightSum = nums.reduce((p,n)=>{return p+n},0);
+var minimumAverageDifference = function (array) {
+    
+    let leftSum = 0
+    let rightSum = array.reduce((s,c)=> s + c, 0)
 
-    let minimum = Infinity;
-    let ans = Infinity;
-    for (let i=0; i<nL; i++){
-        leftSum += nums[i];
-        rightSum -= nums[i];
-        let res = Math.abs(Math.floor(((leftSum)/(i+1))) - Math.floor(((rightSum)/Math.max(1,(nL-1-i)))));
-        if(res < minimum){
-            minimum = res;
-            ans = i;
+    let minFound = Infinity
+    let minDiff = Infinity
+    let indexOfMinDifference = 0
+
+        // parantheses on (index+1) cause  theres / and +
+    //Math floor so you dont get array decimal with the division
+
+    for (let index = 0; index < array.length; index++) {       
+
+        leftSum += array[index]
+        rightSum -= array[index]
+        
+        // (array.length - index) means  
+        // i=0 (6 - 0 = 6) 
+        // i=1 (6 - 1 = 5) 
+        // i = 2(6 - 2 = 4)
+        // start at array high denominator and end at  x/1 
+
+        index
+        
+        let rightSumDenominator = array.length - index - 1 
+
+        if (rightSumDenominator === 0){
+            rightSumDenominator = 1 // cant divide by 0
         }
+        
+        // start at (leftSum / 1) with  leftSum / (index + 1) 
+        // with index=0, that way you still get array[0]
+        let leftSumAvg= Math.floor(leftSum / (index + 1))
+    
+        let rightSumAvg = Math.floor(rightSum / rightSumDenominator) // start at (rightSum / 5)
+
+        let difference = Math.abs(leftSumAvg - rightSumAvg)
+        
+        // `difference` is lower than minFound
+        // that means its the new min
+        
+        if (difference< minFound) {  // difference < Infinity 
+            minFound = difference // difference gets refreshed on every loop, minFound doesnt unless you do this
+            indexOfMinDifference = index // store the index
+            
+            if (minFound===0){
+                return index
+            }
+        }
+
+        // kinda like best time to buy and sell stock
+        // reassign variables and dont return until the end
+    
     }
-    return ans;
-};
+    return indexOfMinDifference
+}
+
+console.log(minimumAverageDifference([2,5,3,9,5,3]), 3)
