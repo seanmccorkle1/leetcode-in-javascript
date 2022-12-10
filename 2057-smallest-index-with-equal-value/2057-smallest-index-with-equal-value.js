810adba6cf1c835608f8b3e3ae0f56bc1797f357
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2022-12-10 15:54:47 -0500

    Time: 118 ms (28.99%), Space: 44.7 MB (5.80%) - LeetHub

--- 2057-smallest-index-with-equal-value/2057-smallest-index-with-equal-value.js
+++ 2057-smallest-index-with-equal-value/2057-smallest-index-with-equal-value.js
@@ -1,15 +1,32 @@
 /**
- * @param {number[]} nums
- * @return {number}
+ * @param [0,1,2]
+ * @return 0
  */
-var smallestEqual = function(array) {
-    
 
-    for (let index=0;index<array.length; index += 1){
-        if (index%10===array[index]){
+
+var smallestEqual = function(array) { 
+
+    for (let index=0; index<array.length; index += 1){
+
+        const num = array[index]
+
+        // if `num` is outside the range of [0-9], just continue
+        // because (index % 10) prevents any number being >=10
+        // modulo operator  is too heavy
+        
+        //  (index % 10) === 11 is impossible
+
+        if (num >= 10){
+            continue
+        }
+            
+        let indexMod10 = index % 10
+
+        if (indexMod10 === num){
             return index
         }
     }
- 
     return -1
 }
+
+console.log(smallestEqual([0, 1, 2]))
