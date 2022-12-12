dd64f995af111599296016174a19997c0bed824f
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2022-12-12 11:22:27 -0500

    Time: 116 ms (44.37%), Space: 44.6 MB (16.56%) - LeetHub

---
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} prices
+ * @return {number[]}
+ */
+
+var finalPrices = function(array) {
+    
+    let returnArray=[]
+    
+    // need to use array.find() to find the closest
+    // shift() is needed with find()
+    
+    
+    // while with .shift() to prevent cutting early
+    
+    while (array.length >= 1){
+        let originalNum = array[0]
+        array.shift()
+        
+        const lowerNumExists = array.find(num => num <= originalNum)
+        let difference = originalNum - lowerNumExists
+        
+        if (lowerNumExists){
+            returnArray.push(difference)
+        }
+        
+        // still have to push something
+        else if (!lowerNumExists){
+            returnArray.push(originalNum)
+        } 
+    }
+    
+    return returnArray
+}
