97a3903ed830742635d5dcadc92fe0db247fcefb
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2022-12-13 15:31:31 -0500

    Time: 80 ms (86.38%), Space: 46.5 MB (50.18%) - LeetHub

--- 0937-reorder-data-in-log-files/0937-reorder-data-in-log-files.js
+++ 0937-reorder-data-in-log-files/0937-reorder-data-in-log-files.js
@@ -1,113 +1,61 @@
-/**
- Many question are incorrectly labeled, but I think less than 10 min for easy, 20 min for medium, and 40 min for hard 
- is stringA good strategy. How many - depend on your schedule, but I think the number isn't really important. Learning from the question you've solved is indeed more essential.
-* 
-* @params 
-* @return 
-* @explain 
-*/
 
+var reorderLogFiles = function(array){
 
-    const getBody = string => {
-        let bodyIndex = string.indexOf(" ")
-        return string.slice(bodyIndex + 1)
-    }
-
-    const bodyContainsDigitFn = string => {
-        return string.match(/[\d]/g)
-    }
-
-
-    const weirdSort = (stringA, stringB) => {
-
-        // it uses getBody(string), so getBody function has to come first
-        
-        let bodyA = getBody(stringA)
-        let bodyB = getBody(stringB)
-
-        // if the BODIES are the exact same,
-        // compare in ascending order using the original string
-
-        if (bodyA == bodyB){
-            let wholeCompareNum = stringA.localeCompare(stringB)
-            return wholeCompareNum
-        }
-
-        else if (bodyA != bodyB) {
-            let bodyCompareNum = bodyA.localeCompare(bodyB)
-            return bodyCompareNum
-        }
-    }
-
-    var reorderLogFiles = function (array) {
-        let digitArray = []
-        let stringArray = []
-
-
-        for (const wholeString of array) {
-
-            let body = getBody(wholeString)
-            const bodyContainsDigit = bodyContainsDigitFn(body)
-
-            wholeString
-            body
-
-            // they all have digits, but the letter-logs have no digits in their body
-            // if theres no digit, its stringA letter-log
-
-            // ex. dig1 8 1 5 1
-
-            if (bodyContainsDigit) {
-                digitArray.push(wholeString)
-            }
-
-            // ex. let1 art can
-            else if (!bodyContainsDigit) {
-                stringArray.push(wholeString)
-            }
-
-        }
-
-        stringArray
-        stringArray.sort(weirdSort)
-
-        stringArray
-
-        return [...stringArray, ...digitArray]
-
-    }
-
-
-
-    // stringArray.sort((stringA, stringB) => {
-
-    //     let bodyA = getBody(stringA)
-    //     let bodyB = getBody(stringB)
-
-    //     // if the BODIES are the exact same,
-    //     // compare in ascending order using the original string
-
-    //     if (bodyA == bodyB){
-    //         let wholeCompareNum = stringA.localeCompare(stringB)
-    //         return wholeCompareNum
-    //     }
-
-    //     else {
-    //         let bodyCompareNum = bodyA.localeCompare(bodyB)
-    //         return bodyCompareNum
-    //     }
-
-        // if (bodyCompareNum === -1){
-        //     return -1
-        // }
-
-        // else if (getBody(stringA).localeCompare(getBody(stringB)) > 0) {
-        //     return 1
-        // }
-
-        // else if (bodyA == bodyB) {
-        //     return stringA.localeCompare(stringB)
-        // }
+    let digitLogs=[]
+    let letterLogs=[]
     
+    
+    // this will be called multiple times
+    // so make it a function
+    
+    const returnBody = string => {
+
+        let indexOfBody = string.indexOf(" ") + 1    
+        let bodySlice = string.slice(indexOfBody)
+        return bodySlice
+    }
+    
+    const sortLetterLogs = (stringA, stringB) => {
+        
+        let bodyA = returnBody(stringA)
+        let bodyB = returnBody(stringB)
+
+        // compare from the start(identifier)
+
+        if (bodyA === bodyB){
+            let identifierCompareNum =  stringA.localeCompare(stringB)
+            return identifierCompareNum
+        }
+        
+        // ascending
+        else {
+            return bodyA.localeCompare(bodyB)
+        }
+        
+        
+    }
+
+        
+    for (const originalString of array) {
+        
+        let body= returnBody(originalString)
+        const bodyContainsDigit = body.match(/[\d]/)
+        
+        if (bodyContainsDigit) {
+            digitLogs.push(originalString)
+        }
+    
+        else if (!bodyContainsDigit){
+            letterLogs.push(originalString)
+        }
+    
+    }
+    
+    letterLogs.sort(sortLetterLogs)
+    
+    // dont mess with digit logs
+    return [...letterLogs, ...digitLogs]
+}    
+
 console.log(reorderLogFiles(["dig1 8 1 5 1","let1 art can","dig2 3 6","let2 own kit dig","a3 art can"]), 
 [ 'a3 art can', 'let1 art can', 'let2 own kit dig', 'dig1 8 1 5 1', 'dig2 3 6' ])
