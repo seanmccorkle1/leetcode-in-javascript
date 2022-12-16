705e22fca401c2a7a5660863029a18effbcb54c1
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2022-12-16 18:07:19 -0500

    Time: 104 ms (99.25%), Space: 49.9 MB (77.14%) - LeetHub

--- 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.js
+++ 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.js
@@ -1,6 +1,6 @@
 /**
  * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
+ * function TreeNode(treeVal, left, right) {
  *     this.val = (val===undefined ? 0 : val)
  *     this.left = (left===undefined ? null : left)
  *     this.right = (right===undefined ? null : right)
