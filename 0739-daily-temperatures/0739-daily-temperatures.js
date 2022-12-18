01e1e9735744c15a7989d79e3ec8e24986b3745c
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2022-12-18 15:01:05 -0500

    Time: 799 ms (27.87%), Space: 65.7 MB (69.07%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.js
+++ 0739-daily-temperatures/0739-daily-temperatures.js
@@ -1,87 +1,111 @@
-/**
- Many question are incorrectly labeled, but I think less than 10 min for easy, 20 min for medium, and 40 min for hard 
- is a good strategy. How many - depend on your schedule, but I think the number isnt really important. Learning from the question youve solved is indeed more essential.
-* 
-*/
+var dailyTemperatures = function(temperatureArray) {
+
+    // .fill(0) for the days where no temperature to the right is higher (the last two days)
+    let daysArray = new Array(temperatureArray.length).fill(0)
+    
+    
+    // stack always starts as []
+    // const stack is fine because you only use pop() on it
+
+    const stack = []
 
 
-var dailyTemperatures = function (array) {
 
-    const returnArray = new Array(array.length).fill(0)
-    const indexStack = []
+    for (let rightIndex = 0; rightIndex < temperatureArray.length; rightIndex += 1) {
 
+        rightIndex
 
-    for (let right = 0; right < array.length; right += 1) {
+        stack
 
-        right
-        indexStack
-
-        let leftIndex = indexStack.at(-1)
-
-        right
-        leftIndex
-        
+        // temperatureArray = 
         // [ 73, 74, 75, 71, 69, 72, 76, 73 ]
         
-        let leftNum = array.at(leftIndex)
-        let currNum = array[right]
+        let mostRecentIndex = stack.at(-1)
+        
+        let leftTemp = temperatureArray[mostRecentIndex]
+        let rightTemp = temperatureArray[rightIndex]
+
+        // higher temperature found if (leftTemp < rightTemp)
+        // only if its less than, not equal temps
+
+        // undefined < 73 - FALSE - store index 0 (73) into the stack
+        // and ignore the while loop
+
+        // 73 < 74 - TRUE
+        // 74 < 75 - TRUE
+        // 75 < 71 - FALSE
+        // 71 < 69 - FALSE
+        // 69 < 72 - TRUE
+        // 72 < 76 - TRUE
+        // 76 < 73 - FALSE
+        
+        // undefined returns false anyway for any kind of comparison
+        // you dont need  (while (stack.length >= 1))
+
+        // while the stack has unfinished temperatures  (stack.length >= 1)
+
+        // temperatureArray = [ 73, 74, 75, 71, 69, 72, 76, 73 ]
+        
+        leftTemp
+        rightTemp    
 
 
-        leftNum
-        currNum
+        while (leftTemp < rightTemp && stack.length >= 1) {
 
-        indexStack
+            leftTemp
+            rightTemp
+            
+            // [2] is 75, the highest temperature that goes into daysArray last
 
-        // [ 73, 74, 75, 71, 69, 72, 76, 73 ]
+            stack
 
-        while (indexStack.length >= 1 && leftNum < currNum) {
+            let leftIndex = stack.pop()
 
-            leftNum = array[leftIndex]
-            currNum = array[right]
+            stack
 
-            leftNum
-            currNum
+            leftIndex
+            rightIndex        
 
+            // (1 - 0) is just one day, dont need an extra -1
+            const daysUntilHigherTemp = rightIndex - leftIndex 
 
-            if (leftNum < currNum) {
+            leftIndex
+            daysArray[leftIndex] = daysUntilHigherTemp
 
-                indexStack
+            daysArray
 
-                let left = indexStack.pop()
+        
+            // change the variable (leftTemp) for the while loop
+            // `stack` was modified  with pop()
 
-                right
-                left
+            leftTemp = temperatureArray[stack.at(-1)]
 
-                const daysToGetHigherTemp = right - left
-                daysToGetHigherTemp 
+            // keep rightTemp at 76 
+            rightTemp 
 
-                // returnArray already has 8 elements, dont push anymore
-
-                returnArray[left] = daysToGetHigherTemp
-
-                returnArray
-
-                indexStack
-                leftIndex = indexStack.at(-1)
-
-                // [ 1, 0, 0, 0, 0, 0, 0, 0 ] 
-                // [ 1, 1, 0, 0, 0, 0, 0, 0 ] 
-                // [ 1, 1, 0, 0, 1, 0, 0, 0 ] 
-
-                // [ 1, 1, 0, 2, 1, 0, 0, 0 ] 
-                // [ 1, 1, 0, 2, 1, 1, 0, 0 ] 
-                // [ 1, 1, 4, 2, 1, 1, 0, 0 ]
-            }
         }
 
-        indexStack
-        indexStack.push(right)
-    
-        indexStack
+        stack.push(rightIndex)
+        stack
     }
 
-    
-    return returnArray
+    //daysArray=
+    // [ 1, 0, 0, 0, 0, 0, 0, 0 ] 
+    // [ 1, 1, 0, 0, 0, 0, 0, 0 ] 
+    // [ 1, 1, 0, 0, 1, 0, 0, 0 ]
+
+    // now go DOWN the stack during the while loop to 71's original place
+    // [ 1, 1, 0, 2, 1, 0, 0, 0 ] 
+
+    // [ 1, 1, 0, 2, 1, 1, 0, 0 ]     
+    // [ 1, 1, 4, 2, 1, 1, 0, 0 ]
+
+
+    // these last two elements stay at 0 in `daysArray`
+    stack
+
+    return daysArray
 }
 
-console.log(dailyTemperatures([73, 74, 75, 71, 69, 72, 76, 73]), [1, 1, 4, 2, 1, 1, 0, 0], "[1 day, 1 day, 4 days]")
+console.log(dailyTemperatures([73, 74, 75, 71, 69, 72, 76, 73]), [1, 1, 4, 2, 1, 1, 0, 0], 
+"[1 day, 1 day, 4 days]")
