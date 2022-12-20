997d05709540bf2416edf7621319de5e2be201b4
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2022-12-20 15:04:13 -0500

    Time: 93 ms (53.50%), Space: 42.2 MB (40.74%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.js
@@ -0,0 +1,19 @@
+var merge = function(array1, length1, array2, length2) {
+
+    array1
+    array1.length = length1
+    array1
+
+    // array2
+    // array2.length = length2
+    // array2
+
+
+    // ...array when an array is inside a function
+
+    array1.push(...array2)
+    array1
+            
+    array1.sort((a,b)=>a - b)
+
+}
