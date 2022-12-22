24f63168261bc386bce93ecd0b41fd6ea0850f93
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2022-12-22 12:31:15 -0500

    Time: 75 ms (93.40%), Space: 43.6 MB (89.39%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.js
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.js
@@ -1,5 +1,26 @@
-var myAtoi = function(stringnum) {
-    return Math.max(
-        Math.min(parseInt(stringnum) || 0, 2147483647),
-        -2147483648)
-}
+
+
+var myAtoi = function (str) {
+  str = str.trim();
+  if (!str) return 0;
+  var sign = 1;
+  var i = 0;
+  var answer = 0;
+  if (str[i] == '+') {
+    sign = 1;
+    i++;
+  } else if (str[i] == '-') {
+    sign = -1;
+    i++;
+  }
+
+  for (; i < str.length; i++) {
+    var temp = str.charCodeAt(i) - 48;
+    if (temp > 9 || temp < 0) break;
+    if (answer > 2147483647 / 10 || answer > (2147483647 - temp) / 10)
+      return sign == 1 ? 2147483647 : -2147483648;
+    else answer = answer * 10 + temp;
+  }
+  return answer * sign;
+};
+    
