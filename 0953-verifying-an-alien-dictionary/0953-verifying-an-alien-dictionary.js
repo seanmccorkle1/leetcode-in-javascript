599872faf2fdc52338f69f9dd3f557f5811fdcb7
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2022-12-26 15:42:31 -0500

    Time: 58 ms (97.19%), Space: 41.8 MB (94.39%) - LeetHub

--- 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.js
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.js
@@ -3,40 +3,46 @@ var isAlienSorted =function(stringArray, alphabet){
     const letterMap = new Map()
     
     letterMap
-
-
-    for (let index = 0; index < alphabet.length; index++){
-        
+    
+    
+    for (let index = 0; index < alphabet.length; index++) {
         let letter = alphabet[index]        
         letterMap.set(letter, index)
     }
     
-    
     stringArray 
     alphabet
-
+    
+    
     for (let index = 1; index < stringArray.length; index++){
-
+        
         let leftWord = stringArray[index - 1]
         let rightWord = stringArray[index]
 
         leftWord
         rightWord
-         
     
-        if (leftWord.includes(rightWord) && leftWord.length!= rightWord.length) {
+        // "apple".includes("app") is true
+        // "app".includes("apple") is false
+        
+        // so the leftWord has to be the bigger word to return false
+    
+
+        if (leftWord.includes(rightWord) && leftWord != rightWord) { //leftWord.length > rightWord.length) {
             return false
         }
-    
+        
         
         for (let index = 0; index < leftWord.length; index++){ 
             
             let letterA = leftWord[index]
             let letterB = rightWord[index]
             
+            // if theyre both "a", go to the 2nd letter
             if (letterA == letterB){
                 continue
             }
+            
             
             letterA
             letterB
@@ -50,8 +56,8 @@ var isAlienSorted =function(stringArray, alphabet){
             if (indexA < indexB){ 
                 break
             }
-
-            if (indexA > indexB) {
+            
+            else if (indexA > indexB) {
                 return false 
             }
         }
