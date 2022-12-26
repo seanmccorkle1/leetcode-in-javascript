6b27145835535e9adaa57a766a39c25010506ecc
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2022-12-26 11:00:57 -0500

    Time: 87 ms (59.32%), Space: 48.6 MB (6.21%) - LeetHub

---
+++ 2427-number-of-common-factors/2427-number-of-common-factors.js
@@ -0,0 +1,85 @@
+/**
+ Many question are incorrectly labeled, but I think less than 10 min for easy, 20 min for medium, and 40 min for hard 
+ is a good strategy. How many  -  depend on your schedule, but I think the number isnt really important. Learning from the question youve solved is indeed more essential.
+* 
+*/
+
+/**
+ * @param {number} num1
+ * @param {number} num2
+ * @return {number}
+ */
+
+
+var commonFactors = function(num1, num2) {
+
+    let factorCount = 1
+
+    // const lastFactor = (Math.min(num1, num2))
+
+    num1
+    num2
+
+    // const lastFactor = (Math.min(num1, num2) + 1)
+    const lastFactor = (Math.min(num1, num2))
+
+    lastFactor
+
+    let commonFactors = []
+    let nonCommonFactors = []
+
+    let quotient1 = 0
+    let quotient2 = 0
+
+
+    num1
+    num2
+
+    lastFactor
+
+    for (let index = 2; index <= lastFactor; index++) {
+
+        index
+
+
+
+
+        const indexIsCommonFactor = ((num1 % index)== 0 && (num2 % index)== 0)
+
+        if (indexIsCommonFactor) {
+
+            factorCount++
+
+            quotient1= num1 / index
+            quotient2= num2/index
+
+            index
+            // commonFactors.push(`${index} is a factor of both ${quotient1} and ${quotient2}`)
+
+            commonFactors.push(index)
+
+            commonFactors
+        }
+
+        else if (!indexIsCommonFactor) {
+            nonCommonFactors.push(index)
+
+            quotient1 = num1 / index
+            quotient2 = num2 / index
+
+            nonCommonFactors.push(`${quotient1} and ${quotient2} are both not integers`)
+
+            nonCommonFactors
+        }
+    
+    }
+
+    commonFactors
+
+    return factorCount
+}
+
+console.log(commonFactors(12,24), "[1, 2, 3, 4, 6, 12] are the 6 common factors of both numbs")
+
+// console.log(commonFactors(6, 1),   1,  "[1] is the only common factor, 6/1 and 1/1 are both integers")
+// console.log(commonFactors(3, 12),   2, "1 and 3")
