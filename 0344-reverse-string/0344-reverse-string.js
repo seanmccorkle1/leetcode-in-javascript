74b753e561ea33a5caa76b314e965ae11230ce3f
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2022-12-28 16:39:28 -0500

    Time: 94 ms (91.43%), Space: 49.3 MB (51.63%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.js
+++ 0344-reverse-string/0344-reverse-string.js
@@ -1,28 +1,42 @@
-/**
- * @param {character[]} s
- * @return {void} Do not return anything, modify s in-place instead.
- */
-var reverseString = function(array) {
 
-        let index=0
-        let backIndex= array.length - 1
-        
-        // no point in reversing on the same index,
-        // it wont do anything
-        
-        while(index<backIndex){
+var reverseString = function (array) {
 
-            index
-            backIndex;
+    let index = 0
+    let backIndex = array.length - 1
+
+    // no point in reversing on the same index,
+    // it wont do anything
+
+    while (index < backIndex) {
+
+        // index
+        // backIndex
+
+        //            index
+        //            backIndex
+
+        //            array
+
+        let frontLetter = array[index]
+        let backLetter = array[backIndex]
+
+        // [array[index], array[backIndex]] = [array[backIndex], array[index]]
+        // let temp = array[index]
+
+        // "frontPosition = backLetter"
+        array[index] = backLetter
+        array[backIndex] = frontLetter
+
         
-            array;
-            [array[index],array[backIndex]] = [array[backIndex],array[index]]
-            array;
-            
-            index++
-            backIndex--
+        // [array[index], array[backIndex]] = [frontLetter, backLetter]
+
+        index++
+        backIndex--
     }
-    
-    
+
+
     return array
 }
+
+reverseString(["h", "e", "l", "l", "o"])
+reverseString(["one", "two", "three", "four", "five"])
