141c6856a5d237da52f1daf8637bea81417e1c7a
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2022-12-30 18:32:18 -0500

    Time: 63 ms (85.55%), Space: 42 MB (70.23%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.js
+++ 0228-summary-ranges/0228-summary-ranges.js
@@ -12,105 +12,92 @@ var summaryRanges = function (array) {
     const rangeArray = []
     let staticIndex = 0
 
-
-
     // index <= array.length
     // start at index=1 for the difference 
-
-
-
+    
     // 7+1 != undefined  = true
     // the if statement will always execute on the last element (undefined)
-    
-    
-    for (let index = 1; index <= array.length; index++) {
 
+
+    for (let index = 1; index <= array.length; index++) {
 
         index
 
-
+        let leftmostNum = array[staticIndex]
         let leftNum = array[index - 1]
         let rightNum = array[index]
 
 
-        array[staticIndex]
-        index == array.length
-
-
-        // "true" on 4, 4 - 2
-        // "true" on 7, 7 - 5
-
-
-
+        leftmostNum
         leftNum
         rightNum
-        // the last loop is [7, undefined]
 
+        // rightNum and leftNum converge on the last loop
 
-        let plusOne = leftNum + 1
- 
-        if (leftNum + 1 !== rightNum) {      //   || index == array.length) {
+        // this if statement will (maybe) always execute on the last loop
+        // at least its [7, undefined] for the first one
+    
+        leftNum + 1
 
+        if (leftNum + 1 != rightNum) {      //   || index == array.length)
 
-
-            // rightNum and leftNum converge on the last loop
-            
-            array[staticIndex]
+            // let leftmostNum = array[staticIndex]
             leftNum
             rightNum
 
-            // multiple numbers:
-            if (array[staticIndex] != leftNum) {
+            // if (array[staticIndex] != leftNum)
 
-                let leftmostNum = array[staticIndex]
+            // case: there are multiple numbers in the range
 
-                // string
+            let leftmostNum = array[staticIndex]
+
+            leftmostNum
+            leftNum
+
+            // 3 != 7, there are multiple numbers in the range
+
+            if (leftmostNum != leftNum) {
+
                 let range = `${leftmostNum}->${leftNum}`
+                range
 
-
-                rangeArray
-            
+                rangeArray            
                 rangeArray.push(range)
-            
+
                 rangeArray
             }
-
-
-            // just use the else statement, not else if
-
-
-            else {
+        
+            // 7 == 7, its just one number (7)
+            
+            else if (leftmostNum == leftNum) {
                 rangeArray.push(leftNum.toString())
                 rangeArray
             }
 
-
-
             // store the index of `rightNum` in staticIndex
             // only do this when the non-nested if statement executes
 
-
+            leftmostNum
             leftNum
             rightNum
 
-
             // ignore "6" and undefined
-
 
             staticIndex
             index
 
+            rangeArray
 
+            array[staticIndex]
+            
             staticIndex = index
             array[staticIndex]
+
         }
     }
 
-
-return rangeArray
+    return rangeArray
 }
 
-
 console.log(summaryRanges([0, 1, 2, 4, 5, 7]), ["0->2", "4->5", "7"], "break at 3 and at 6, total of 3 ranges. 7 is left alone at the end")
-
 // console.log(summaryRanges([0,2,3,4,6,8,9]), ["0","2->4","6","8->9"])
