1dc906b9662d4ce02e533110ff4dff38b476f459
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-01-01 17:17:01 -0500

    Time: 71 ms (81.16%), Space: 44.2 MB (40.20%) - LeetHub

--- 1394-find-lucky-integer-in-an-array/1394-find-lucky-integer-in-an-array.js
+++ 1394-find-lucky-integer-in-an-array/1394-find-lucky-integer-in-an-array.js
@@ -3,44 +3,51 @@ var findLucky = function(array) {
     // const map = new Map();
     const obj = {}
 
-    let maxLuckyNum = 0
+    let max = -1
 
-    for (let num of array){
+    for (const num of array){
 
-        const numExists = (obj[num] >= 1)
-
+        obj
+        const numExists = (obj[num] != undefined)
+        
         if (numExists) {
             obj[num]  += 1
         }
-
+        
         else if (!numExists) {
             obj[num] = 1
         }
 
         obj
     }
-    
+        
     obj    
+
+    // filter out the ones that arent lucky numbers
 
     array = array.filter(num => {
 
         let frequency = obj[num]
         const isLuckyNumber =  num == frequency
-
-
-        return isLuckyNumber == true
+        return isLuckyNumber == true 
     })
 
-    array = [...new Set(array)]
 
-    if (array.length == 0){
+    // new Set(arrayLike)
+    const set = new Set(array)
+
+    array =  [...set]
+
+    const noLuckyNumbers = array.length == 0
+
+    if (noLuckyNumbers){
         return -1
     }
 
     array
-    
-    maxLuckyNum = Math.max(...array)    
-    return maxLuckyNum
+
+    max = Math.max(...array)    
+    return max
 }
 
 // findLucky([1,1,2,2,2,3,3,3,3])
