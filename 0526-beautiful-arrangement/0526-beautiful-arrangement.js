35940dca718d1dc378ef69fe7ed1e74052a0a275
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-01-02 15:14:38 -0500

    Time: 322 ms (49.12%), Space: 42.8 MB (68.42%) - LeetHub

--- 0526-beautiful-arrangement/0526-beautiful-arrangement.js
+++ 0526-beautiful-arrangement/0526-beautiful-arrangement.js
@@ -1,57 +1,87 @@
-var countArrangement = function(num) {
+/**
+ *
+ * @param {number} inputNum
+ * @return {number[]} array
+ *
+ * @function patternMatch(["abba", ["dog", "cat", "dog", "rocketship"])
+ * @return false
+ * 
+ * @explain
+ */
 
-    if (num <= 2) {
-        return num
+
+var countArrangement = function(inputNum) {
+
+    if (inputNum <= 3) {
+        return inputNum
     }
-    
+
+    inputNum
+
     // array[3] is valid because it breaks at GREATER than 3, not equal to 3
-    // so there needs to be a fourth element
-    const visitedArray = Array(num + 1).fill(false)
-    
+    // so there needs to be a fourth currNum
+
+    const visitedArray = Array(inputNum + 1).fill(false)
+
+    visitedArray
+
     let permutationCount = 0
 
-    var dfsFn = (element) => {
 
-        // if it made it here, it means all numbers in [1,2,3] are valid
-        if (element > num) {
-            permutationCount += 1
-            return // base case
+    var depthFirstSearch = currNum => {
+
+        if (currNum > inputNum) {
+            permutationCount++
+            return  // start from beginning of call stack
         }
 
-        // dfs(1) gets pushed to the bottom of the call stack at index = 1 in the for loop
+        // DFS(1) gets pushed to the bottom of the call stack at index = 1 in the for loop
         // so it still has to loop 2 more times to get all the permutations
 
+        // index <= num
 
-        for (let index = 1; index <= num; index++) {
+        for (let index = 1; index <= inputNum; index++) {
 
-            visitedArray
             index
 
             // avoid double counting
-            
-            const alreadyVisited = visitedArray[index]
-            const isDivisible=  index % element === 0 || element % index === 0
-            
-            if (!alreadyVisited && isDivisible) {
-                
-                visitedArray[index] = true
-                dfsFn(element + 1)
-                visitedArray[index] = false
 
+            visitedArray
+                         
+            const notVisitedYet = visitedArray[index] == false
+            const isDivisible = (currNum % index == 0) || (index % currNum == 0)
+
+            if (notVisitedYet && isDivisible) {
+
+                visitedArray[index] = true
+                visitedArray
+
+                depthFirstSearch(currNum + 1)
+
+                visitedArray[index] = false
                 visitedArray
             }
-            
+
             visitedArray
-        } // end for loop
+        }
 
         return
     }
 
-    visitedArray
 
-    dfsFn(1)
+    visitedArray
+    permutationCount
+
+
+    depthFirstSearch(1)
+
+    permutationCount
     return permutationCount
 }
 
-console.log(countArrangement(3), 3, "[1,2,3] [3,2,1] [2,1,3]")
-// console.log(countArrangement(2), 2, "[1, 2] and [2, 1]")
+console.log(countArrangement(3), 3, "[1,2,3] [3,2,1] [2,1,3]",   
+"[2 (1), 1 (2), 3 (3)]  [(2 % 1 == 0), (2 % 1 == 0), (3 % 3 == 0)], 3 beautiful arrays")
+
+console.log(countArrangement(4), 8)
+
+// console.log(countArrangement(2), 2, "[1, 2] and [2, 1] are the two arrangements")
