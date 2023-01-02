884f6932add74e71372fb2f06eb1d17ad05bf6ed
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-01-02 17:34:07 -0500

    Time: 3455 ms (5.05%), Space: 84.4 MB (5.10%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.js
@@ -0,0 +1,67 @@
+var findDuplicate = function(array) { 
+    
+    // cycle detection - slowIndex jumper and fastIndex jumper will meet somewhere in the cycle
+
+    var slowIndex = 0    // start both at 0
+    var fastIndex = 0
+
+    var indexesAreDifferent = true
+    
+    // slowIndex jumper hops 1 step, fastIndex hops 2
+    // [2, 6, 4, 1, 3, 1, 5]
+    
+    
+    while (indexesAreDifferent) {
+
+        slowIndex
+        slowIndex = array[slowIndex]
+
+        console.log(`one step is ${slowIndex}`)
+
+        fastIndex
+
+        // const intermediate = array[fastIndex]
+
+        fastIndex = array[array[fastIndex]]
+        fastIndex
+
+        // fastIndex = array[array[fastIndex]]
+
+        console.log(`two steps is ${fastIndex}`)
+
+        if (slowIndex == fastIndex) {
+            indexesAreDifferent = false
+            break
+        }
+    }
+
+    // Locate the start node of the *cycle*, which will be the duplicate number
+
+    let go = 0
+    let prevIndex = slowIndex
+
+    prevIndex
+    go
+
+    const duplicateFound = false
+
+    while (!duplicateFound){
+
+        prevIndex
+        prevIndex = array[prevIndex]
+
+        go
+        go = array[go]
+
+        if (prevIndex == go){
+            let duplicate = go
+            return duplicate
+        }
+    }
+
+    return -1
+}
+
+console.log(findDuplicate([1,3,4,2,2]),  2)
+
+// console.log(findDuplicate([2, 5, 9, 6, 9, 3, 8, 9, 7, 1]),  9, "9 is the duplicate")
