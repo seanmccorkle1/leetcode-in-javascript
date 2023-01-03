cdf3342c716e945421934e659a16bc4cbc354cb9
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-01-02 20:28:51 -0500

    Time: 372 ms (43.86%), Space: 41.4 MB (100.00%) - LeetHub

--- 0526-beautiful-arrangement/0526-beautiful-arrangement.js
+++ 0526-beautiful-arrangement/0526-beautiful-arrangement.js
@@ -1,70 +1,70 @@
-/**
- *
- * @param {number} inputNum
- * @return {number[]} array
- *
- * @function patternMatch(["abba", ["dog", "cat", "dog", "rocketship"])
- * @return false
- * 
- * @explain
- */
 
 
 var countArrangement = function(inputNum) {
     
     inputNum
-
-    // array[3] is valid because it breaks at GREATER than 3, not equal to 3
-    // so there needs to be a fourth currNum
-
-    const visitedArray = new Array(inputNum + 1).fill(false)
-
-    visitedArray
-
     let permutationCount = 0
 
 
+    // the first element is one-indexed
+    // basically ignore array[0] and add another number at the end to make up for it 
+    
+    const visitedArray = new Array(inputNum + 1).fill(false)
+    
+
+
     var depthFirstSearch = currNum => {
-        
-        // it can only get here if it repeatedly hits (notVisitedYet && isDivisibleByEither)
-        
+
+
+        // currNum can only get here if it keeps hititng (notVisitedYet && isDivisibleByEither) 
+        // go back to the call at line 41 with (currNum - 1)
+
+
         if (currNum > inputNum) {
-            permutationCount++
-            return  // start from beginning of call stack
+            permutationCount +=1 
+            return  "array found" 
         }
 
-        // DFS(1) gets pushed to the bottom of the call stack at index = 1 in the for loop
-        // so it still has to loop 2 more times to get all the permutations
 
         // index <= num
 
+
         for (let index = 1; index <= inputNum; index++) {
 
+
             index
+            
+            visitedArray
+
 
             // avoid double counting
-
-            visitedArray
-                         
             const notVisitedYet = visitedArray[index] == false
+
+
             const isDivisibleByEither = (currNum % index == 0) || (index % currNum == 0)
 
+
             if (notVisitedYet && isDivisibleByEither) {
+
 
                 visitedArray[index] = true
                 visitedArray
 
+
                 depthFirstSearch(currNum + 1)
+
 
                 visitedArray[index] = false
                 visitedArray
             }
-
+            
             visitedArray
         }
 
-        return
+
+        return "end of for loop"
     }
+
 
 
     visitedArray
@@ -73,16 +73,13 @@ var countArrangement = function(inputNum) {
 
     depthFirstSearch(1)
 
+
+    visitedArray
     permutationCount
+
+
     return permutationCount
 }
 
-console.log(countArrangement(2), 2, "[1, 2] and [2, 1] are the two arrangements")
 
-// console.log(countArrangement(3), 3, "[1,2,3] [3,2,1] [2,1,3]",   
-// "[2 (i1), 1 (i2), 3 (i3)]  [(2 % (i)1 == 0), ( (i)2 % 1 == 0), (3 % i3 == 0)], 3 beautiful arrays")
-
-// console.log(countArrangement(4), 8)
-
-// console.log(countArrangement(3), 3, "[1,2,3] [3,2,1] [2,1,3]",
-// "[2 (i1), 1 (i2), 3 (i3)]  [(2 % (i)1 == 0), ( (i)2 % 1 == 0), (3 % i3 == 0)], 3 beautiful arrays")
+countArrangement(4)
