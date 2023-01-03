c5bf87f6e1828917d4202829161dabf5d2ffec0c
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-01-03 11:27:53 -0500

    Time: 96 ms (63.98%), Space: 46.7 MB (40.09%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -8,7 +8,7 @@ var isPalindrome = function (string) {
 
     let fixed = string.toLowerCase().replace(/[^a-z0-9]/g, "")
     let reverse = fixed.split("").reverse().join("")
-    
+
     if (fixed==reverse){
         return true
     }
