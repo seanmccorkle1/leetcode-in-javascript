054f066a2253a56250f1fe76367a1b09d133295f
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-01-05 13:58:51 -0500

    Time: 114 ms (14.41%), Space: 43.6 MB (24.89%) - LeetHub

---
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.js
@@ -0,0 +1,21 @@
+var uniqueOccurrences = function(array) {
+
+    const obj = {}
+
+    
+    for(let element of array) {
+        (obj[element]) ? obj[element] += 1 
+        : obj[element] = 1 // cant do (undefined + 1)
+    }
+    const uniqueFrequenciesArray = [...new Set(Object.values(obj))]
+    
+    if (Object.values(obj).join("") == uniqueFrequenciesArray.join("")) {
+        return true
+    }
+
+    return false
+}
+
+console.log(uniqueOccurrences([1,2,2,1,1,3]), true, "all frequencies are unique")
+
+// console.log(uniqueOccurrences(["a", "b", "c", "c"]), false, "a and b have the same frequency of 1")
