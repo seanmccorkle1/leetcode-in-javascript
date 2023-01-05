2a3732c41dae24b022b5d813629639448be83f05
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-01-04 19:31:26 -0500

    Time: 444 ms (44.14%), Space: 74.6 MB (40.69%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.js
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.js
@@ -1,12 +1,18 @@
 var findMinArrowShots = function(points) {
-    points.sort((a, b) => a[0] - b[0]);
-    let prev = null, count = 0;
     
-    for(let [start, end] of points) {
-        if(prev == null || prev < start) {
-            count++;
-            prev = end;
-        } else prev = Math.min(prev, end);
+    points.sort((a, b) => a[0] - b[0])
+    
+    let prev = null
+    let count = 0
+    for (let [start, end] of points) {
+        
+        if ( prev < start|| prev == null ) {
+            count++
+            prev = end
+        } 
+        
+        else prev = Math.min(prev, end);
     }
-    return count;
-};
+    
+    return count
+}
