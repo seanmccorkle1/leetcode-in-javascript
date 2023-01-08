c994e4b021c94fd3a89f1784442f87b2a8db4268
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-01-08 15:38:06 -0500

    Time: 70 ms (74.02%), Space: 44 MB (26.14%) - LeetHub

--- 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.js
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.js
@@ -1,6 +1,6 @@
 var arraySign = function (array) {
 
-    let product = array.reduce((a,b) => a*b)
+    let product = array.reduce((a,b) => a*b, 1)
     
     if (product > 0){
         return 1
@@ -9,5 +9,6 @@ var arraySign = function (array) {
         return -1
     }
     
-    else return 0
+    // just use else in these 3+ if statements
+    return 0
 }
