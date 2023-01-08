886f7ea21b96d1d895d5f1e98409145fe58283bf
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-01-08 15:43:06 -0500

    Time: 71 ms (93.75%), Space: 46.4 MB (50.33%) - LeetHub

--- 0055-jump-game/0055-jump-game.js
+++ 0055-jump-game/0055-jump-game.js
@@ -1,12 +1,18 @@
-var canJump = function(array) {
+/**
+ Many question are incorrectly labeled, but i think less than 10 minutes for easy, 20 minutes for medium, and 40 minutes for hard problems is a good strategy. How many depends on your schedule, but i think the number isn't really important. Learning from the question you've solved is more important.
+* 
+*/
+
+
+
+
+var canJump = function (array) {
     let index = 0
-    let max = 0
+    let maxJump = 0
 
     let lastIndex = array.length - 1
-
     lastIndex
 
-    
     while (index < array.length) {
 
         let num = array[index]
@@ -14,27 +20,25 @@ var canJump = function(array) {
         index
         index + array[index]
 
-        max = Math.max(max, index + num)
-        
-        max
+        let jump = index + num
 
+        maxJump = Math.max(maxJump, jump)
+        maxJump
 
-        if (max >= lastIndex) {
+        if (maxJump >= lastIndex) {
             return true
         }
 
-        
         // got a 0 and it didnt make it to the end
-
-        if (max <= index && num == 0) {
-            break
+        
+        if (maxJump <= index && num == 0) {
+            return false
         }
-
         index++
     }
 
-    return false
+
+    // return false
+
 
 }
-
-console.log(canJump([2,3,1,1,4]), true, "1 step to 3 and end at the last element")
