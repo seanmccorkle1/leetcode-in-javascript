b038526e64d1bc7e286f16af72fd768d5e383505
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-01-08 17:09:25 -0500

    Time: 98 ms (91.49%), Space: 52.6 MB (68.62%) - LeetHub

--- 2256-minimum-average-difference/2256-minimum-average-difference.js
+++ 2256-minimum-average-difference/2256-minimum-average-difference.js
@@ -1,98 +1,87 @@
-/**
- Many question are incorrectly labeled, but I think less than 10 min for easy, 20 min for medium, and 40 min for hard 
- is a good strategy. How many - depend on your schedule, but I think the number isn't really important. Learning from the question you've solved is indeed more essential.
- * 
- * @params 
- * @return 
- * @explain 
- */
-
 
 var minimumAverageDifference = function (array) {
 
-    let leftSum = 0
+    let leftSum = 0    
+   let rightSum = array.reduce((s, c) => s + c, 0)
+   
+   let min = Infinity
+   let indexOfMinDifference = 0
+   
+   // parantheses on (index+1) cause theres / and +
+   //Math floor so you dont get array decimal with the division
 
-    // start rightSum 
-    let rightSum = array.reduce((s, c) => s + c, 0)
-    
-    let minFound = Infinity
-    let indexOfMinDifference = 0
+   leftSum
+   rightSum
 
-    // parantheses on (index+1) cause theres / and +
-    //Math floor so you dont get array decimal with the division
+   // (array.length - index - 1) means it progressively goes down
 
-    leftSum
-    rightSum
+   // i=0 (6 - 0 - 1 = 5)
+   // i=1 (6 - 1 - 1 = 4)
+   // i = 2(6 - 2 -1 = 3)
 
-    // (array.length - index - 1) means it progressively goes down
+   // start at array high denominator and end at x/1
+   
+   for (let index = 0; index < array.length; index += 1) {
 
-    // i=0 (6 - 0 - 1 = 5)
-    // i=1 (6 - 1 - 1 = 4)
-    // i = 2(6 - 2 -1 = 3)
+       let num = array[index]
 
-    // start at array high denominator and end at x/1
+       leftSum
+       rightSum
+   
+       // leftSum starts at 2
+       // rightSum leavs out  the 2 by doing (rightSum -= 2)
+       
+       leftSum += num
+       rightSum -= num
+       
+       // start at (x / 1)
+       let leftSumDenominator = index + 1
+       let rightSumDenominator = (array.length - index - 1)
 
+       if (rightSumDenominator === 0) {
+           rightSumDenominator = 1 // cant divide by 0
+       }
 
+       // start at (leftSum / 1) with leftSum / (index + 1)
+       // with index=0, that way you still get array[0]
 
-    for (let index = 0; index < array.length; index += 1) {
+       let leftSumAvg = Math.trunc(leftSum / leftSumDenominator)
+       
+       // start at (rightSum / 5) 
+       let rightSumAvg = Math.trunc(rightSum / rightSumDenominator)
 
-        const num = array[index]
+       let difference = Math.abs(leftSumAvg - rightSumAvg)
 
-        leftSum
-        rightSum
+       // `difference` is lower than min
+       // that means its the new min
+       // `difference` gets refreshed on every loop, 
+       // min doesnt unless you assign it
 
-        leftSum += num
-        rightSum -= num
+       
+       if (difference < min) { 
 
-        // leftSum starts with +2
-        // rightSum doesnt get the +2 by doing (rightSum -= 2)
+           min = difference 
+           indexOfMinDifference = index // store the min index here
 
-        let leftSumDenominator = index + 1
-        let rightSumDenominator = array.length - index - 1
+           // leftSum is 4 and rightSum is 4, (4-4) 
 
-        if (rightSumDenominator === 0) {
-            rightSumDenominator = 1 // cant divide by 0
-        }
+           // leftSum =>(2+5+3+9 / 4) => 19/4 
+           // floor(19/4 )= 4
 
-        // start at (leftSum / 1) with leftSum / (index + 1)
-        // with index=0, that way you still get array[0]
+           // rightSum is (5+3+4 / 3) => 12/3
+           // 12/3 = 4
 
-        let leftSumAvg = Math.floor(leftSum / leftSumDenominator)
+           if (min === 0) {
+               return index // return current index
+           }
+       }
 
-        // start at (rightSum / 5) 
-        let rightSumAvg = Math.floor(rightSum / rightSumDenominator)
+       // kinda like best time to buy and sell stock
+       // reassign variables and dont return until the end
 
-        let difference = Math.abs(leftSumAvg - rightSumAvg)
+   }
 
-        // `difference` is lower than minFound
-        // that means its the new min
-        // `difference` gets refreshed on every loop, 
-        // minFound doesnt unless you assign it
-
-
-        if (difference < minFound) { 
-            minFound = difference 
-            indexOfMinDifference = index // store the min index here
-
-            // leftSum is 4 and rightSum is 4, (4-4) 
-
-            // leftSum =>(2+5+3+9 / 4) => 19/4 
-            // floor(19/4 )= 4
-
-            // rightSum is (5+3+4 / 3) => 12/3
-            // 12/3 = 4
-
-            if (minFound === 0) {
-                return index // return current index
-            }
-        }
-
-        // kinda like best time to buy and sell stock
-        // reassign variables and dont return until the end
-
-    }
-
-    return indexOfMinDifference
+   return indexOfMinDifference
 }
 
 console.log(minimumAverageDifference([2, 5, 3, 9, 5, 3, 4]),   3)
