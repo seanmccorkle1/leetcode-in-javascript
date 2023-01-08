8b886d53d5bb27d8ca1cbdd72beae0511a56b5cc
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-01-08 16:32:39 -0500

    Time: 115 ms (74.15%), Space: 50.9 MB (80.95%) - LeetHub

--- 1684-count-the-number-of-consistent-strings/1684-count-the-number-of-consistent-strings.js
+++ 1684-count-the-number-of-consistent-strings/1684-count-the-number-of-consistent-strings.js
@@ -1,39 +1,18 @@
-/**
- * 
- * Given an array of strings, and an integer k, return the kth distinct string in the array. 
- * If there are fewer than k distinct strings, return an empty string "".
- *
- * @param summaryRanges([0, 1, 2, 4, 5, 7])
- * @return  ["0->2", "4->5", "7"]
- * @explain
- */
+var countConsistentStrings = function(allowedLetters, stringArray){
 
-var countConsistentStrings = function(allowedLetters, wordArray) {
+    let count = 0
+    const r  = new RegExp(`[${allowedLetters}]` , "g") 
+
     
-    let count=0
-
-    const regexString = `[${allowedLetters}]`
-    const allowedRegex = new RegExp(regexString, "g")
-    
-    
-    // `${allowedLetters}`
-    // const r=/`${allowedLetters}]/g
-
-    for (let word of wordArray){
-
-        word = word.replace(allowedRegex, "")
-        word
-
-        const wordAllowed = (word == "")
-
-        
-        if (wordAllowed){
+    for (let word of stringArray) {
+        word = word.replace(r, "")
+        if (word===""){
             count++
         }
-
-        word
+            
     }
+    
     return count
 }
 
-countConsistentStrings("ab", ["ad","bd","aaab","baa","badab"])
+console.log(countConsistentStrings("ab", ["xd", "aaaaa", "aaab", "baa"]), 3)
