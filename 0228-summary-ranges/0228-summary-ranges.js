306e49a5d56bfa4ecae160c66341369c160bb098
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-01-08 17:02:16 -0500

    Time: 74 ms (57.53%), Space: 42.1 MB (55.29%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.js
+++ 0228-summary-ranges/0228-summary-ranges.js
@@ -1,31 +1,23 @@
 var summaryRanges = function (array) {
     
     const rangeArray = []
-    var rangeFound = false    
     let leftIndex = 0
-
-
-
+    
     // rightIndex <= array.length
     // start at rightIndex=1 for the difference 
     
     // 7+1 != undefined  = true
     // the if statement will always execute on the last element (undefined)
-
-
-
+    
     //  currNum is at array[index - 1]
     // in order to get the last number inside currNum, you have to do index + 1
-    // to make it [lastNum (curr), undefined (rightNum)]
+    //[lastNum (curr)], undefined (rightNum)
     
-    
+
     for (let rightIndex = 1; rightIndex <= array.length; rightIndex++) {
-
-
+        
         let startOfRange = array[leftIndex]
 
-        // start at array[0]
-        
         let currNum = array[rightIndex - 1]
         let rightNum = array[rightIndex]
         
@@ -35,42 +27,24 @@ var summaryRanges = function (array) {
 
 
         // rightNum and currNum converge on the last loop
-
-
         // this if statement will (maybe) always execute on the last loop
         // at least its [7, undefined] for the first one
-    
-
-
-        currNum + 1
-        rightNum
         
-        const numberIsMissing = (currNum + 1 != rightNum)
         
         // the last number will always either be the end of a range 
         // or it will be its own range
         
-        if (numberIsMissing) { // || rightNum == undefined) {
-            
-            currNum 
-            // let startOfRange = array[leftIndex]
+        currNum + 1
+        rightNum
+        
+        if ((currNum + 1) != rightNum) {
+
             currNum
             rightNum
-
-
-            // if (array[leftIndex] != currNum)
-
-
-            // case: there are multiple numbers in the range
-
-
-            currNum
-
-
-            // 3 != 7, so push the range '3->7'
-
+            
             let endOfRange = currNum
             
+            // if (array[leftIndex ] != array[rightIndex - 1])
             
             if (startOfRange != endOfRange) { 
                 let inclusiveRange=  startOfRange+"->" + endOfRange
@@ -78,32 +52,21 @@ var summaryRanges = function (array) {
             }
             
             // 7 == 7, its just one number (7)
-
+            
             else if (startOfRange == endOfRange) {
                 rangeArray.push(currNum.toString())
                 rangeArray
             }
-
-
+            
             // store the rightIndex of `rightNum` in leftIndex
             // only do this when the non-nested if statement executes
             
-            startOfRange
-            currNum
-            rightNum
-            
-            leftIndex
-            rightIndex
-            
             rangeArray
             
-            leftIndex = rightIndex
-            array[leftIndex]
-            
+            leftIndex = rightIndex            
         }
     }
-
-
+    
     return rangeArray
 }
 
