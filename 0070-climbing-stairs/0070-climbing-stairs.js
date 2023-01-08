c1557d4fb3c41a7036cfa170fcd1ccb0a1ae0ac1
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-01-08 18:09:20 -0500

    Time: 65 ms (79.70%), Space: 42.1 MB (18.31%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.js
+++ 0070-climbing-stairs/0070-climbing-stairs.js
@@ -1,16 +1,27 @@
+/**
+ * 
+ * @function
+ * @return
+ * 
+ */
 
-var climbStairs = function(num) {
-    let array=new Array(num+1).fill(0)
-    array[1]=  1
-    array[2]=2
+
+var climbStairs = function (num) {
+
+    const array = new Array(num).fill(0)
+
+    array[1] = 1
+    array[2] = 2
     array[3] = 3
 
-    if (num <= 3){
+    array
+
+    if (num <= 3) {
         return num
     }
-    
-    for (let index=4; index <= num; index += 1){
-        array[index] =array[index-1]  + array[index-2]
+
+    for (let index = 4; index <= num; index += 1) {
+        array[index] = array[index - 1] + array[index - 2]
     }
 
     // get the last number in the array
@@ -18,4 +29,5 @@ var climbStairs = function(num) {
 }
 
 console.log(climbStairs(4), 5, "5 ways to climb 4 steps, [[2, 2], [1,1,1,1], [2, 1, 1], [1, 2, 1], [1, 1, 2]")
+
 // console.log(climbStairs(2), 2, "two ways, [2] and [1, 1]")
