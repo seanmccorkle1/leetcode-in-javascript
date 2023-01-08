2e51432781b0062b4a5f7eaca0b837185ad13f49
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-01-08 18:04:59 -0500

    Time: 72 ms (65.85%), Space: 42.3 MB (26.23%) - LeetHub

--- 2427-number-of-common-factors/2427-number-of-common-factors.js
+++ 2427-number-of-common-factors/2427-number-of-common-factors.js
@@ -1,101 +1,31 @@
-/**
- Many question are incorrectly labeled, but I think less than 10 min for easy, 20 min for medium, and 40 min for hard 
- is a good strategy. How many  -  depend on your schedule, but I think the number isnt really important. Learning from the question youve solved is indeed more essential.
-* 
-*/
-
-/**
- * @param {number} num1
- * @param {number} num2
- * @return {number}
- */
-
-
-var commonFactors = function(num1, num2) {
-
-    let factorCount = 1
-    let index = 2
-
-    // const lowestNum = (Math.min(num1, num2))
+var commonFactors = function (num1, num2) {
 
     num1
     num2
 
-    const lowestNum = 
-
-    Math.min(
-        num1,  
-        num2) 
-
-
-    lowestNum
-
-
-    let commonFactors = [1]
-    let nonCommonFactors = [0]
-
-    let quotient1 = 0
-    let quotient2 = 0
-
-
-    num1
-    num2
-
-    lowestNum
+    let factorCount = 0
     
-    // <= to get the last 12 as a factor 
+    // only go to the lower of the 2 numbers
+    const lowerNum = Math.min(num1, num2)
 
-    factorCount
-    index
-    
-    lowestNum
+    // index <= lowerNum
 
-
-    while (index <= lowestNum) {
+    for (let index = 0; index <= lowerNum; index++) {
 
         index
 
-        const indexIsCommonFactor = ((num1 % index)== 0 && (num2 % index)== 0)
+        let quotient1 = num1 / index
+        let quotient2 = num2 / index
 
-        if (indexIsCommonFactor) {
+        const commonFactorFound = Number.isInteger(quotient1) && Number.isInteger(quotient2)
 
-            factorCount++
-
-            quotient1= num1 / index
-            quotient2= num2/index
-
-            index
-            // commonFactors.push(`${index} is a factor of both ${quotient1} and ${quotient2}`)
-
-            commonFactors.push(index)
-
-            commonFactors
+        if (commonFactorFound) {
+            factorCount += 1
         }
-
-        else if (!indexIsCommonFactor) {
-            nonCommonFactors.push(index)
-
-            quotient1 = num1 / index
-            quotient2 = num2 / index
-
-            nonCommonFactors.push(`${quotient1} and ${quotient2} are both not integers`)
-
-            nonCommonFactors
-        }
-
-        index++
     }
-
-    nonCommonFactors
-        
-    commonFactors
-    commonFactors.length
-
 
     return factorCount
 }
 
-console.log(commonFactors(12,24), "[1, 2, 3, 4, 6, 12] are the 6 common factors of both nums")
-
-// console.log(commonFactors(6, 1),   1,  "[1] is the only common factor, 6/1 and 1/1 are both integers")
-// console.log(commonFactors(3, 12),   2, "1 and 3")
+console.log(commonFactors(12, 24), 6, "[1, 2, 3, 4, 6, 12] are the 6 common factors of both numbs")
+// console.log(commonFactors(1, 6),  1,  "1 is the only common factor")
