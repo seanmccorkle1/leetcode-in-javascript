31ddc9d0808876ed83808c2f9acea0c97d81fad8
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-01-08 23:04:53 -0500

    Time: 60 ms (83.99%), Space: 42.3 MB (11.66%) - LeetHub

--- 0198-house-robber/0198-house-robber.js
+++ 0198-house-robber/0198-house-robber.js
@@ -1,64 +1,75 @@
+/**
+ * 
+ * @function
+ * @return
+ * 
+ */
+
+
 var rob = function(inputArray) {
-    
     
     if (inputArray.length === 0) {
         return 0
     }
 
-    
-    // for rob([8])  array[1] = Math.max(8, inputArray[1]) returns NaN
+    // for robMoney([8])  array[1] = Math.max(8, inputArray[1]) returns NaN
     // Math.max and min always privilege NaN
-    
-    if (inputArray.length===1){ 
+
+    if (inputArray.length == 1){ 
         return Math.max(...inputArray)
     }
-    
 
-    let array = new Array(inputArray.length)
+    const array = new Array(inputArray.length)
+    
 
     array[0] = inputArray[0]
-    
+
     // start [2,1,1,2] off as [2, 2]    
-    // you have to skip (inputArray[0]) or rob (take the current element) here
+    // you have to skipMoney (inputArray[0]) or robMoney (take the current element) here
 
     array[1] = Math.max(inputArray[0], inputArray[1])
-    
-    
-    /*
-    If we choose to rob, we can't rob the next house
-    If we choose to skip, we can rob the next house
-
-    array[index] is the max amount of money we can rob without alerting the police at house `index`
-    */
-
-    // modify the array to keep the sum running
     array
 
+    /*
+    If we choose to robMoney, we can't robMoney the next house
+    If we choose to skipMoney, we can robMoney the next house
+
+    array[index] is the max amount of money we can robMoney without alerting the police at house `index`
+    */
+    
+    // modify the array to keep the sum running
+    array
+    
     for (let index = 2; index < inputArray.length; index++) {
 
         index
         const currNum = inputArray[index]
 
-        let rob = array[index - 2] + currNum
-        let skip = array[index - 1]
+        let robMoney = array[index - 2] + currNum
+        let skipMoney = array[index - 1]
 
-        rob
-        skip
+        robMoney
+        skipMoney
 
-        
-        array[index] = Math.max(rob, skip)
+        const maxMoney =
+            Math.max(
+                robMoney, 
+                skipMoney)
 
-        // 2 + 9 beats 7 + 3, so put 11 in ("rob" it)
-        // "skip" [3] by just ignoring it and keeping 11
+        array[index] = maxMoney
+
+        // 2 + 9 beats 7 + 3, so put 11 in ("robMoney" it)
+        // "skipMoney" [3] by just ignoring it and keeping 11
         
         inputArray
         array
     }
-
     
     array
     return array.at(-1) 
 }
 
-console.log(rob([2, 7, 9, 3, 1]), 12, "2, 9, 1 = 12")
-console.log(rob([2,1,1,2]), 4)
+// console.log(rob([2, 7, 9, 3, 1]), 12, "2 + 9 + 1 = 12, move up 2 each time")
+console.log(rob([7, 2, 9, 3, 1]), 12, "2 + 9 + 1 = 12, move up 2 each time")
+
+// console.log(rob([2,1,1,2]), 4)
