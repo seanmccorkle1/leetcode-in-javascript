360af285b17b1ec584b4f2f41aeeede602c85adb
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-01-10 09:51:47 -0500

    Time: 62 ms (79.89%), Space: 42.2 MB (19.56%) - LeetHub

--- 0198-house-robber/0198-house-robber.js
+++ 0198-house-robber/0198-house-robber.js
@@ -1,61 +1,46 @@
 var rob = function(inputArray) {
-    
-    if (inputArray.length == 0) {
-        return 0
-    }
-    
-    // if (inputArray.length == 1){ 
-    //     return inputArray[0]
-    // }
-    
+
+    // [1, 1] no adjacent houses can be robbed, max is 1
     if (inputArray.length == 1 || inputArray.length == 2){
-        return Math.max(...inputArray)
+        return Math.max(...inputArray) 
     }
     
     // start [2,1,1,2] off as [2, 2]    
     // you have to skipMoney (inputArray[0]) or robMoney (take the current element) here
-
+    
+        // array[index] (each element) is the max amount of money you can rob without alerting the police at house #`index`
+    // modify the array to keep the sum running
+    
     const array = new Array(inputArray.length)
     
     array[0] = inputArray[0]
-    array[1] = Math.max(inputArray[0], inputArray[1])
-    // array[1] = inputArray[1]
-    
-    
-    // array[index] (each element) is the max amount of money you can rob without alerting the police at house #`index`
-    // modify the array to keep the sum running
-    
+    array[1] = Math.max(inputArray[0], inputArray[1])    
+
     array
+
     
     for (let index = 2; index < inputArray.length; index++) {
-
+        
         index
         
         let currHouse = inputArray[index]
-
+        
         // rob by adding the currHouse (robbing the current house)
         let robMoney = array[index - 2] + currHouse
         
         // skip by ignoring currHouse (ignoring the house)
         let skipMoney = array[index - 1]
-
+        
         robMoney
         skipMoney
-
+        
         const maxMoney =
             Math.max(
                 robMoney, 
                 skipMoney)
 
         array[index] = maxMoney
-        
-        // 2 + 9 beats 7 + 3, so put 11 in ("robMoney" it)
-        // "skipMoney" [3] by just ignoring it and keeping 11
-        
-        inputArray
-        array
-    }
-    
+    }    
     array
     return array.at(-1) 
 }
