7f22b4045f7009920c891945dcb55e08831b14ea
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-01-12 14:16:37 -0500

    Time: 105 ms (48.19%), Space: 45.9 MB (5.70%) - LeetHub

--- 0273-integer-to-english-words/0273-integer-to-english-words.js
+++ 0273-integer-to-english-words/0273-integer-to-english-words.js
@@ -7,11 +7,44 @@
 
 
 var numberToWords = function (num) {
-    
-    const numsWithWordsArray = [[1000000000, 'Billion'], [1000000, 'Million'], [1000, 'Thousand'], [100, 'Hundred'], [90, 'Ninety'], [80, 'Eighty'], [70, 'Seventy'], [60, 'Sixty'], [50, 'Fifty'], [40, 'Forty'], [30, 'Thirty'], [20, 'Twenty'], [19, 'Nineteen'], [18, 'Eighteen'], [17, 'Seventeen'], [16, 'Sixteen'], [15, 'Fifteen'], [14, 'Fourteen'], [13, 'Thirteen'], [12, 'Twelve'], [11, 'Eleven'], [10, 'Ten'], [9, 'Nine'], [8, 'Eight'], [7, 'Seven'], [6, 'Six'], [5, 'Five'], [4, 'Four'], [3, 'Three'], [2, 'Two'], [1, 'One'], [0, "Zero"]]
-    
+
+    const numsWithWordsArray = [
+        [1000000000, 'Billion'],
+        [1000000, 'Million'],
+        [1000, 'Thousand'],
+        [100, 'Hundred'],
+        [90, 'Ninety'],
+        [80, 'Eighty'],
+        [70, 'Seventy'],
+        [60, 'Sixty'],
+        [50, 'Fifty'],
+        [40, 'Forty'],
+        [30, 'Thirty'],
+        [20, 'Twenty'],
+        [19, 'Nineteen'],
+        [18, 'Eighteen'],
+        [17, 'Seventeen'],
+        [16, 'Sixteen'],
+        [15, 'Fifteen'],
+        [14, 'Fourteen'],
+        [13, 'Thirteen'],
+        [12, 'Twelve'],
+        [11, 'Eleven'],
+        [10, 'Ten'],
+        [9, 'Nine'],
+        [8, 'Eight'],
+        [7, 'Seven'],
+        [6, 'Six'],
+        [5, 'Five'],
+        [4, 'Four'],
+        [3, 'Three'],
+        [2, 'Two'],
+        [1, 'One'],
+        [0, "Zero"]
+    ]
+
     const wordMap = new Map(numsWithWordsArray)
-    const resultArray = []
+    const stringArray = []
 
 
     // wordsMap has every exact number from [0, ..., 20] 
@@ -25,18 +58,13 @@ var numberToWords = function (num) {
         word
 
         const multiplier = Math.trunc(num / value)
-
-        // if (multiplier == 0) {
-        //     continue
-        // }
-
         const negCheck = num - value
 
         if (negCheck <= -1) {
             continue
         }
 
-        if (num == 0){
+        if (num == 0) {
             break
         }
 
@@ -47,31 +75,34 @@ var numberToWords = function (num) {
         num
 
         if (multiplier == 1 && value >= 100) {
-            resultArray.push("One", word)
-            resultArray
+            stringArray.push("One", word)
+            stringArray
             continue
-        }
-
+        } 
+        
         else if (multiplier == 1 && value <= 99) {
-            resultArray.push(word)
-            resultArray
+            stringArray.push(word)
+            stringArray
             continue
+        } 
+        
+        else {
+            const singleDigitWord = numberToWords(multiplier)
+
+            stringArray.push(singleDigitWord, word)
+            stringArray
         }
-
-        var v = numberToWords(multiplier)
-        // word
-
-        resultArray.push(numberToWords(multiplier), word)
-        resultArray
     }
 
-    resultArray
-    return resultArray.join(" ")
+    stringArray
+    return stringArray.join(" ")
 }
 
 
 // console.log(numberToWords(123), "One Hundred Twenty Three")
 // console.log(numberToWords(223), "Two Hundred Twenty Three")
+
+// console.log(numberToWords(208), "Two Hundred Eight")
 
 // console.log(numberToWords(1000023), "One Million")
 // console.log(numberToWords(2000023), "Two Million")
