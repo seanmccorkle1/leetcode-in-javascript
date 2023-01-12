88bd3682b9c936a7e62b568fb844fe2f7521887f
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-01-12 16:05:43 -0500

    Time: 70 ms (78.94%), Space: 42.4 MB (80.29%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.js
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.js
@@ -1,73 +1,37 @@
 var minCostClimbingStairs = function (array) {
-    
-    // if (array.length <= 2) {
-    //     return Math.min(...array)
-    // }
 
-   
     array
 
-   
-    // 3 or more, go into this loop    
     for (index = 2; index < array.length; index++) {
-    
+
         const currCost = array[index]
-        
-        array
-        let oneBefore = array[index-1]
-        let twoBefore = array[index-2]
 
-
+        let oneStepBeforeCost = array[index - 1]
+        let twoStepsBeforeCost = array[index - 2]
 
         // 1 or 2 steps, whichever one has a lower cost
-        let minOfTwo=Math.min(oneBefore, twoBefore)
-
-
+        const minCostOfLastTwo = Math.min(oneStepBeforeCost, twoStepsBeforeCost)
 
         // prefix sum thing
-
-
 
         // [10, 15, 20, 2000] becomes
         // [10, 15, 30, 2000]
         // to account for the 20 cost and the 10 cost
 
-
-
         currCost
-        minOfTwo
+        minCostOfLastTwo
 
+        array[index] = currCost + minCostOfLastTwo
 
-
-        array[index] = minOfTwo + currCost
         array
     }
 
-
-
-    array
-
-
-    let last = array.at(-1)
-    let secondToLast = array.at(-2)
-
-
-    //[10, 15, 20], top
-
-
-
-    let minCostToTop =
-        Math.min(
-            last,
-            secondToLast)
-
-
-    return minCostToTop
+    // take 1 step from the last, or 2 steps for the second-to-last
+    array    
+    return Math.min(array.at(-1), array.at(-2))
 }
 
-
 console.log(minCostClimbingStairs([1, 100, 1, 1, 1, 100, 1, 1, 100, 1]), 6, "dont get any 100s")
-
 
 // console.log(minCostClimbingStairs([10, 15, 20, 1000]), 30, "pay 10, pay 20, then you get to the end. the end is really outside the array like `[10, 15, (20 + 10), 2000]`")
 // console.log(minCostClimbingStairs([10, 1], 1), 1)
