9531a21ec5a1f025ea3074728c00590ffc1620ca
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-01-12 13:52:05 -0500

    Time: 79 ms (76.68%), Space: 46 MB (5.70%) - LeetHub

--- 0273-integer-to-english-words/0273-integer-to-english-words.js
+++ 0273-integer-to-english-words/0273-integer-to-english-words.js
@@ -1,52 +1,72 @@
+/**
+ * 
+ * @function
+ * @return
+ * 
+ */
+
+
 var numberToWords = function (num) {
     
     const numsWithWordsArray = [[1000000000, 'Billion'], [1000000, 'Million'], [1000, 'Thousand'], [100, 'Hundred'], [90, 'Ninety'], [80, 'Eighty'], [70, 'Seventy'], [60, 'Sixty'], [50, 'Fifty'], [40, 'Forty'], [30, 'Thirty'], [20, 'Twenty'], [19, 'Nineteen'], [18, 'Eighteen'], [17, 'Seventeen'], [16, 'Sixteen'], [15, 'Fifteen'], [14, 'Fourteen'], [13, 'Thirteen'], [12, 'Twelve'], [11, 'Eleven'], [10, 'Ten'], [9, 'Nine'], [8, 'Eight'], [7, 'Seven'], [6, 'Six'], [5, 'Five'], [4, 'Four'], [3, 'Three'], [2, 'Two'], [1, 'One'], [0, "Zero"]]
     
     const wordMap = new Map(numsWithWordsArray)
     const resultArray = []
-    
-    if (num <= 20){
+
+
+    // wordsMap has every exact number from [0, ..., 20] 
+    if (num <= 20) {
         return wordMap.get(num)
     }
-    
-    //     if (num == 0) {
-    //         return "Zero"
-    //     }
 
-    //     if (num <= 20) {
-    //         let numAsWord = wordMap.get(num)
-    //         return numAsWord
-    //     }
+    for (const [value, word] of wordMap) {
 
-    
-    for (let [value, english] of wordMap) {
+        // const numIsTooSmall = (num - value) <= 0
+
+        // if (numIsTooSmall || num == 0) {
+        //     continue
+        // }
+
+        const multiplier = Math.floor(num / value)
+
+        // if (multiplier == 0) {
+        //     continue
+        // }
+        
+        const check = num - value
+        
+        if (check <= -1) {
+            continue
+        }
+
+        if (num == 0){
+            continue
+        }
 
         value
-        english
+        multiplier
 
-        const times = Math.floor(num / value)
+        num -= (value * multiplier)
 
-        if (times == 0) {
+        num
+
+        if (multiplier == 1 && value >= 100) {
+            resultArray.push("One", word)
+            resultArray
+
             continue
         }
 
-        if (num == 0) {
+        else if (multiplier == 1 && value <= 99) {
+            resultArray.push(word)
+            resultArray
             continue
         }
 
-        num -= (times * value)
+        let s="asd"
+        s
 
-        if (times == 1 && value >= 100) {
-            resultArray.push("One", english)
-            continue // top
-        }
-
-        if (times == 1) {
-            resultArray.push(english)
-            continue
-        }
-
-        resultArray.push(numberToWords(times), english)
+        resultArray.push(numberToWords(multiplier), word)
         resultArray
     }
 
@@ -54,7 +74,10 @@ var numberToWords = function (num) {
     return resultArray.join(" ")
 }
 
-// console.log(numberToWords(12), "Twelve")
+console.log(numberToWords(123), "One Hundred Twenty Three")
 
-// console.log(numberToWords(123), "One Hundred Twenty Three")
+// console.log(numberToWords(1000023), "One Million")
+// console.log(numberToWords(2000023), "Two Million")
+
+// console.log(numberToWords(12), "Twelve")
 // console.log(numberToWords(0), "Zero")
