05ea0c62994c97c0823c43745412d956419e55f9
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-01-11 23:24:31 -0500

    Time: 69 ms (69.44%), Space: 42 MB (62.79%) - LeetHub

--- 0066-plus-one/0066-plus-one.js
+++ 0066-plus-one/0066-plus-one.js
@@ -1,37 +1,24 @@
-
-
 var plusOne = function(array) {
-
-
-
+    
     // start from the ones place
-
-
     for (let backIndex = array.length - 1; backIndex >= 0; backIndex--) {
-
-
+        
         let digit = array[backIndex]
-
-
+        
         if (digit == 9){
             array[backIndex] = 0
         }
-
-
+        
         else if (digit != 9){
             array[backIndex] += 1
             return array
         }
     }
-
-
     // if all the numbers were 9, return this
     return [1,...array]
 }
 
-
 console.log(plusOne([1,2]), [1,3], "12 + 1 = 13")
-
 
 // console.log(plusOne([8,2,9]), [8,3,0], "829 +1 = 830")
 // console.log(plusOne([9,9]), [1,0,0])
