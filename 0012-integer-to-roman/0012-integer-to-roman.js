e06188c6f40293c66f2b8e0d50a3a3d481f7bbba
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-01-12 00:57:18 -0500

    Time: 123 ms (87.12%), Space: 47 MB (76.93%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.js
@@ -0,0 +1,58 @@
+/**
+ Many question are incorrectly labeled, but i think less than 10 minutes for easy, 20 minutes for medium, and 40 minutes for hard problems is a good strategy. How many depends on your schedule, but i think the num isn't really important. Learning from the question you've solved is more important.
+* 
+*/
+
+/**
+ * @param {num} num
+ * @return {string}
+ */
+
+var intToRoman = function (num) {
+
+    let currString = ""
+
+    //  descending order object
+    
+    const letterObj = {
+        "M": 1000,
+        "CM": 900,
+        "D": 500,
+        "CD": 400,
+        "C": 100,
+        "XC": 90,
+        "L": 50,
+        "XL": 40,
+        "X": 10,
+        "IX": 9,
+        "V": 5,
+        "IV": 4,
+        "I": 1,
+    }
+
+    // always start from "M"
+
+    for (const letter in letterObj) {
+
+        if (num == 0) {
+            return currString
+        }
+        
+        let letterNum = letterObj[letter]
+
+        while (num >= letterNum) {
+            currString += letter
+            num -= letterNum
+
+            currString
+        }
+        currString
+    }
+
+    return currString
+}
+
+console.log(intToRoman(19), "XIX")
+
+// console.log(intToRoman(58), "LVIII")
+// console.log(intToRoman(165), "CLXV")
