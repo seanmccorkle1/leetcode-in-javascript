e4ee43cb82cd31a4f7b563a9b79a1cf4a471ddba
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-01-12 12:16:10 -0500

    Time: 69 ms (81.93%), Space: 44.4 MB (5.90%) - LeetHub

--- 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.js
+++ 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.js
@@ -1,52 +1,38 @@
 var arithmeticTriplets = function(array, targetDifference) {
-
-
+    
     const indexTriplets = []
     const triplets = []
-
-
+    
     let tripletCount = 0
-
-
+    
+    // you have 1 (num) and 3 (difference), 
+    // if the array has (1 + 3) and (1 + (3 x 2)), then its a triplet
 
     for (let firstNum of array){
 
-
-        // you have 1 (num) and 3 (difference), 
-        // if the array has (1 + 3) and (1 + (3 x 2)), then its a triplet
-
-
         let secondNum = firstNum + targetDifference
         let thirdNum = firstNum + (targetDifference * 2)
-
-
         
         // triplets all follow this pattern
         const tripletFound = array.includes(secondNum) && array.includes(thirdNum) 
-
-
+        
         if (tripletFound) {
-
-
             tripletCount++
-
-
+            
             let indexSubarray = [array.indexOf(firstNum), array.indexOf(secondNum), array.indexOf(thirdNum)]
+            
             let numSubarray = [firstNum,secondNum,thirdNum]
-
-
+            
             indexTriplets.push(indexSubarray)
             triplets.push(numSubarray)
-            
-            
         }
     }
-
-
     triplets
     indexTriplets
-
-
+    
     return tripletCount
 }
 
+console.log(arithmeticTriplets([0,1,4,6,7,10], 3), 2, "(1,2,4) and (2, 4, 5) are the two triplets")
+
+// console.log(arithmeticTriplets([0, 1, 2], 1), 1, "just 1 triplet, [2-1 = 1, 1-0= 1, two differences to get a triplet")
