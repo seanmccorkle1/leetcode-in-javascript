c4cf9244e60d54da79aacc918144ac664944778e
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-01-11 23:41:01 -0500

    Time: 102 ms (61.51%), Space: 51.9 MB (41.08%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,34 @@
+var maxProfit = function (array) {
+
+
+    let maxStockProfit = 0
+
+
+    // accounts for buying first by being behind and being negative
+    let buyNum = array[0]
+
+
+    // if buyNum is greater than the result is negative
+    
+    for (const sellNum of array) {
+        
+        maxStockProfit
+        
+        // buy it for low (lose $2), sell it high (gain $10)
+
+
+        if (buyNum <= sellNum) {
+            let profit  = sellNum - buyNum
+            maxStockProfit = Math.max(profit, maxStockProfit)
+        } 
+        
+        else if (buyNum > sellNum) {
+            buyNum = sellNum // buy comes first like this
+        }
+    }
+    
+    return maxStockProfit
+}
+
+
+console.log(maxProfit([7, 3, 1, 5, 3, 6, 0, 4]), 5, "6 - 1 for 5")
