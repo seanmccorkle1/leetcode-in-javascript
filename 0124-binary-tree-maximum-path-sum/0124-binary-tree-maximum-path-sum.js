e8a7450124e0e41c6df3a018418fb2076278801c
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-01-12 19:33:19 -0500

    Time: 102 ms (62.53%), Space: 51.5 MB (63.79%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.js
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.js
@@ -12,26 +12,29 @@
  */
 
 var maxPathSum =function(root){
-	let max = -Infinity;
-
-	const findSums = (node) => {
-		// Base case / hit a null
-		if (!node) return 0;
-
+	let max = -Infinity
+    
+    // base case / hit a null
+	const findSums = (node) => {		// base case / hit a null
+        
+        if (!node) {
+            return 0
+        }
+        
 		let left = findSums(node.left),
 			right = findSums(node.right),
 			allSum = left + right + node.val,
 			leftNodeSum = left + node.val,
-			rightNodeSum = right + node.val;
+			rightNodeSum = right + node.val
 
 		// Max is all possible combinations
 		max = Math.max(max, node.val, allSum, leftNodeSum, rightNodeSum);
 		
 		// Return the MAX path, which can be node.val, left + node.val, or right + node.val
 		return Math.max(leftNodeSum, rightNodeSum, node.val);
-	};
+	}
 
-	findSums(root);
-
-	return max;
+	findSums(root)
+    
+	return max
 }
