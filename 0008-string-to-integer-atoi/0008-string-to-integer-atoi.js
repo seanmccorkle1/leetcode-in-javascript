cc4e2603d192df24b84e2f5dc68364616ea47266
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-01-13 10:44:09 -0500

    Time: 74 ms (91.67%), Space: 43.5 MB (85.35%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.js
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.js
@@ -1,11 +1,3 @@
-/**
- Many question are incorrectly labeled, but I think less than 10 min for easy, 20 min for medium, and 40 min for hard 
- is a good strategy. How many - depend on your schedule, but I think the number isnt really important. Learning from the question youve solved is indeed more essential.
-* 
- * @param {string} 
- * @return {number}
- */
-
 var myAtoi = function (stringnum) {
 
     stringnum = stringnum.trim()
@@ -13,30 +5,29 @@ var myAtoi = function (stringnum) {
     // param is a stringnum, so "" at worst    
     // check this after the trim(), so myAtoi("    ") should return 0
 
-    if (stringnum == "" || stringnum == null)  { 
+    if (stringnum == "") { 
         return 0
     }
-
+    
     // "assume the result is positive if neither + or - is present"
     let sign = 1
-
+    
     let index = 0
-
+    
     let currNum = 0
     let firstChar = stringnum[0]
-
+    
     if (firstChar == '+') {
-        index += 1 // start at "3" in "+3154", not the "+"
+        index++
     }
-
-    // only if the first non-whitespace character is + or -
-    // -42 is valid, 42-3 is not
-
-    if (firstChar == '-') {
+    
+    // now it went from sign = 1 (initial state) to sign= - 1 
+    
+    else if (firstChar == '-') {
         sign = -1
-        index += 1
+        index++
     }
-
+    
     // "3".charCodeAt() = 51
     // String.fromCharCode(51) = "3"
 
@@ -45,38 +36,37 @@ var myAtoi = function (stringnum) {
 
     stringnum
     index
-
-
-    while (index < stringnum.length) {
     
+    while (index < stringnum.length) {
+        
         index
+        // const currNum  =  stringnum[index]
 
         let charCode = stringnum.charCodeAt(index) - 48
-
+        
+        // if its not in the range 0-9
         const charIsNotANumber = (charCode <= -1 || charCode >= 10)
-
-        // -1 and 10 are not in the range [0 - 9]    
-
+        
         if (charIsNotANumber) {
-            break // break before adding the letter
+            break   // break before adding the letter
         }
-
+        
         // convert these string digits into an INTEGER
         // "0032" -> 32
 
-        let digit = charCode
-
+        const onesDigit = charCode
+        
         // start with (0x10) + 3 = 3
         // then (3 x 10) + 1 = 31
-        // each extra digit is another place, ie x10
+        // each extra onesDigit is another place, ie x10
 
         currNum
         currNum * 10
 
         currNum
-        digit
+        onesDigit
 
-        let sum = (currNum *  10) + digit
+        let sum = (currNum *  10) + onesDigit
 
         // overwrite, dont do += or you get 3 + 31 (34)
 
@@ -87,48 +77,49 @@ var myAtoi = function (stringnum) {
 
         let max = Math.pow(2, 31) - 1
         let min = Math.pow(-2, 31)
-
+        
         // it becomes a negative at the end, 
         //right now you only look at unsigned (positive)
 
         // if num is 2147483647, its ok, its inclusive
 
         // check AFTER currNum is updated
-
+        
         if (currNum > max) {
 
             if (sign == 1) {
                 return max
             }
-
+            
             else if (sign == -1) {
                 return min
             }
         }
-
-        index += 1
+        
+        index++
     }
-
+    
     sign
-
     currNum
 
-    if (sign == -1){
+    
+    if (sign == 1){
+        return currNum
+    }
+    else if (sign == -1){
         return currNum * -1
     }
-
-    else return currNum
 }    
 
-// console.log(myAtoi("3154 with words"), 3154)
+console.log(myAtoi("3154 with words"), 3154)
 // console.log(myAtoi("+3154 with words"), 3154)
+
+// console.log(myAtoi("0032"),  32)
 
 // console.log(myAtoi("   -42"),  -42)
 
 // console.log(myAtoi("-999999999999999"),  -2147483648)
 // console.log(myAtoi("+999999999999999"),  2147483647)
-
-console.log(myAtoi("0032"),  32)
 
 // console.log(myAtoi(""),  0)
 // console.log(myAtoi("   "),  0)
