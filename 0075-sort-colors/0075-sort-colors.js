7e0467d23825ca441dd12aebc138f643fa7bcb53
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-01-13 18:32:17 -0500

    Time: 54 ms (98.64%), Space: 41.9 MB (69.68%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.js
+++ 0075-sort-colors/0075-sort-colors.js
@@ -1,70 +1,69 @@
-var sortColors = function(array) {
+var sortColors = function (array) {
 
-let leftIndex = 0
-let index = 0
-let rightIndex = array.length - 1
+    let leftIndex = 0
+    let index = 0
+    let rightIndex = array.length - 1
 
-let leftTemp
-let rightTemp
+    let leftTemp
+    let rightTemp
 
-// Swap red (0) to lefthand side
+    // Swap red (0) to lefthand side
 
-while (index <= rightIndex) {
+    while (index <= rightIndex) {
 
-let num = array[index]
-
-
-if (num == 0){
-
-leftIndex
-array
-
-leftTemp = array.at(leftIndex)
-
-// array[0] = 0 for the first one
-// all 0's go to the left
-
-array
-
-array[leftIndex] = 0
-array[index] = leftTemp
-
-array
-
-// index and leftIndex both go up by 1, this is fine
-
-leftIndex += 1
-index += 1
-}
-
-// Swap blue (2) to the right
-
-else if (num == 2) {
-rightIndex
-
-rightTemp = array.at(rightIndex)
-
-array
-
-array[rightIndex] = 2
-array[index] = rightTemp
-
-array
+        let num = array[index]
 
 
-rightIndex -= 1
+        if (num == 0) {
+
+            leftIndex
+            array
+
+            leftTemp = array.at(leftIndex)
+
+            // array[0] = 0 for the first one
+            // all 0's go to the left
+
+            array
+
+            array[leftIndex] = 0
+            array[index] = leftTemp
+
+            array
+
+            // index and leftIndex both go up by 1, this is fine
+
+            leftIndex += 1
+            index += 1
+        }
+
+        // Swap blue (2) to the right
+        else if (num == 2) {
+            rightIndex
+
+            rightTemp = array.at(rightIndex)
+
+            array
+
+            array[rightIndex] = 2
+            array[index] = rightTemp
+
+            array
 
 
-// index stays at this position, but the loop still progresses
-// because the condition is index <= rightIndex
-
-}
-
-else if (num == 1) {index++}
-}
+            rightIndex -= 1
 
 
-return array
+            // index stays at this position, but the loop still progresses
+            // because the condition is index <= rightIndex
+
+        } else if (num == 1) {
+            index += 1
+        }
+    }
+
+
+    return array
 }
 
 // console.log(sortColors([2,0,2,1,1,0]), [0,0,1,1,2,2], "sort it")
