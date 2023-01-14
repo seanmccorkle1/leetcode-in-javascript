1e46c18a95cdd118b3e90696f2e702682b30a3b1
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-01-13 23:55:53 -0500

    Time: 743 ms (28.68%), Space: 51.9 MB (93.15%) - LeetHub

---
+++ 0901-online-stock-span/0901-online-stock-span.js
@@ -0,0 +1,65 @@
+// the whole program can see priceArray now
+// by putting `let` here
+
+var priceArray;
+
+class StockSpanner {
+
+    // "let priceArray = []" would mean
+    // `priceArray` is only visible in the tiny constructor block
+
+    // have to assign values inside the constructor
+
+    constructor() {
+        priceArray = []
+    }
+
+    next(currentPrice) {
+
+        priceArray.push(currentPrice)
+
+        priceArray
+
+        let consecutiveDayCount = 0
+        let backIndex = priceArray.length - 1
+
+        // the prices are the same on the first loop
+        // [before, time1, time2, time3, present]
+        // each next() call is a newer stock (more to the right)
+
+
+        priceArray
+
+        while (backIndex >= 0) {
+
+            const previousPrice = priceArray[backIndex]
+
+            previousPrice
+            currentPrice
+            
+            if (previousPrice <= currentPrice) {
+                consecutiveDayCount += 1
+            } 
+            else if (previousPrice > currentPrice) {
+                // break
+                return consecutiveDayCount
+
+            }
+
+            backIndex -= 1
+        }
+
+        return consecutiveDayCount
+    }
+}
+const stockSpanObject = new StockSpanner()
+
+// [100,80,60,70,60,75,85]
+// console.log(stockSpanObject.next(100), 1, "no days to the left are less in [100,80,60,70,60,75,85]")
+// console.log(stockSpanObject.nextFn(80), 1)
+// console.log(stockSpanObject.nextFn(60), 1)
+// console.log(stockSpanObject.nextFn(70), 2)
+
+// console.log(stockSpanObject.nextFn(60), 1)
+// console.log(stockSpanObject.nextFn(75), 4)
+// console.log(stockSpanObject.nextFn(85), 6)
