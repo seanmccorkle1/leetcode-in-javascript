acbeb5dabef97d45e5fdc42b26b64a6ebe309ec6
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-03-03 17:26:32 -0500

    Time: 58 ms (97.14%), Space: 41.8 MB (85.14%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.js
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.js
@@ -1,44 +1,80 @@
-/**
- * 
- * @function
- * @return
- * 
- */
+// Next capital one questions:
 
-var judgeSquareSum = function(num) {
+// * Coin Change	35.5%	Medium	1.08	https://leetcode.com/problems/coin-change
+// * Add Two Numbers	33.9%	Medium	1.25	https://leetcode.com/problems/add-two-numbers
+// * Greatest Common Divisor of Strings	52.9%	Easy	0.12	https://leetcode.com/problems/greatest-common-divisor-of-strings
 
-    let leftPointer = 0
-    let rightPointer = Math.floor(Math.sqrt(num))
 
-    // there can only be 2 multiples, a and b
+// Named default parameters:
 
-    while (leftPointer <= rightPointer) {
+// var multiplyTwo =  ({num1, num2 } = {}) =>  num1*num2
+// console.log(multiplyTwo({ num1: 4, num2: 4}),  16)
 
-        let leftSquare = Math.pow(leftPointer, 2)
-        let rightSquare = Math.pow(rightPointer, 2)
 
-        const sumofSquares = leftSquare + rightSquare
+var judgeSquareSum = function (targetNum) {
 
-        // const sumofSquares = Math.pow(leftPointer, 2) + Math.pow(rightPointer, 2)
-        // const sumofSquares  = leftPointer**2 + rightPointer**2
+    let root1 = 0
 
-        if (sumofSquares == num) {
+    // start near the result
+    let root2 = Math.floor(Math.sqrt(targetNum))
+
+    if (Math.pow(root2, 2) == targetNum) {
+        return true
+    }
+
+    root1
+    root2
+
+    // keep going until the roots converge
+
+    // for 146, start from 0 and 12,
+    // slowly converge to 5 and 11
+    
+    // (1x1) +(1x1) =  2
+    // and they are <= each other
+
+    let closestDistance = Infinity
+
+    while (root1 <= root2) {
+
+        let leftSquare = Math.pow(root1, 2)
+        let rightSquare = Math.pow(root2, 2)
+
+        targetNum
+
+        root1
+        root2
+
+        const sumOfSquares = leftSquare + rightSquare
+        sumOfSquares
+
+
+        if (sumOfSquares == targetNum) {
             return true
         }
 
-        else if (sumofSquares < num) {
-            leftPointer += 1
+        // slowly go higher by turning 0 to 1
+        // dont turn 12 to 13 to go higher
+
+        else if (sumOfSquares < targetNum) {
+            root1 += 1
         }
 
-        else if (sumofSquares > num) {
-            rightPointer -=1
+        // have to change root2 too
+        else if (sumOfSquares > targetNum) {
+            root2 -= 1
         }
+
+        root1
+        root2
     }
-    
+
     return false
 }
 
-// console.log(judgeSquareSum(5),  true)
-// console.log(judgeSquareSum(144),  true)
+console.log(judgeSquareSum(2), true, "(1x1 + 1x1) => (1 + 1) => 2")
 
-console.log(judgeSquareSum(11),   false)
+// console.log(judgeSquareSum(144), true, "0 squared is (0x0)=0")
+// console.log(judgeSquareSum(146), true, "(11^2 + 5^2) = (121 + 25) = 146")
+
+// console.log(judgeSquareSum(11), false, "no 2 square roots exist that sum to 11")
