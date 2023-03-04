8733326e6e9709cd9cfa35db127daef7fec68bb4
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-03-04 15:17:53 -0500

    Time: 109 ms (46.76%), Space: 48.3 MB (41.11%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -1,3 +1,11 @@
+// Next capital one questions:
+
+// * Coin Change	35.5%	Medium	1.08	https://leetcode.com/problems/coin-change
+// * Add Two Numbers	33.9%	Medium	1.25	https://leetcode.com/problems/add-two-numbers
+// * Greatest Common Divisor of Strings	52.9%	Easy	0.12	https://leetcode.com/problems/greatest-common-divisor-of-strings
+
+
+
 var lengthOfLongestSubstring = function (string) {
 
     const obj = {}
@@ -6,43 +14,53 @@ var lengthOfLongestSubstring = function (string) {
     let rightIndex = 0
 
     // base case is 0 for "" which is length 0
-    let maxUniqueSubstringLength = 0
-    
-    while (rightIndex < string.length) {
-        let letter = string[rightIndex]
+    let longestSubstringWithNoRepeats = 0
 
+    while (rightIndex < string.length) {
+
+        const letter = string[rightIndex]
+
+        longestSubstringWithNoRepeats
 
         // if the object already has it, move left part of the window up
+        // "left part of the window" is leftIndex
+        obj
 
-        if (obj[letter] !== undefined) {
+        const isRepeatingLetter = obj[letter] != undefined
 
-           leftIndex=
-           Math.max(obj[letter] + 1,
+        if (isRepeatingLetter) {
+
+            leftIndex =
+
+                Math.max(
+                    obj[letter] + 1,
                     leftIndex)
-                }
+        }
 
-        // letter is the key, rightIndex is the value
-        // key = value
-
+        obj
         obj[letter] = rightIndex
+
 
         leftIndex
         rightIndex
 
         obj
 
-        // when rightIndex and leftIndex are both 0, its still "n", which is length 1
-        
-        maxUniqueSubstringLength = 
-        Math.max(
-            (rightIndex - leftIndex) + 1, 
-            maxUniqueSubstringLength)
-                        
+        // when rightIndex and leftIndex are both 0,
+        // the longest substring is "n", which is length 1
+
+        longestSubstringWithNoRepeats =
+            Math.max(
+                (rightIndex - leftIndex) + 1,
+                longestSubstringWithNoRepeats)
+
         rightIndex++
     }
-    return maxUniqueSubstringLength
+
+    return longestSubstringWithNoRepeats
 }
 
-console.log(lengthOfLongestSubstring("abcabcbb"), 3)
-// console.log(lengthOfLongestSubstring("bbbbbb"), 1)
-// console.log(lengthOfLongestSubstring("leetcode"), 5, "etcod")
+console.log(lengthOfLongestSubstring("abcabc"), 3)
+
+console.log(lengthOfLongestSubstring("bbbbbb"), 1)
+console.log(lengthOfLongestSubstring("leetcode"), 5, "etcod")
