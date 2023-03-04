233e676fee9de40d1d39be7128653c98816ce2d8
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-03-04 18:22:15 -0500

    Time: 63 ms (56.63%), Space: 41.7 MB (86.12%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.js
+++ 0228-summary-ranges/0228-summary-ranges.js
@@ -1,77 +1,81 @@
 var summaryRanges = function (array) {
-    
+
     const rangeArray = []
-    let leftIndex = 0
-    
+    let leftIndex = 0 // init
+
     // rightIndex <= array.length
     // start at rightIndex=1 for the difference 
-    
-    // 7+1 != undefined  = true
+
     // the if statement will always execute on the last element (undefined)
-    
+
     //  currNum is at array[index - 1]
     // in order to get the last number inside currNum, you have to do index + 1
     //[lastNum (curr)], undefined (rightNum)
-    
+
 
     for (let rightIndex = 1; rightIndex <= array.length; rightIndex++) {
-        
-        let startOfRange = array[leftIndex]        
-        let currNum = array[rightIndex - 1]
-        let endOfRange = currNum
+
+        rangeArray
+        leftIndex
+
+        let startOfRange = array[leftIndex]
+
+        let leftNum = array[rightIndex - 1]
+        let endOfRange = leftNum // helper
 
         let rightNum = array[rightIndex]
-        
-        startOfRange
-        currNum
-        rightNum
 
+        startOfRange
+        leftNum
+        rightNum
 
         // rightNum and currNum converge on the last loop
         // this if statement will (maybe) always execute on the last loop
         // at least its [7, undefined] for the first one
-        
-        
+
         // the last number will always either be the end of a range 
         // or it will be its own range
-        
-        currNum + 1
-        rightNum
-        
-        if ((currNum + 1) != rightNum) {
 
-            currNum
+        const discontinuityFound = (leftNum + 1) != rightNum
+
+        if (discontinuityFound) {
+
+            leftNum
             rightNum
-            
-            // let endOfRange = currNum
-            
-            // if (array[leftIndex ] != array[rightIndex - 1])
+
+            startOfRange
+            endOfRange
             
             if (startOfRange != endOfRange) {
-                rangeArray.push(startOfRange+"->" + endOfRange)
+                rangeArray.push(`${startOfRange}->${endOfRange}`)
             }
-            
+
             // 7 == 7, its just one number (7)
             
             else if (startOfRange == endOfRange) {
-                rangeArray.push(currNum.toString())
+                rangeArray.push(leftNum.toString())
                 rangeArray
             }
-            
-            // store the rightIndex of `rightNum` in leftIndex
-            // only do this when the non-nested if statement executes
-            
-            rangeArray
-            
-            leftIndex = rightIndex            
+
+            rightIndex
+
+            // only update leftIndex when theres a discontinuity
+            // this changes `startOfRange` too, because (startOfRange = array[leftIndex])
+
+            leftIndex
+
+            // jump to 4 after "0->2"
+            // you dont need 2 anymore
+
+            leftIndex = rightIndex
+            leftIndex
         }
+
     }
-    
+
     return rangeArray
 }
 
+console.log(summaryRanges([0, 1, 2, 4, 5, 7]), ["0->2", "4->5", "7"], "break at 3 and at 6, total of 3 ranges. 7 is alone at the end")
 
-console.log(summaryRanges([0, 1, 2, 4, 5, 7]), ["0->2", "4->5", "7"], "break at 3 and at 6, total of 3 ranges. 7 is left alone at the end")
-
-
-// console.log(summaryRanges([0,2,3,4,6,8,9]), ["0","2->4","6","8->9"])
+// console.log(summaryRanges([0, 2, 3, 4, 6, 8, 9]), ["0","2->4","6","8->9"])
