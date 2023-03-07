d231b57f049779a2f38fa9f8ffc2ae1e3f3ae7a6
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-03-07 18:51:14 -0500

    Time: 73 ms (41.20%), Space: 44.9 MB (27.81%) - LeetHub

--- 0443-string-compression/0443-string-compression.js
+++ 0443-string-compression/0443-string-compression.js
@@ -1,6 +1,5 @@
 var compress = function (array) {
 
-
     let deleteCount = 0
     // let stringDigitArray = []
 
@@ -11,7 +10,9 @@ var compress = function (array) {
     // "a" automatically counts as length 1, not 0
     let adjacentCount = 1
 
-    for (let backIndex = array.length - 2; backIndex >= 0; backIndex -= 1) {
+
+    for (let backIndex = array.length - 2; backIndex >= 0; backIndex--) {
+
 
         array
 
@@ -20,40 +21,46 @@ var compress = function (array) {
 
         const numsAreEqualAndAdjacent = (leftNum == rightNum)
 
+
         if (numsAreEqualAndAdjacent) {
             adjacentCount += 1
         }
 
+
         // adjacentCount starts at 1 because a single letter "b" is already length 1, 
         // ["z"] instead of ["z", 1]
-
         else if (!numsAreEqualAndAdjacent) {
 
             // only if the letters are unequal and (count >= 2)
 
             if (adjacentCount >= 2) {
 
+
                 deleteCount = adjacentCount
                 startDeleteIndex = backIndex + 2
-                
+
                 // split to get the digits ["1", "2"] instead of ["12"]
-                
+
+
                 if (adjacentCount >= 10) {
-                    array.splice(startDeleteIndex, deleteCount - 1, ...String(adjacentCount))    
+
+
+                    // stringDigitArray = String(adjacentCount).split("")
+                    array.splice(startDeleteIndex, deleteCount - 1, ...String(adjacentCount))
+
                     array
                 }
-            
-                // [backIndex  + 2) to start deleting at the SECOND "b"
-                // result is ["b", "1", "2"] instead of ["1", "2", "b"]
-                
+
+                // (backIndex  + 2) to start deleting at the SECOND "b
+                // to get ["b", "1", "2"] instead of ["1", "2", "b"]
                 else if (adjacentCount <= 9) {
                     array.splice(startDeleteIndex, deleteCount - 1, String(adjacentCount))
                 }
-                
+
                 adjacentCount = 1
             }
         }
-        
+
     }
 
     // if it repeats on the last loop
@@ -62,22 +69,29 @@ var compress = function (array) {
     adjacentCount
 
     array
-
+    
+    // dont count 1 letter as ["b", 1] 
     if (adjacentCount >= 2) {
 
         deleteCount = adjacentCount
 
         if (adjacentCount >= 10) {
+            // stringDigitArray = String(adjacentCount).split("")
             array.splice(1, deleteCount - 1, ...String(adjacentCount))
         }
-
-        else if (adjacentCount <= 9) {
-            array.splice(1, deleteCount - 1, String(adjacentCount))
-        }    
-    }
     
+        else if (adjacentCount <= 9) {
+            array.splice(1, deleteCount - 1, ...String(adjacentCount))
+        }
+    }
+
     adjacentCount
     array
-    
+
     return array.length
 }
+
+console.log(compress(["a", "a", "b", "b", "b", "b", "b", "b", "b", "b", "b", "b", "b", "b", "z"]), 6,
+    `array should be modified to ['a', '2', 'b', '1', '2', 'z'] which is length 6. b12 becomes b 1 2`)
+
+// console.log(compress(["a"]), 1)
