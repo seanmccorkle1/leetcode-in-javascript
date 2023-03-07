b71e1c0cf9f05aecbb34511211fa1c4c3756783a
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-03-07 15:55:31 -0500

    Time: 63 ms (58.24%), Space: 41.9 MB (53.85%) - LeetHub

--- 2427-number-of-common-factors/2427-number-of-common-factors.js
+++ 2427-number-of-common-factors/2427-number-of-common-factors.js
@@ -1,29 +1,26 @@
 var commonFactors = function (num1, num2) {
 
-    num1
-    num2
-
     let factorCount = 0
-    
+
     // only go to the lower of the 2 numbers
     const lowerNum = Math.min(num1, num2)
 
-    // index <= lowerNum, 0 isnt a factor
+    // index <= lowerNum
 
     for (let index = 1; index <= lowerNum; index++) {
-        
-        let quotient1 = num1 / index
-        let quotient2 = num2 / index
+        index
 
-        const commonFactorFound = Number.isInteger(quotient1) && Number.isInteger(quotient2)
+        // num1/index
+        // num2/index
 
-        if (commonFactorFound) {
-            factorCount += 1
+        if (Number.isInteger(num1/index) && Number.isInteger(num2/index)){
+            factorCount++
         }
     }
 
     return factorCount
 }
 
-console.log(commonFactors(12, 24), 6, "[1, 2, 3, 4, 6, 12] are the 6 common factors of both numbs")
-// console.log(commonFactors(1, 6),  1,  "1 is the only common factor")
+console.log(commonFactors(12, 24), 6, "[1, 2, 3, 4, 6, 12] are the 6 common factors of both nums")
+
+// console.log(commonFactors(1, 6), 1, "1 is the only common factor")
