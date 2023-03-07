652aac46f60469f840425c7b4314b16777f7f660
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-03-07 18:38:44 -0500

    Time: 82 ms (89.32%), Space: 46.4 MB (27.78%) - LeetHub

--- 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.js
+++ 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.js
@@ -1,37 +1,48 @@
-var answerQueries = function (inputArray, limitNums) {
 
+
+var answerQueries = function(inputArray, limitNums) {
+    
     const numCounts = []
+    const sumsReference = []
 
-    inputArray.sort((a, b) => a - b)
-
-    for (const limitNum of limitNums) {
-
-        // reset 
+    inputArray.sort((a,b)=>a - b)
+    
+    for (const limitNum of limitNums){
+        
         let sum = 0
         let numCount = 0
 
-
-        for (const num of inputArray) {
-
-            if ((sum + num) <= limitNum){
+        for (let num of inputArray) {
+            
+            const sumFitsInLimit = (sum + num) <= limitNum
+            
+            if (sumFitsInLimit) {
                 sum += num
-                numCount++
+                numCount++   // add the number to the count
             }
             
-            else {
-                numCounts.push(numCount)
-                break
-            }
-
-            if (numCount == inputArray.length){
-                numCounts.push(numCount)
+            // only 1 numCount should be pushed per limitNum
+            // break to go straight to the next limitNum
+            
+            else if (!sumFitsInLimit){
+                break 
             }
         }
-
+        sum
+        numCount
+        
+        sumsReference.push(sum)
+        numCounts.push(numCount)
     }
 
+    // there should be one count in the array per limitNum
+
+sumsReference 
+
+   
     return numCounts
 }
+
 
 console.log(answerQueries([4, 5, 2, 1], [3, 10, 21]), [2, 3, 4], 
 "2 + 1 is <= 3, 2 elements fit in, 3 nums fit in 10, all 4 numbers fit in 21.")
