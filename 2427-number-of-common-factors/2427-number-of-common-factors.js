00872355ae81d976273398da424a017eb4581359
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-03-07 15:55:53 -0500

    Time: 60 ms (73.26%), Space: 42.5 MB (5.86%) - LeetHub

--- 2427-number-of-common-factors/2427-number-of-common-factors.js
+++ 2427-number-of-common-factors/2427-number-of-common-factors.js
@@ -7,7 +7,7 @@ var commonFactors = function (num1, num2) {
 
     // index <= lowerNum
 
-    for (let index = 1; index <= lowerNum; index++) {
+    for (let index = 0; index <= lowerNum; index++) {
         index
 
         // num1/index
