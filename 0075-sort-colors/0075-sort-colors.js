084cc962945ea73a6027d02d7a32e4825b92eda0
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-03-07 16:01:58 -0500

    Time: 73 ms (20.57%), Space: 42.1 MB (53.76%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.js
+++ 0075-sort-colors/0075-sort-colors.js
@@ -3,26 +3,24 @@ var sortColors = function (array) {
     let leftIndex = 0
     let index = 0
     let rightIndex = array.length - 1
-
-    let leftTemp =0
-    let rightTemp =0
+    
+    let leftTemp = 0
+    let rightTemp = 0
 
     // Swap red (0) to lefthand side
 
+
     while (index <= rightIndex) {
 
-        let num = array[index]
+        const num = array[index]
         
         if (num == 0) {
 
             leftIndex
+
             array
-            
+
             leftTemp = array[leftIndex]
-
-            // array[0] = 0 for the first one
-            // all 0's go to the left
-
             array
 
             array[leftIndex] = 0
@@ -32,42 +30,37 @@ var sortColors = function (array) {
 
             // index and leftIndex both go up by 1, this is fine
 
-            leftIndex += 1
-            index += 1
+            leftIndex++
+            index++
         }
 
-        // move 2 to the right
-
+        // Swap blue (2) to the right
         else if (num == 2) {
-            
             rightIndex
-            
             rightTemp = array[rightIndex]
-            
+
             array
 
             array[rightIndex] = 2
             array[index] = rightTemp
-            
-            array
-            
-            rightIndex -= 1
 
+            array
+
+            rightIndex--
 
             // index stays at this position, but the loop still progresses
             // because the condition is index <= rightIndex
+        }
 
-        } 
-        
-        // leftIndex doesnt go up
-
+        // dont increment index on every loop, only if its 1
         else if (num == 1) {
-            index += 1
+            index++
         }
     }
-    
+
+
     return array
 }
 
-console.log(sortColors([2, 0, 1]), [0, 1, 2], "2 has to go from the front to back")
 // console.log(sortColors([2,0,2,1,1,0]), [0,0,1,1,2,2], "sort it")
+console.log(sortColors([2, 0, 1]), [0, 1, 2])
