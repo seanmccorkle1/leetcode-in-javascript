b307d129fec26afd7a64a3065683981c23821fab
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-03-07 16:41:20 -0500

    Time: 68 ms (62.50%), Space: 45.1 MB (5.09%) - LeetHub

--- 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.js
+++ 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.js
@@ -1,11 +1,1 @@
-
-
-var nextGreatestLetter = (letterArray, targetLetter)=> {
-let n=Math.min(...letterArray.map(letter => letter.charCodeAt()).filter(charCode=>charCode > targetLetter.charCodeAt()))
-
-return n == Infinity? letterArray[0]: String.fromCharCode(n)
-
-}
-
-console.log(nextGreatestLetter(["c", "b"], "a"), "b", "the smallest letter thats greater than `a` in the array is `b`")
-console.log(nextGreatestLetter(["m", "c"], "z"), "x", "return letterArray[0] since theres no letter greater than z")
+var nextGreatestLetter = (a,t)=>Math.min(...a.map(l => l.charCodeAt()).filter(cc => cc > t.charCodeAt())) != Infinity ? String.fromCharCode(Math.min(...a.map(l => l.charCodeAt()).filter(cc => cc > t.charCodeAt()))):a[0]
