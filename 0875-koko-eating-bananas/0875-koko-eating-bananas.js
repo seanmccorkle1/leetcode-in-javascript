59bd8bc327ceebc5b931d54723422b390dd36434
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-03-07 19:07:02 -0500

    Time: 97 ms (37.16%), Space: 45.2 MB (51.58%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.js
@@ -0,0 +1,19 @@
+const minEatingSpeed = (piles, h) => {
+    let min = 1,
+        max = Math.max(...piles),
+        best = max
+
+    const time = speed => piles.reduce((sum, pile) => sum + Math.ceil(pile/speed), 0)
+
+    while (min <= max) {
+        const mid = Math.floor((min + max) / 2)
+		
+        if (time(mid) <= h) {
+            best = mid
+            max = mid - 1
+        } else
+            min = mid + 1
+    }
+
+    return best
+}
