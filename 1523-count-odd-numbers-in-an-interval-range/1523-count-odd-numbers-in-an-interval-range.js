820dd23b7a4d29144fee05ad9ab924be088bd9b8
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-03-12 09:49:51 -0400

    Time: 65 ms (72.37%), Space: 42.3 MB (8.34%) - LeetHub

--- 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.js
+++ 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.js
@@ -1,27 +1,24 @@
-var countOdds =(lowNum, highNum)=> {
-    lowNum%2==0 ? lowNum+=1:"a"
-    highNum % 2==0 ? highNum-=1 : "a"
-    return (highNum-lowNum)/2+1
+var countOdds = function (lowNum, highNum) {
+
+    const lowNumIsEven = lowNum % 2 == 0
+    const highNumIsEven = highNum % 2 == 0
+
+    // count odd numbers, so make it odd
+
+    if (lowNumIsEven) {
+        lowNum = lowNum + 1
+    }
+
+    if (highNumIsEven) {
+        highNum = highNum - 1
+    }
+
+    let range = highNum-lowNum
+
+    return range/2 + 1
+
 }
 
-// const lowNumIsEven = (lowNum % 2 == 0) ?true:false
-//     const highNumIsEven = highNum % 2 == 0
-
-//     // lowNum going up keeps it in the range
-//     if (lowNumIsEven) {
-//         lowNum = lowNum + 1
-//     }
-
-//     // highNum going down keeps it in the range
-//     if (highNumIsEven) {
-//         highNum = highNum - 1
-//     }
-//     let range = highNum - lowNum
-
-//     return range/2+1
-// }
-
-// console.log(countOdds(3, 7),  3, "there are 3 odd numbers in the range [3, 5, 7]")
-// console.log(countOdds(3, 6),   2, [3, 5])
-
-// // console.log(countOdds(8, 10), 1, "9 is the only odd in [8, 9, 10]")
+console.log(countOdds(3, 7), 3, [3, 5, 7], "range is 5, odd")
+console.log(countOdds(3, 6), 2, [3, 5], "range is even 4")
+console.log(countOdds(8, 10), 1, "9 is the only odd in [8, 9, 10]")
