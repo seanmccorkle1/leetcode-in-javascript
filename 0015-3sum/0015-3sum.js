ab14d700a449e3a4792982dc06da9419bab5f00b
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-03-14 12:24:43 -0400

    Time: 192 ms (52.60%), Space: 56.9 MB (34.23%) - LeetHub

--- 0015-3sum/0015-3sum.js
+++ 0015-3sum/0015-3sum.js
@@ -7,7 +7,7 @@ var threeSum = function(array) {
 
     outerFrontIndexLoop:
 
-    for (let frontIndex = 0; frontIndex < array.length; frontIndex += 1) {
+    for (let frontIndex = 0; frontIndex < array.length; frontIndex++) {
 
         frontIndex
         array
