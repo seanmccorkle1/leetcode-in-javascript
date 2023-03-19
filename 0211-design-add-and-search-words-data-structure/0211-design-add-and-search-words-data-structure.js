b5c5da8503a8345b182ecd73a8c46d2f82be741a
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-03-18 20:10:09 -0400

    Time: 2729 ms (23.48%), Space: 105.1 MB (22.71%) - LeetHub

--- 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.js
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.js
@@ -47,43 +47,57 @@ WordDictionary.prototype.addWord = function(word) {
 
 // This is the main recursive function where all of the magic is happening
 
-
 WordDictionary.prototype.search = function(word) {
     
     let node = this.root;
     function rec(node, word){
-	
+        
 		// returns false if the node doesn't exist
         if(node ==undefined) return false;
 		
 		// if the word exist
         if(word){
 		
-			//skipping any calculation if the word[0] is "."
+			// skipping any calculation if the word[0] is "."
             
-			if (word[0]== '.') {
-				
-				// Since we don't know what should be the next node so, need to go through all of them one by one
-                let out = false;
-                for(let val of node.keys.keys()){
-					// Any one of the nodes will return true for the upcoming character in word after .
-					// So, taking an OR operation
-                    out = out || rec(node.keys.get(val), word.substr(1));
+            // Since we don't know what should be the next node, 
+            // we need to go through all of them one by one
+            
+			if (word[0]== ".") {
+                
+                let out = false
+                
+                // Any one of the nodes will return true for the upcoming character in word after .
+                // So, taking an OR operation
+                
+                for (let val of node.keys.keys()){
+                    
+                    if (!out){
+                        out = rec(node.keys.get(val), word.substr(1))
+                    }
+                    
+                    else {
+                        out = out
+                    }
+                    // out = out || rec(node.keys.get(val), word.substr(1))
                 }
                 return out;
             }
-            else if(node.keys.has(word[0])){
+            else if (node.keys.has(word[0])){
 				// If the node has encountered a word then the simple stuff
                 return rec(node.keys.get(word[0]), word.substr(1));
             }
-			// otherwise return false when the above two are false
-            else{ return false}
+
+            // otherwise return false when the above two are false
             
+            else{
+                return false
+            }
         }
 		// If the word doesn't exist but it could have existed so just checking if the node is the end node and returning the result
         else return node.isEnd();
     }
-	
+    
     return rec(node, word);
 }
 
