e09b03ef4548d3f868e53fedb0661320f572bd79
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-03-23 16:54:48 -0400

    Time: 81 ms (73.27%), Space: 43.3 MB (81.68%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.js
+++ 0016-3sum-closest/0016-3sum-closest.js
@@ -1,4 +1,5 @@
 var threeSumClosest = function (array, targetNum) {
+
     // start it at "not close at all"
     let closestThreeSum = Infinity
 
@@ -11,32 +12,30 @@ var threeSumClosest = function (array, targetNum) {
         let leftIndex = index + 1
         let rightIndex = array.length - 1
 
+        // cant use the same-position number twice in the three-sum
+        // so dont look at the same final element
+
         index
         leftIndex
         rightIndex
 
         while (leftIndex < rightIndex) {
 
-            let num1=array[leftIndex]
+            let num1 = array[leftIndex]
             let num2 = array[index]
             let num3 = array[rightIndex]
 
             // -3 is just more negative so it whens the < condition
             // its not closer to the target
 
-            let localThreeSum =num1+num2+num3 
-            
-            // early return
-            if (localThreeSum == targetNum){
-                return targetNum
-            }
+            let localThreeSum = num1 + num2 + num3
 
             localThreeSum
             closestThreeSum
             targetNum
 
             let currDistance = Math.abs(localThreeSum - targetNum)
-            let closestDistance= Math.abs(closestThreeSum - targetNum)
+            let closestDistance = Math.abs(closestThreeSum - targetNum)
 
             currDistance
 
@@ -50,16 +49,19 @@ var threeSumClosest = function (array, targetNum) {
             if (localThreeSum > targetNum) {
                 rightIndex--
             } 
-
+            
             else if (localThreeSum < targetNum) {
                 leftIndex++
             }
-            
+            else if (localThreeSum == targetNum){
+                return localThreeSum||targetNum
+            }
+
         }
     }
-    
+
     return closestThreeSum
 }
 
-// console.log(threeSumClosest([3, 2, 1, 5, 4, 1, 40], 5), "{result here}")
-console.log(threeSumClosest([-1, 2, 1, -4], 1), 2)
+console.log(threeSumClosest([3, 2, 1, 5, 4, 1, 40], 5), 5, `[1, 1, 3] is 5, the exact target num was found`)
+// console.log(threeSumClosest([-1, 2, 1, -4], 1),  2, "[-1, 2, 1] =  2 and is the closest possible sum to target 1")
