eda8c5bb1ef2076cfc3f19cc7384f5a2faf70591
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-03-23 13:56:29 -0400

    Time: 77 ms (15.15%), Space: 44.8 MB (15.01%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.js
+++ 0605-can-place-flowers/0605-can-place-flowers.js
@@ -1,65 +1,69 @@
-var canPlaceFlowers = function (array, targetCount) {
-    
-     let count = 0
-     
+var canPlaceFlowers = function (array, numOfFlowersToPlace) {
+
+    let flowerCount = 0
+
+
     for (let index = 0; index < array.length; index++) {
 
-        const currNum = array[index] 
-        
+        if (flowerCount >=numOfFlowersToPlace){
+            return true
+        }
+
         const numToTheRight = array[index + 1]
         const numToTheLeft = array[index - 1]
-        
-        const emptySpotFound = currNum !=1
-        
+
+        const emptySpotFound = array[index] == 0
+        const numIsAtTheFront = index==0 ? true : false
+
         if (emptySpotFound) {
-            
-            if (array[index - 1] === 0 && array[index + 1] === 0) {
-                array[index] = 1
-                    count++
+
+            if (numToTheLeft == 0 && numToTheRight == 0) {
+                array[index] = 1 //plant
+                flowerCount++
             }
-            
-            if (index == 0 && numToTheRight == 0) {
+
+            else if (numIsAtTheFront && numToTheRight == 0) {
                 array[index] = 1
-                count++
+                flowerCount++
             }
-            
-            if (index == 0 && array.length == 1) {
+
+            // for one-length arrays like [0]
+            // it only gets here with emptySpotFound
+
+            else if (numIsAtTheFront && array.length == 1) {
                 array[index] = 1
-                count++
+                flowerCount++
             }
-        
-            
-//             if (index == 0) {
-            
-//                 // [0, 0] or 
-                
-//                 if (numToTheRight == 0 || index == array.length - 1) {
-//                     array[index] = 1
-//                     count++
-//                 }
-//             }
-    
-            
-            if (index == array.length - 1) {
-                
-                if (array[index - 1] == 0) {
+
+            else if (index == array.length - 1) {
+
+                if (numToTheLeft == 0) {
                     array[index] = 1
-                    count++
+                    flowerCount++
                 }
             }
         }
 
     }
-    count
-    targetCount
+
+    flowerCount
+    numOfFlowersToPlace
+
+    array
+
+    if (flowerCount > numOfFlowersToPlace || flowerCount == numOfFlowersToPlace) {
+        return true
+    } 
     
-     if (count > targetCount || count == targetCount){
-         return true
-     }
-    
-     else if (count < targetCount) {
-         return false
-     }
+    else if (flowerCount < numOfFlowersToPlace) {
+        return false
+    }
 }
 
-console.log(canPlaceFlowers([0], 1), true, "1 flower can be planted")
+console.log(canPlaceFlowers([1, 0, 0, 0, 0], 2), true, [1, 0, [1], 0, [1]], "the new 1's are the 2 flowers")
+
+// console.log(canPlaceFlowers([1, 0, 0, 0, 1], 20), false, "cant plant")
+
+// console.log(canPlaceFlowers([1,0,0,0,1], 1), true,"1 flower can be planted in the middle")
+
+// console.log(canPlaceFlowers([1, 0, 0, 0, 1], 2), false, "2 flowers cannot be planted")
