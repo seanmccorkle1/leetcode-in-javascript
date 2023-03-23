3f379e62fa24626cc673892afac0afaa6d2b5b3f
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-03-23 13:48:28 -0400

    Time: 62 ms (77.57%), Space: 44 MB (78.65%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.js
+++ 0605-can-place-flowers/0605-can-place-flowers.js
@@ -1,26 +1,46 @@
- var canPlaceFlowers = function (array, targetCount) {
+var canPlaceFlowers = function (array, targetCount) {
+    
      let count = 0
-
+     
     for (let index = 0; index < array.length; index++) {
+        
+        const numToTheRight = array[index + 1]
+        const numToTheLeft = array[index - 1]
 
-        if (array[index] === 0) {
-
-
+        if (array[index] == 0) {
+            
             if (array[index - 1] === 0 && array[index + 1] === 0) {
                 array[index] = 1
-                    ++count
+                    count++
             }
             
-            if (index == 0) {
-                
-                if (array[index + 1] === 0 || index === array.length - 1) {
-                    array[index] = 1
-                    ++count
-                }
+            if (index == 0 && numToTheRight == 0) {
+                array[index] = 1
+                count++
             }
+            
+            if (index == 0 && array.length == 1) {
+                array[index] = 1
+                count++
+            }
+        
+            
+//             if (index == 0) {
+                
+//                 const numToTheRight = array[index+1]
+
+//                 // [0, 0] or 
+                
+//                 if (numToTheRight == 0 || index == array.length - 1) {
+//                     array[index] = 1
+//                     count++
+//                 }
+//             }
+    
             
             if (index == array.length - 1) {
-                if (array[index - 1] === 0 ){//||index === 0){
+                
+                if (array[index - 1] == 0) {
                     array[index] = 1
                     count++
                 }
@@ -35,5 +55,5 @@
          return true
      }
      
-     return false
+     else if (count < targetCount) {return false}
 }
