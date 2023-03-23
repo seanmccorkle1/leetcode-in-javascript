8511b6f9be8b369d75c7a0d60afc139f444d056c
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-03-23 14:06:01 -0400

    Time: 80 ms (10.27%), Space: 44.9 MB (12.30%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.js
+++ 0605-can-place-flowers/0605-can-place-flowers.js
@@ -1,14 +1,20 @@
 var canPlaceFlowers = function (array, numOfFlowersToPlace) {
 
+
     let flowerCount = 0
 
+
     for (let index = 0; index < array.length; index++) {
+
+
 
         const numToTheRight = array[index + 1]
         const numToTheLeft = array[index - 1]
 
+
         const emptySpotFound = array[index] == 0
         const numIsAtTheFront = index==0 ? true : false
+
 
         if (emptySpotFound) {
 
@@ -17,21 +23,20 @@ var canPlaceFlowers = function (array, numOfFlowersToPlace) {
                 flowerCount++
             }
 
+            // currNum is 0 in all of these
             else if (numIsAtTheFront && numToTheRight == 0) {
                 array[index] = 1
                 flowerCount++
             }
 
             // for one-length arrays like [0]
-            // it only gets here with emptySpotFound
-
             else if (numIsAtTheFront && array.length == 1) {
                 array[index] = 1
                 flowerCount++
             }
 
             else if (index == array.length - 1) {
-                
+
                 if (numToTheLeft == 0) {
                     array[index] = 1
                     flowerCount++
@@ -39,12 +44,17 @@ var canPlaceFlowers = function (array, numOfFlowersToPlace) {
             }
         }
 
-        if (flowerCount >=numOfFlowersToPlace){
+        // not needed, but conditions that you want to check on every loop
+        // should be at the bottom
+
+        // one-time conditions should be at the top
+
+        if (flowerCount == numOfFlowersToPlace){
             return true
         }
+
     }
-    return false
-    
+
     flowerCount
     numOfFlowersToPlace
 
@@ -52,11 +62,16 @@ var canPlaceFlowers = function (array, numOfFlowersToPlace) {
 
     if (flowerCount > numOfFlowersToPlace || flowerCount == numOfFlowersToPlace) {
         return true
-    }
-    else if (flowerCount < numOfFlowersToPlace) {
+    }    
+
+    // if flowerCount is less than targetCount    
+
+    else {
         return false
     }
+
 }
+
 
 console.log(canPlaceFlowers([1, 0, 0, 0, 0], 2),  true,  [1, 0, [1], 0, [1]], "the new 1's are the 2 flowers")
 // console.log(canPlaceFlowers([1, 0, 0, 0, 1], 3), false, "3 new flowers cannot be planted here")
