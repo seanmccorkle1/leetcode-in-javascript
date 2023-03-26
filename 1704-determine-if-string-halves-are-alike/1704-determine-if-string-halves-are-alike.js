4c8bb06201d9f38b37b92f9d3a4e9e9e72058362
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-03-26 11:44:37 -0400

    Time: 61 ms (87.41%), Space: 44.1 MB (27.97%) - LeetHub

---
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.js
@@ -0,0 +1,34 @@
+var halvesAreAlike = function (string) {
+
+
+    let count = 0
+    let half = string.length / 2
+    // let containsVowel = /[aeiou]/gi.test(string)
+    
+    for (let index = 0; index < string.length; index++) {
+
+        const letter = string[index]
+        const containsVowel = /[aeiou]/gi.test(letter)
+
+        if (index < half && containsVowel) {
+            count++
+        }
+
+
+        // without >= it would be false for both 2 < 2 and 2 > 2
+        else if (index >= half && containsVowel) {
+            count--
+        }
+        else {count}
+    }
+    count
+
+
+    if (count == 0) {
+        return true
+    }
+    else return false
+}
+
+// console.log(xd("book"), true, "bo, ok, both have 1 vowel")
+// console.log(xd("bookss"), "boo kss -> oo vs nothing")
