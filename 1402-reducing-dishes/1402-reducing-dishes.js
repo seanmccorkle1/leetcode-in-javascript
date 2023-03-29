c18d1562ee5a2c40795c721e926a8df09cae9fda
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-03-28 21:27:43 -0400

    Time: 74 ms (35.37%), Space: 42 MB (81.71%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.js
+++ 1402-reducing-dishes/1402-reducing-dishes.js
@@ -1,10 +1,9 @@
 var maxSatisfaction = function (array) {
 
     let maxNum = 0
-
-    // let term2 = 0
+    // let multiplierThatStartsFrom1= 0
     // let currSumOfProducts = 0 
-
+    
     array.sort((a, b) => a - b)
 
     for (let index = 0; index < array.length; index++) {
@@ -21,8 +20,9 @@ var maxSatisfaction = function (array) {
             array
 
             const currNum = array[index2]
-            let term2 = (index2 - index) + 1
-            const currProduct = currNum * term2
+            let startFrom1Index = (index2 - index) + 1
+
+            const currProduct = currNum * startFrom1Index
 
             currSumOfProducts += currProduct
         }
@@ -33,17 +33,19 @@ var maxSatisfaction = function (array) {
         if (currSumOfProducts > maxNum) {
             maxNum = currSumOfProducts
 
-            if (array[0] >= 0){
+            // if theres no negatives, this will be the highest
+
+            if (array[0] >= 0) {
                 return maxNum
             }
 
-        }        
+        }
     }
 
     return maxNum
 }
 
-// console.log(maxSatisfaction([2, 3, 4]), 20)
-console.log(maxSatisfaction([0, 2, 3, 4]), 29, "0x1 + 2x2 + 3x3 + 4x4 = (16 + 4  + 2) = 29")
+// console.log(maxSatisfaction([2, 3, 4]), 20, "early return this")
+// console.log(maxSatisfaction([0, 2, 3, 4]), 29, "0x1 + 2x2 + 3x3 + 4x4 = (16 + 4  + 2) = 29, start from (Nx1) every time")
 
-// console.log(maxSatisfaction( [-1,-8,0,5,-9]))
+console.log(maxSatisfaction([-8, -2, 2, 5]),   17, "-2x1 + 2x2 + 5x3 = (-2 + 4 + 15) = 17")
