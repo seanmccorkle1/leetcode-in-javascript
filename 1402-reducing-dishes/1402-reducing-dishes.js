57964c7682eeceebc4e641a4a8700cf56f77ad22
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-03-28 21:11:06 -0400

    Time: 61 ms (82.93%), Space: 43.1 MB (31.71%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.js
+++ 1402-reducing-dishes/1402-reducing-dishes.js
@@ -1,22 +1,62 @@
-var maxSatisfaction = function(s) {
-    
-    s.sort((a,b)=> a - b)
-    
-    let result = 0
-    let len = s.length
-    
-    for (let i = 0; i < len; i++) {
-        
-        let current = 0
-        
-        for (let j = i; j < len; j++) {
-            current += (s[j] * (j - i + 1))
+var maxSatisfaction = function (array) {
+
+    array.sort((a, b) => a - b)
+
+    let maxNum = 0
+
+    let find = array.find(e => e<=-1)
+
+    array
+
+    if (find == undefined){
+
+        let index = 0
+
+        while (index < array.length){
+            array[index] = (array[index] * (index + 1))
+            index++
         }
+
+        array
+        return array.reduce((s,c)=>s+c,0)
+    }
+    
+
+    for (let index = 0; index < array.length; index++) {
+
+        array
+        let currSumOfProducts = 0
+
+        index
+
+
+        for (let index2 = index; index2 < array.length; index2++) {
         
-        if (current > result) {
-            result = current
+            array
+
+            const currNum = array[index2]
+    
+            let term2 = (index2 - index) + 1
+
+            // [2, 3, 4]
+            // [3, 4]
+            // [4]
+
+            const currProduct = currNum * term2
+            currSumOfProducts += (currProduct)
+        }
+
+        currSumOfProducts
+
+        if (currSumOfProducts > maxNum) {
+            maxNum = currSumOfProducts
         }
     }
-    return result
+
+    return maxNum
 }
 
+// console.log(maxSatisfaction([2, 3, 4]), 20)
+console.log(maxSatisfaction([4, 3, 2, 0]), 29, "0x1 + 2x2 + 3x3 + 4x4 = (16 + 4  + 2) = 29")
+
+// console.log(maxSatisfaction( [-1,-8,0,5,-9]))
