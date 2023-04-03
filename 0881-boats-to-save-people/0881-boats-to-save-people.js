089cd8799e92c7749239a1e18bcc5f93ce040d47
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-04-02 20:01:41 -0400

    Time: 153 ms (88.93%), Space: 50.3 MB (22.90%) - LeetHub

---
+++ 0881-boats-to-save-people/0881-boats-to-save-people.js
@@ -0,0 +1,11 @@
+var numRescueBoats = function(people, limit) {
+    people = people.sort((a, b) => b - a);
+    let i = 0, j = people.length-1;
+    let count = 0;
+    while (i <= j) {
+        if (people[i] + people[j] <= limit) j--;
+        i++;
+        count++;
+    }
+    return count;
+};
