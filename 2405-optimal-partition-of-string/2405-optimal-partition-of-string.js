e3c28396ce4dbd1ffba63a996c897c64e8a8e70b
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-04-03 21:53:31 -0400

    Time: 101 ms (80.17%), Space: 52.3 MB (23.14%) - LeetHub

--- 2405-optimal-partition-of-string/2405-optimal-partition-of-string.js
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.js
@@ -1,15 +1,20 @@
-
 var partitionString = function (string) {
 
     let leftIndex = 0
+
+    // 0 or 1 doesnt matter    
     let rightIndex = 1
-    
+
     let numOfPartitions = 0
     const partitions = []
 
-    while (rightIndex < string.length) {
 
+    while (rightIndex <= string.length) {
+
+        // starts at string[1]
         const currLetter = string[rightIndex]
+
+        string[8]
         let stringPartition = string.slice(leftIndex, rightIndex)
 
         currLetter
@@ -37,10 +42,11 @@ var partitionString = function (string) {
     partitions.push(string.slice(leftIndex, rightIndex))    
     partitions
 
+    // always +1 partition for the end of a word
     return ++numOfPartitions
 }
 
-// console.log(partitionString("newstring"), 2, "['newstri', 'ng']")
-// console.log(partitionString("newstrinn"), 3, "['newstri', 'n', 'n']")
+console.log(partitionString("newstring"), 2, "['newstri', 'ng']")
+console.log(partitionString("newstrinn"), 3, "['newstri', 'n', 'n']")
 
 console.log(partitionString("sss"), 3, "[s,s,s]")
