27c6ac938d83350eb1fe000a80f4f34c68ae7972
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-04-08 15:06:24 -0400

    Time: 326 ms (63.97%), Space: 72 MB (83.64%) - LeetHub

--- 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.js
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.js
@@ -1,55 +1,61 @@
-var successfulPairs = function(spells, potions, successNum) {
+var successfulPairs = function (spells,potions,successNum
+) {
 
-    const successOfSpellsArray = []
+    const successOfSpellsArray = new Array(spells.length)
+
 
     // binary search needs a sort
     potions.sort((a, b) => a - b)
-    
+
+
+
     spells
     potions
-    
+
+
+
     for (let index = 0; index < spells.length; index++) {
+
+        let currSpell = spells[index]
 
         let leftIndex = 0
         let rightIndex = potions.length - 1
-        
-        let midIndex
 
-        leftIndex
-        rightIndex
-        index
-        
-        const currSpell = spells[index]
+        // <= to get -1 for rightIndex
 
         while (leftIndex <= rightIndex) {
-            
-            leftIndex
-            rightIndex
 
-            spells[index]
-            
-            let midIndex=Math.floor( (rightIndex+leftIndex) / 2)
+            let midIndex = Math.trunc((leftIndex+rightIndex) / 2)
 
-            // midIndex = ~~(leftIndex + (rightIndex - leftIndex) / 2)
-            const currSpellWithPotionNum = currSpell * potions[midIndex]
+            // binary search with > and <= conditions        
+            const spellValue = currSpell * potions[midIndex]
 
-            if (currSpellWithPotionNum >= successNum) {
+            if (spellValue >= successNum){
                 rightIndex = midIndex - 1
-            } 
+            }
 
-            else if (currSpellWithPotionNum < successNum){
-                leftIndex = midIndex + 1
+            else if (spellValue < successNum) {
+                leftIndex = midIndex+1
             }
 
         }
+        index
 
+        leftIndex
         rightIndex
 
-        let currSpellEfficacy = (potions.length - rightIndex) - 1
-
-        successOfSpellsArray[index] = currSpellEfficacy
-        successOfSpellsArray
-
+        successOfSpellsArray[index]= potions.length - rightIndex-1
     }
     return successOfSpellsArray
 }
+
+// console.log(successfulPairs({
+//         spells: [5, 1, 3],
+//         potions: [1, 2, 3, 4, 5],
+//         successNum: 7
+//     }), [4, 0, 3],
+//     "spell 1 with all 5 potions had 4 successful potions, so 4 goes into the first position")
+
+// console.log(successfulPairs({spells:[5, 8, 12, 15], potions: [3, 2], successNum: 1}), [2, 2, 2, 2], "[spell #1: 2 successful potions, spell #2: 2 successful potions")
+
+// console.log(successfulPairs({spells:[3,1,2], potions: [8,5,8], successNum: 16}),   [2, 0 , 2])
