40ea2eaf31f804d08c7ad49bda962360565363c1
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-04-11 13:02:16 -0400

    Time: 143 ms (31.75%), Space: 54.3 MB (26.95%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.js
@@ -0,0 +1,38 @@
+var groupAnagrams = function(stringArray){
+
+    // take a normal array and return a double array = use an object
+    const object = {}
+    
+    for (let normalString of stringArray){
+
+        let anagramString = normalString.split("").sort().join("")
+        
+        object
+        
+        const anagramArray = object[anagramString]
+
+        if (anagramArray != undefined){
+            anagramArray.push(normalString)
+        }
+        
+        // else add it to the object for the first time
+        // think of = sign as the colon
+    
+        
+        else if (!anagramArray) { 
+            object[anagramString] = [normalString] // so you can push into the array
+        }
+        
+        object
+    }
+    
+    const doubleArray = Object.values(object)
+    // use .sort() for alphabetical order, dont use a and b
+    // sort it on a lower level with map, otherwise it only compares the first string of each array
+
+
+    // ["bat"] is a, ["nat", "tan"] is b
+    // a and b represent the elements
+
+    return doubleArray.map(subarray => subarray.sort()).sort((a,b) => a.length - b.length)
+}
