7012bcb8d72411ff8f0134d28e533afcfc997d1f
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-04-14 13:15:45 -0400

    Time: 228 ms (73.99%), Space: 83.5 MB (50.29%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.js
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.js
@@ -1,33 +1,39 @@
-var longestPalindromeSubseq = function(s) {
+var longestPalindromeSubseq = function(string) {
     
     // Get the length of the input string
     
-    const n = s.length;
 
     // Initialize a 2D array to store the length of the longest palindromic subsequence
     
-    const dp = Array(n).fill().map(() => Array(n).fill(0));
+    const dp = new Array(string.length).fill().map(() => Array(string.length).fill(0))
     
       // Iterate over the substrings in reverse order to fill in the dp table bottom-up
-    for (let i = n - 1; i >= 0; i--) {
+
+    for (let index = string.length - 1; index >= 0; index--) {
         // Base case: the longest palindromic subsequence of a single character is 1
         
-        dp[i][i] = 1
+        dp[index][index] = 1
         dp
 
-        for (let j = i+1; j < n; j++) {
-
-            // If the two characters match, the longest palindromic subsequence can be extended by two
+        for (let j = index+1; j < string.length; j++) {
             
-            if (s[i] === s[j]) {
-                dp[i][j] = 2 + dp[i+1][j-1];
-            } else {
-                // Otherwise, we take the maximum of the two possible substrings
-                dp[i][j] = Math.max(dp[i+1][j], dp[i][j-1]);
+            // If the two characters match, the longest palindromic subsequence can be extended by two
+    
+            if (string[index] == string[j]) {
+                dp[index][j] = 2 + dp[index+1][j-1]
+            } 
+            
+            // Otherwise, we take the maximum of the two possible substrings
+            
+            else {
+                dp[index][j] = Math.max(dp[index+1][j], dp[index][j-1])
             }
         }
     }
     
-    // The length of the longest palindromic subsequence is in the top-right corner of the dp table
-    return dp[0][n-1];
-};
+    // The length of the longest palindromic subsequence 
+    // is in the top-right corner of the dp table
+    
+    dp
+    return dp[0][string.length-1]
+}
