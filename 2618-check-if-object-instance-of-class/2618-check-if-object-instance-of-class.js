3b1cee7ac99760a1f2ad88a5c9892bb44aafbdcc
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-04-17 17:32:11 -0400

    Time: 108 ms (57.07%), Space: 52.1 MB (58.83%) - LeetHub

--- 2618-check-if-object-instance-of-class/2618-check-if-object-instance-of-class.js
+++ 2618-check-if-object-instance-of-class/2618-check-if-object-instance-of-class.js
@@ -6,10 +6,14 @@
 
 var checkIfInstanceOf = function(obj, classFunction) {
     
-    if (obj == undefined  || typeof classFunction != "function") {
+    if (obj==null  || typeof classFunction != "function") {
         return false
     }    
+
     return (Object(obj) instanceof classFunction)
 }
 
-console.log(checkIfInstanceOf(new Date(), Date), true)
+// console.log(checkIfInstanceOf(new Date(), Date), true)
+
+console.log(checkIfInstanceOf(5, Number), true)
+console.log(checkIfInstanceOf(5, Object), false)
