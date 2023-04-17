edf6f1c2b531215d714607e969233c87049d839c
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-04-16 22:00:28 -0400

    Time: 63 ms (57.75%), Space: 41.9 MB (92.06%) - LeetHub

---
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.js
@@ -0,0 +1,4 @@
+const kidsWithCandies = (array, extraCandies)=> array.map(candies => candies + extraCandies >= Math.max(...array))
+    // let max = Math.max(...numArray)
+    // return numArray.map(number => number + extraNum >= max)
+
