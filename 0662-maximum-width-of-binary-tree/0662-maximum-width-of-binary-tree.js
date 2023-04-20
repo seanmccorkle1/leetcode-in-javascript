3e7877d4ccee43cd8599c34b2e32e767026384e6
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-04-19 20:42:36 -0400

    Time: 65 ms (95.24%), Space: 46.5 MB (80.27%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.js
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.js
@@ -12,18 +12,17 @@
  * @return {number}
  */
 
+
 var widthOfBinaryTree = function(root) {
-    
+
     const minPos = [0]
     let maxWidth = 0
-    
-    callDFS(root, 0, 0)
-    return maxWidth
 
     
-    function callDFS(node, level, pos) {
+    const callDFS = (node, level, pos) => {
+        
         if (node==undefined) return;
-
+        
         if(minPos[level] == undefined) {
             minPos.push(pos)
         }
@@ -34,4 +33,9 @@ var widthOfBinaryTree = function(root) {
         callDFS(node.left, level+1, diff*2)        
         callDFS(node.right, level+1, diff*2+1)
     }
+    
+    
+    callDFS(root, 0, 0)
+    return maxWidth
+    
 }
