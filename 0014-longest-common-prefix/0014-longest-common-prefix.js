5f5c53ad627720cbc9818df41722ada7a274bc14
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-04-21 15:17:22 -0400

    Time: 62 ms (64.02%), Space: 45.1 MB (5.28%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.js
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -1,23 +1,30 @@
-var longestCommonPrefix = function(array) {
-
-    // if (array.length == 0){
-    //     return ""
-    // }
+var longestCommonPrefix = function(strings) {
 
     // as soon as it doesnt equal return it to force the prefix
+    const firstWord = strings[0]
 
-    const firstWord = array[0]
+    // keep the (index < variable.length) condition consistent 
+    // with the (return {variable}) statement AFTER the loop
 
-    for (let index = 0; index < firstWord.length; index++){
+    // for each letter, loop through all the words in the array
 
-        index
+    // check "f"  => "flower", "flow", "flight"
+    // check "l"  => "flower", "flow", "flight"
 
-        for (const currWord of array) {
+    for (let letterIndex = 0; letterIndex < firstWord.length; letterIndex++) {
+
+        letterIndex
+
+        for (const currWord of strings) {
 
             currWord
+            const currLetter = currWord[letterIndex]
+            const letterInFirstWord = firstWord[letterIndex]
 
-            if (currWord[index] != firstWord[index]){
-                return currWord.slice(0, index)
+            // leave out the letter at the index cause it doesnt match
+
+            if (currLetter != letterInFirstWord){
+                return firstWord.slice(0, letterIndex)
             }
         }
     }
@@ -26,10 +33,6 @@ var longestCommonPrefix = function(array) {
     return firstWord
 }
 
-// console.log(longestCommonPrefix(["flower","flow","flight"]), "fl")
-// console.log(longestCommonPrefix(["flower","flow","flight", "f"]),  "f")
-
-console.log(longestCommonPrefix(["fl","fl","fl"]), "fl")
-// console.log(longestCommonPrefix(["asd","xd"]), "")
-
-// console.log(longestCommonPrefix([]))
+console.log(longestCommonPrefix(["flower","flow","flight"]), "fl")
+console.log(longestCommonPrefix(["flower","flow","flight", "f"]),  "f")
+console.log(longestCommonPrefix(["asd","xd"]), "")
