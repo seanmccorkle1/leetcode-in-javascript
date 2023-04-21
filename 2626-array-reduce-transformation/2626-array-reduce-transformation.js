2bae7c76905b985b8b74514eeb994325fb09fc8e
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-04-21 15:33:27 -0400

    Time: 53 ms (92.07%), Space: 42.4 MB (59.34%) - LeetHub

--- 2626-array-reduce-transformation/2626-array-reduce-transformation.js
+++ 2626-array-reduce-transformation/2626-array-reduce-transformation.js
@@ -3,18 +3,11 @@ var reduce = function(nums, reduceFn, initialValue) {
     let currSum = initialValue
 
     for (let index = 0; index < nums.length; index++) {
-        currSum = reduceFn(currSum, nums[index])
+        const currNum = nums[index]
+        currSum = reduceFn(currSum, currNum)
     }
     return currSum
 }
 
-const nums = [1, 2, 3, 4]
-var fn = function sum(accum, curr) {
-    return accum + curr;
-}
-
-let initialValue = 0
-
-// reduce(nums, fn, initialValue)
-
-reduce([2,3,4,5], (sum, curr) => sum + curr,  1)
+let initValue= 1
+console.log(reduce([2,3,4,5], (sum, curr) => sum + curr,  initValue),   15)
