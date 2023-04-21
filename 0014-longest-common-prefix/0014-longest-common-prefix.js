25c7508ef500cadd760f24a58dbebe9404018a05
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-04-21 13:51:15 -0400

    Time: 47 ms (98.88%), Space: 45.1 MB (5.28%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -0,0 +1,35 @@
+var longestCommonPrefix = function(array) {
+
+    if (array.length == 0){
+        return ""
+    }
+
+    // as soon as it doesnt equal return it to force the prefix
+
+    const firstWord = array[0]
+
+    for (let index = 0; index < firstWord.length; index++){
+
+        index
+
+        for (const currWord of array) {
+
+            currWord
+
+            if (currWord[index] != firstWord[index]){
+                return currWord.slice(0, index)
+            }
+        }
+    }
+
+    // the longest prefix is just the whole word
+    return firstWord
+}
+
+// console.log(longestCommonPrefix(["flower","flow","flight"]), "fl")
+// console.log(longestCommonPrefix(["flower","flow","flight", "f"]),  "f")
+
+console.log(longestCommonPrefix(["fl","fl","fl"]), "fl")
+// console.log(longestCommonPrefix(["asd","xd"]), "")
+
+// console.log(longestCommonPrefix([]))
