e7d64d152bdbc587102aa9516c9a5bf1bdddf262
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-04-22 16:02:42 -0400

    Time: 67 ms (10.30%), Space: 42.1 MB (50.32%) - LeetHub

--- 2634-filter-elements-from-array/2634-filter-elements-from-array.js
+++ 2634-filter-elements-from-array/2634-filter-elements-from-array.js
@@ -6,12 +6,13 @@ var filter = function(array, fn) {
 
     for (let index = 0; index < array.length; index++){
         
-       if (fn(array[index], index)){
-           newArray.push(array[index])
+        const num=array[index]
+       if (Boolean(fn(num, index))==true){ 
+           newArray.push(num)
       }
    }
     
     return newArray
 }
 
-filter([0,10,20,30], greaterThan10 = num => num > 10)
+filter([0,8,20,30], greaterThan10 = num => num > 10)
