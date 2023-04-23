9e17e56ebfad94e5cec4dfbe7602785f72f38afb
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-04-23 10:20:41 -0400

    Time: 61 ms (36.52%), Space: 42.5 MB (20.00%) - LeetHub

---
+++ 2648-generate-fibonacci-sequence/2648-generate-fibonacci-sequence.js
@@ -0,0 +1,39 @@
+/**
+ * @return {Generator<number>}
+ */
+
+// initialise first 2 sequence numbers
+// initialise a loop so that function can return values indefinitely when called
+
+// yield pauses fx execution and returns value, before continuing subsequent steps
+// fibonacci sequence is updated in steps after yield
+
+// note: 
+// third term is the sum of first and second term
+
+// therefore: term n is (n-1 + n- 2)
+
+
+var fibGenerator = function*() {
+
+    let leftNum = 0
+    let rightNum = 1
+
+    while (true) {
+        yield leftNum;
+
+        // leftNum goes up to rightNum
+        // rightNum goes up to the two-sum rightNum +leftNum
+
+        [leftNum,rightNum] = [rightNum,  leftNum+rightNum]
+    }  
+}
+
+const generatorObj = fibGenerator()
+
+fibGenerator()
+generatorObj
+generatorObj.next()
+
+generatorObj.next().value; // 0
+generatorObj.next().value; // 1
