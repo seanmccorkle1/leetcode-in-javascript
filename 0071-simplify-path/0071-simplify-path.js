6d599b1868cf98464b394bbf32e997294aa2fe1b
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-04-23 11:29:48 -0400

    Time: 71 ms (36.53%), Space: 43.5 MB (96.70%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.js
+++ 0071-simplify-path/0071-simplify-path.js
@@ -3,41 +3,32 @@ var simplifyPath = function (pathArray) {
     const stack = []
     
     pathArray = pathArray.split('/')
+
     
     pathArray = pathArray.filter(element => element != ".")
-        .filter(element=> Boolean(element) == true)
-        //.filter(element => element != "")
+        .filter(element => element != "")
     
     pathArray    
+
     
     for (let index = 0; index < pathArray.length; index++) {
 
-        const currString = pathArray[index]
+        const currDirectory = pathArray[index]
         
         // double slash becomes an empty string on the .split("/")
         // one period just means "current directory", it doesnt do anything
         
-        // avoid pushing "." which does nothing
-
-        // also avoid pushing the empty string which comes form
-        // `//` being in the string
-        
-        // if (currDirectory=='.' || currDirectory== "") {
-        //     continue 
-        // }
-
         // remove the current directory and go up to the most recent one
         // currString is an array element, not a letter
 
-        if (currString == '..') {
+        if (currDirectory == '..') {
             stack
             stack.pop()
-        } 
-
-        else {
-            stack.push(currString)
-            stack
+            continue
         }
+        
+        stack.push(currDirectory)
+        stack
     }
     
     stack
