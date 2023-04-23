97ee26b9d3d4f8eb4637d08c06c31281078569f1
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-04-23 11:15:46 -0400

    Time: 66 ms (21.35%), Space: 43.1 MB (13.42%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.js
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.js
@@ -1,18 +1,18 @@
-var mergeAlternately = (word1, word2) => {
-    
-        let currString = ''
-        
-        for (let index = 0; index < Math.max(word1.length, word2.length); index++) {
+const mergeAlternately = (word1, word2) => {
 
-            console.log(undefined??null)
-            let word1Letter= word1[index] ?? ""
-            let word2Letter = word2[index] ?? ""
-            
-            let twoLetters = word1Letter + word2Letter
-            currString += twoLetters    
-        }
-    
+    let currString = ""
+
+    for (let index = 0; index < Math.max(word1.length, word2.length); index++) {
+
+        let word1Letter =word1[index] ?? ""
+        let word2Letter = word2[index]?? ""
+
+        currString += (word1Letter + word2Letter)
+
+    }
+
     return currString
 }
 
 console.log(mergeAlternately("leet", "code"), "lceoedte")
+// console.log(mergeAlternately("leet", "co"), "lceoet")
