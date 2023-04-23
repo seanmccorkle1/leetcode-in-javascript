c0c3aaead6999b9bfd2639a3a56e881be2c696b9
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-04-23 10:03:54 -0400

    Time: 55 ms (72.37%), Space: 41.9 MB (70.76%) - LeetHub

--- 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
+++ 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
@@ -1,15 +1,14 @@
-/**
- * @param {number[]} arr
- * @param {Function} fn
- * @return {number[]}
- */
-
 var map = function(array, mapFn) {
+
+    const mappedArray = new Array(array.length)
     
-    return array.map(mapFn)
+    for (let index = 0; index < array.length; index++) {    
+        
+        const mappedNum = mapFn(array[index], index)
+        mappedArray[index] = mappedNum
+  }    
+    return mappedArray
 }
 
-// const newArray = map(arr, plusone)
 const plusIndexFn = (num, index)=> {return num + index}
-
 console.log(map([1,2,3], plusIndexFn),   [1,3,5])
