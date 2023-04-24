a41500d16ccb7f2819b19522b858e6d37838cbc3
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-04-24 17:22:51 -0400

    Time: 155 ms (38.70%), Space: 66.7 MB (23.32%) - LeetHub

--- 2631-group-by/2631-group-by.js
+++ 2631-group-by/2631-group-by.js
@@ -1,18 +1,21 @@
-Array.prototype.groupBy = function(fn) {    
+Array.prototype.groupBy = function(typeFn) {    
     
     const obj = {}
     
     for (const e of this) {     
         
-        const key = fn(e)
+        // the key is String(e) or Array(e)        
+        const key = typeFn(e)
         
         if (!obj[key]){
             obj[key] = []
         }
+        
         let array=obj[key]
         array.push(e)
     }
     return obj
 }
 
-// [1,2,3].groupBy(String) // {"1":[1],"2":[2],"3":[3]}
+const a=[1,2,3]
+a.groupBy(String) // {"1":[1],"2":[2],"3":[3]}
