43b7ed5ff7d41cdff1932890e092c88e844b9bf0
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-04-24 18:55:04 -0400

    Time: 95 ms (38.72%), Space: 64.9 MB (32.21%) - LeetHub

--- 2633-convert-object-to-json-string/2633-convert-object-to-json-string.js
+++ 2633-convert-object-to-json-string/2633-convert-object-to-json-string.js
@@ -1,35 +1,60 @@
 /**
- * @param {any} object
+ * @param {any} input
  * @return {string}
  */
 
-var jsonStringify = function(object) {
-    
-    if (object === null) {
+var jsonStringify = function (input) {
+
+    if (input === null) {
         return 'null'
     }
-    
+
     // return the string value surrounded by double quotes.
-  if (typeof object == 'string') {
-      return `"${object}"`
-  }
-    
-    // return its string representation.
-    if (typeof object == 'number' || typeof object === 'boolean') {
-        return object.toString()
+    if (typeof input == 'string') {
+        return `"${input}"`
     }
-    
-if (Array.isArray(object)) {
+
+    // return its string representation.
+    if (typeof input == 'number' || typeof input === 'boolean') {
+        return input.toString()
+    }
+
     // Recursively convert each item to a JSON string and join them with commas.
-    const items = object.map(item => jsonStringify(item)).join(',');
-    return '[' + items + ']';
-  }
-  // Recursively convert each value to a JSON string and pair it with the corresponding key.
-  if (typeof object === 'object') {
-    const keys = Object.keys(object);
-    const items = keys.map(key => '"' + key + '":' + jsonStringify(object[key]));
-    return '{' + items.join(',') + '}';
-  }
+
+    if (Array.isArray(input)) {
+
+        const array = input.map(item => jsonStringify(item))
+
+        return `[${array}]`
+        
+        let s=`[${array}]`
+
+        let s2=`[${array.join(",")}]`
+
+        s.length
+        s2.length
+
+        return `[${array.join(",")}]`
+    }
+
+    // Recursively convert each value to a JSON string and pair it with the corresponding key.
+
+    if (typeof input === "object") {
+
+        const keys = Object.keys(input)
+        const items = keys.map(key => '"' + key + '":' + jsonStringify(input[key]))
+        
+        // return '{' + items.join(',') + '}'
+        return '{' + items + '}'
+    }
 }
 
-console.log(jsonStringify({"y":1,"x":2}),  {"y":1,"x":2})
+// console.log(jsonStringify({
+//     "y": 1,
+//     "x": 2
+// }), {
+//     "y": 1,
+//     "x": 2
+// })
+
+jsonStringify({"key":{"a":1,"b":[{},null,"Hello"]}})
