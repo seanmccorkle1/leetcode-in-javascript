d4a608b6d532fe55110dde94668f0dc62c8dc30f
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-04-25 15:42:41 -0400

    Time: 210 ms (42.78%), Space: 75 MB (15.69%) - LeetHub

---
+++ 2625-flatten-deeply-nested-array/2625-flatten-deeply-nested-array.js
@@ -0,0 +1,43 @@
+
+
+
+
+var flat = function (array, flattenDepth) {
+
+    // if n is 0, no flattening is required, hence return the original array    
+    if (flattenDepth == 0){
+        return array
+    }
+
+    const flattenedArray = []
+
+    for(let index=0; index < array.length; index++){
+
+        const currElement = array[index]
+
+        // recursively call the function for this array
+        // and push the flattened array to the answer array
+
+        if (Array.isArray(currElement) &&flattenDepth >= 1) {
+            
+            const subarray = array[index]
+            const recursiveArray = flat(subarray, flattenDepth - 1)
+            
+            flattenedArray.push(...recursiveArray)
+
+            flattenedArray
+        }
+
+        else if (Array.isArray) {
+            flattenedArray.push(currElement)
+            flattenedArray
+        }
+    }
+    
+    return flattenedArray
+}
+
+// console.log(flat([1, 2, 3, [4, 5, 6], [7, 8, [9, 10, 11], 12], [13, 14, 15]], 0))
+console.log(flat([1, 2, 3, [4, 5, 6], [7, 8, [9, 10, 11], 12], [13, 14, 15]], 1))
+
+// console.log(flat([1, 2, 3, [4, 5, 6], [7, 8, [9, 10, 11], 12], [13, 14, 15]], 5))
