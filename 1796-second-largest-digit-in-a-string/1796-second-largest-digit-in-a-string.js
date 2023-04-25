dd6d37754f84b51262d6ca0f1179387127696a82
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-04-25 14:02:46 -0400

    Time: 59 ms (91.89%), Space: 44.4 MB (55.41%) - LeetHub

---
+++ 1796-second-largest-digit-in-a-string/1796-second-largest-digit-in-a-string.js
@@ -0,0 +1,28 @@
+var secondHighest = function(string) {   
+
+    const r=/[\d]/g
+
+    let a= string.match(r)
+
+    if (a==null){
+        return -1
+    }
+
+    a = a.sort((a,b)=> a- b)
+
+    // outer being [] means its an array
+    a = [...new Set(a)] 
+
+    a   
+
+    if (a.length <= 1){
+        return -1
+    }  
+    
+    let secondHighest=a.at(-2)
+    return secondHighest
+    // return Number(a.at(-2))
+}
+
+console.log(secondHighest("dfa12321afd"),  2)
+secondHighest("dfa")
