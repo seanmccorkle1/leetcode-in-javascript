74fca609ef677871977002f4858a749ed3d6efb4
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-04-25 13:38:06 -0400

    Time: 61 ms (54.67%), Space: 42.1 MB (45.47%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.js
+++ 0392-is-subsequence/0392-is-subsequence.js
@@ -1,44 +1,58 @@
-var isSubsequence = function(subsequence, bigString){ 
-    
-    if (subsequence==null || bigString==null){
+var isSubsequence = function (subsequence, bigString) {
+
+
+    if (subsequence == null || bigString == null) {
         return false
     }
-    
-    if (subsequence.length > bigString.length){
+
+
+    if (subsequence.length > bigString.length) {
         return false
     }
-    
-    if (subsequence == ""){
+
+
+    if (subsequence == "") {
         return true
     }
-    
+
+
     if (subsequence == bigString) {
         return true
     }
-    
-    let index =0
-    let index2 =0
-    
-    while (index2 < Math.max(bigString.length,subsequence.length)) { 
 
-        if (subsequence[index] == bigString[index2]){ 
 
-            index += 1
-            
-            if (index == subsequence.length){
+    let substringIndex = 0
+    let bigStringIndex = 0
+
+
+    while (bigStringIndex < bigString.length) {
+
+
+        if (subsequence[substringIndex] == bigString[bigStringIndex]) {
+
+
+            substringIndex += 1
+
+
+            if (substringIndex == subsequence.length) {
                 return true
             }
-            
-        }
-        
-        index2++ 
-    }
-    
-    return false
-} 
 
-console.log(isSubsequence("letcod", "leetcode"), true) 
+
+        }
+
+
+        bigStringIndex++
+    }
+
+
+    return false
+}
+
+
+console.log(isSubsequence("nwstrng", "newstring"), true)
+// console.log(isSubsequence("nqstrng", "newstring"), false) 
+
 
 // console.log(isSubsequence("string", ""), false)
 // console.log(isSubsequence("", "string"), true)
-// console.log(isSubsequence("nqrstring", "newstring"), false) 
