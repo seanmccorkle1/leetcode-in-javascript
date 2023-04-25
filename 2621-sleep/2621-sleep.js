01e24bb1e69f129dd45c5a1406cebcb85b7108fb
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-04-25 18:34:28 -0400

    Time: 62 ms (41.91%), Space: 42.3 MB (14.17%) - LeetHub

---
+++ 2621-sleep/2621-sleep.js
@@ -0,0 +1,8 @@
+
+
+async function sleep(millis) {
+    await new Promise(resolve => setTimeout(resolve, millis))
+}
+
+let t = Date.now()
+sleep(100).then((v) => console.log(Date.now() - t))   // 100
