1e2c557522d51dfb1570759ae4c633b5e593f99d
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-04-25 17:06:49 -0400

    Time: 54 ms (82.00%), Space: 43.4 MB (8.86%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.js
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.js
@@ -1,31 +1,18 @@
-const mergeAlternately = (word1, word2) => {
-
-    let currString = ""
-    let index = 0
-
-    while  (index < Math.min(word1.length,word2.length)) {
-
-        // for (var index = 0; index < Math.min(word1.length, word2.length); index++) {
-
-        let word1Letter =word1[index] 
-        let word2Letter = word2[index]
-
-        currString += word1Letter + word2Letter
-        index++
-    }
-
-    let remainingLetters =""
+var mergeAlternately = (word1, word2) => {
     
-    if (word1.length > word2.length){
-        remainingLetters = word1.slice(index)
-    }
+        let mergedString = ""
+        
+        for (let index = 0; index < Math.max(word1.length, word2.length); index++) {
+            
+            let word1Letter= (word1[index] ?? "")
+            let word2Letter = word2[index] ?? ""
+            
+            const twoLetters = word1Letter + word2Letter
+            mergedString += twoLetters    
+        }
     
-    if (word2.length > word1.length){
-        remainingLetters = word2.slice(index)
-    }
-    
-    return currString + remainingLetters
+    return mergedString
 }
 
-// console.log(mergeAlternately("leet", "code"), "lceoedte")
-console.log(mergeAlternately("leet", "co"), "lceoet")
+console.log(mergeAlternately("leet", "code"), "lceoedte")
+// console.log(mergeAlternately("leet", "co"), "lceoet")
