b0a17c2ab256959c2c5641b36cdbbb24e5088cfa
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-04-25 11:58:09 -0400

    Time: 53 ms (77.60%), Space: 41.9 MB (62.58%) - LeetHub

--- 2619-array-prototype-last/2619-array-prototype-last.js
+++ 2619-array-prototype-last/2619-array-prototype-last.js
@@ -1,12 +1,13 @@
 // "function Array."" wont work
 
 Array.prototype.last= function() {
-    
-    if (this.length == 0) {
+    const array=this
+
+    if (array.length == 0) {
         return -1
     } 
     
-    return this.at(-1)
+    return array.at(-1)
 }
 
 const a = [1, 2, 3]
