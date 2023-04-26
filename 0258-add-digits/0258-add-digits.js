3d3853ed6cb8f90190820b6345daffc36baaa49b
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-04-25 20:14:52 -0400

    Time: 75 ms (44.80%), Space: 43.5 MB (79.78%) - LeetHub

--- 0258-add-digits/0258-add-digits.js
+++ 0258-add-digits/0258-add-digits.js
@@ -1,18 +1,12 @@
-
 var addDigits = function (num) {
     
     if (num==0){
         return 0
     }
     
-    // if (num <= 9) {
-    //     return num
-    // }  
-
     num
     
-    // this is called a "digital root"
-    const isDivisibleBy9 =(num % 9) == 0
+    // called a digital root
     
     // 27 -> 2 + 7 = 9
     // return 9
@@ -20,17 +14,21 @@ var addDigits = function (num) {
     // 36 -> 3 + 6 = 9
     // return 9 
     
+    // const isDivisibleBy9 =(num % 9) == 0
+    const isDivisibleBy9 = Number.isInteger(num /9)
+    
+    //otherwise (num % 9) would be 0
+    
     if (isDivisibleBy9) {
-        return 9 //otherwise (num % 9) would be 0
+        return 9 
     }
     
     else if (!isDivisibleBy9) {
         let remainderOn9 = num % 9
         return remainderOn9
     }
-    
 }
 
 console.log(addDigits(28), 1, "28 -> 10 -> 1")
-console.log(addDigits(27), 9, "2+7=9")
-console.log(addDigits(26), 8)
+// console.log(addDigits(27), 9, "2+7=9")
+// console.log(addDigits(26), 8)
