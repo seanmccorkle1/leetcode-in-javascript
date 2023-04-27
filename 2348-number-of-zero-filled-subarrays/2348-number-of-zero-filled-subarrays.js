44937d75915e2194bd81cdeee07b5ec590a4b9f7
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-04-27 16:43:01 -0400

    Time: 85 ms (82.50%), Space: 54 MB (36.47%) - LeetHub

--- 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.js
+++ 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.js
@@ -1,30 +1,25 @@
 var zeroFilledSubarray = function(array) {
-
-
+    
     let numOfZeroSubarrays = 0
     let consecutiveCount = 0
-
-
+    
     for (let index = 0; index < array.length; index++) {
-
-
+        
         const currNum = array[index]
         
         if (currNum == 0) {
-            consecutiveCount++
+            consecutiveCount += 1
             numOfZeroSubarrays += consecutiveCount
-        } 
-
-
-        else if (currNum != 0){
+            
+        }
+            else if (currNum != 0){
             consecutiveCount = 0 // reset
         }
     }
-
-
+    
+    console.log(consecutiveCount)
     return numOfZeroSubarrays
 }
-
 
 console.log(zeroFilledSubarray([1,3,0,0,2,0,0,4]),  6, "[0], [0], [0], [0], [0, 0], [0, 0]")
 // console.log(zeroFilledSubarray([0,0,0,2,0,0]), 9)
