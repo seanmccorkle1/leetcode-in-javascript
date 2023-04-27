57313c9b5c59f6a2642a45b8e0cd32566f0cf8cd
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-04-27 15:41:29 -0400

    Time: 377 ms (53.27%), Space: 109 MB (5.20%) - LeetHub

--- 2623-memoize/2623-memoize.js
+++ 2623-memoize/2623-memoize.js
@@ -14,24 +14,33 @@ var memoize = (sumFn) => {
             return  cachedReturnValue
             // return memo[argsAsString]
         }
-
+        
         else if (!memoAlreadyHasArguments){
+
             let returnValue = sumFn(...args)
             memo[argsAsString] =returnValue
+
             return returnValue
         }
     }    
 }
 
-// let callCount = 0
+let callCount = 0
+
+var sumTwoFn = (num1, num2) =>{
+    callCount+=1
+    return num1+num2
+} 
+
+var memoizedFn = memoize(sumTwoFn)
 
 // var memoizedFn = memoize(function (a, b) {
 //     callCount += 1
 //     return a + b
 // })
 
-// memoizedFn.length
+memoizedFn.length // 0 (any amount of arguments)
 
-// memoizedFn(2, 3) // 5
-// memoizedFn(2, 3) // 5
-// console.log(callCount) // 1
+memoizedFn(2, 3) // 5
+memoizedFn(2, 3) // 5
+console.log(callCount) // 1
