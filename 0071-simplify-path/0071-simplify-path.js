a4a5fabf884bb11d4ab36bc741c251d55ac11dbd
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-04-27 13:18:21 -0400

    Time: 62 ms (78.27%), Space: 44 MB (69.15%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.js
+++ 0071-simplify-path/0071-simplify-path.js
@@ -1,28 +1,25 @@
 var simplifyPath = function (pathString) {
-
+    
     const stack = []
     
     // double slash becomes an empty string on the .split("/")
     // one period just means "current directory", it doesnt do anything
     
-
-    // if (pathString[0] == "/"){
-    //     pathString = pathString.slice(1)
-    // }
-
-    
     pathString = pathString.replace(/[\/]+/g, "/")
     
     // periods dont go in the result
+    // it has to be both NOT a period and NOT an empty string
     
-    const pathArray =pathString.split("/") .filter(directory => directory != "." && directory != "")
+    const pathArray =
+          pathString.split("/") 
+            .filter(directory => directory != "." && directory != "")
     
     pathArray
     
     for (let index = 0; index < pathArray.length; index++) {
-
+        
         const currDirectory = pathArray[index]
-
+        
         // if its "..", pop (remove) the current directory from the stack a
         // and go up to the most recent directory
 
@@ -44,19 +41,9 @@ var simplifyPath = function (pathString) {
     }
     
     stack
-    
-    return "/"+stack.join("/")
-    
     let joinedPath = stack.join("/")
-    const firstLettersAlreadySlash = joinedPath[0]=="/"
+    return "/" + joinedPath
     
-    if (joinedPath[0] == "/"){
-        return joinedPath
-    }
-    
-    else return "/" + joinedPath
-    
-    // return "/" + joinedPath
 }
 
 // console.log(simplifyPath("/C:/users/../Program files"), "/C:/Program files", "at 'users', go up to C")
