f9c93a4acfdec039e71477221837018e76cc3af2
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-04-27 22:18:05 -0400

    Time: 118 ms (69.42%), Space: 48.7 MB (15.04%) - LeetHub

--- 0561-array-partition/0561-array-partition.js
+++ 0561-array-partition/0561-array-partition.js
@@ -1,4 +1,4 @@
 
 var arrayPairSum = function(array) {
-    return array.sort((a,b) => a - b).filter((n, index) =>index % 2 == 0 ? "pass" : false).reduce((sum,curr) => sum+curr)
+    return array.sort((a,b) => a - b).filter((n, index) =>index % 2 == 0 ? "pass" : false).reduce((sum,curr) => sum+curr,0)
 }
