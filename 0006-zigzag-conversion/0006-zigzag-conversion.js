387e91d2cd76c0b1d8efb2771342a273ba2135c4
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-04-28 19:51:23 -0400

    Time: 73 ms (89.26%), Space: 48.5 MB (41.55%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.js
@@ -0,0 +1,41 @@
+var convert = function(s, numRows) {
+    
+    // return original string if you can't zigzag
+    if (numRows == 1 || s.length < numRows) {
+        return s
+    }
+    
+    const rows = []
+    let converted = '';
+
+    let reverse = false;
+    let count = 0
+
+    // prepare rows
+    
+    for (let i = 0; i < numRows; i++) rows[i] = [];
+    // reverse the push flow when reaching turning points
+    
+    for (let i = 0; i < s.length; i++) {
+
+        const currLetter = s[i]
+        const currArray = rows[count]
+        currArray.push(currLetter)
+        
+        
+        reverse ? count -= 1 : count+=1 
+        
+        if (count == numRows - 1) {
+            reverse = !reverse
+        }
+        
+        if (count == 0){
+            reverse = !reverse
+        }
+    }
+    
+    // put together converted string
+    return rows.reduce((converted, cur) => converted + cur.join(''), "")
+}
+
+console.log(convert("PAYPALISHIRING", 3), "PAHNAPLSIIGYIR")
