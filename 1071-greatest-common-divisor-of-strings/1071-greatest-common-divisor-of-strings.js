bd422eea6e9ff8153cadaa08f58d5589dfab5446
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-04-28 13:06:53 -0400

    Time: 60 ms (70.61%), Space: 42.2 MB (79.01%) - LeetHub

---
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.js
@@ -0,0 +1,23 @@
+const gcdOfStrings = (string1, string2) => {
+
+    let concat = string1+string2
+    let reverseConcat = string2+string1
+    
+    if (concat != reverseConcat ) {
+        return ""
+    }
+    
+  const gcd = (length1, length2) => {
+      
+      if (length2 == 0){
+          return length1
+      }
+      else {
+          return gcd(length2, length1 % length2) 
+      }
+  }
+  // (length2 == 0? a : gcd(b, a % b))
+  
+  const endLength =gcd(string1.length, string2.length)
+  return string1.slice(0, endLength)
+}
