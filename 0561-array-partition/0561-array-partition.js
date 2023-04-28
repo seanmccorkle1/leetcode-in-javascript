b690db32e008db446bcb1f77ef7ba582c59b117a
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-04-27 22:23:07 -0400

    Time: 124 ms (41.98%), Space: 48.6 MB (15.04%) - LeetHub

--- 0561-array-partition/0561-array-partition.js
+++ 0561-array-partition/0561-array-partition.js
@@ -1,4 +1,12 @@
 
 var arrayPairSum = function(array) {
-    return array.sort((a,b) => a - b).filter((n, index) =>index % 2 == 0 ? "pass" : false).reduce((sum,curr) => sum+curr,0)
+    
+    // limit the ranges for max problems?
+    array.sort((a,b) => a - b)
+
+    array = array.filter((n, index) =>((index % 2) == 0) ? "pass" : false)
+    
+    const maxSumOfMinPairs = array.reduce((sum,curr) => sum+curr, 0)
+    
+    return maxSumOfMinPairs
 }
