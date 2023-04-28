23dc3dec69ac47f19922fa04fcdd2a164bbb515d
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-04-28 17:13:58 -0400

    Time: 54 ms (8.81%), Space: 16.2 MB (21.04%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.py
@@ -0,0 +1,14 @@
+class Solution:
+    def addDigits(self, num: int) -> int:
+        
+        if num==0:
+            return 0
+        
+        isDivisibleBy9 = num % 9 == 0
+        
+        if isDivisibleBy9:
+            return 9
+
+        else:
+            return num % 9
+    
