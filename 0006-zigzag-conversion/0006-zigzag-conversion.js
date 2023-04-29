f7e687dff303db317ba82fed1d9fdce99640e583
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-04-28 22:38:28 -0400

    Time: 99 ms (30.42%), Space: 49.2 MB (29.40%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.js
+++ 0006-zigzag-conversion/0006-zigzag-conversion.js
@@ -1,34 +1,29 @@
 var convert = function(s, numRows) {
     
     // return original string if you can't zigzag
+    
     if (numRows == 1 || s.length < numRows) {
         return s
     }
-
     
-    // const rows = []
-    let converted = '';
-
-    let reverse = false;
+    let converted = ''
+    let reverse = false
+    
     let count = 0
     
-    // prepare rows
-    // const empty = []
+    
     const rows = new Array(numRows).fill(">").map(e => [])
+
     
-    //.fill("?").map(e => [])
-    
-    // for (let i = 0; i < numRows; i++) rows[i] = [];
     // reverse the push flow when reaching turning points
     
     for (let i = 0; i < s.length; i++) {
-
+        
         const currLetter = s[i]
         const currArray = rows[count]
         currArray.push(currLetter)
         
-        
-        reverse ? count -= 1 : count+=1 
+        reverse ? count -= 1 : count+=1
         
         if (count == numRows - 1) {
             reverse = !reverse
@@ -39,8 +34,9 @@ var convert = function(s, numRows) {
         }
     }
     
-    // put together converted string
-    return rows.reduce((converted, cur) => converted + cur.join(''), "")
+    let zigzagString = rows.reduce((string, subarray) => string + subarray.join(""), "")
+    
+    return zigzagString
 }
 
 console.log(convert("PAYPALISHIRING", 3), "PAHNAPLSIIGYIR")
