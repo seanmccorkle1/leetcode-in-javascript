b0de45dfbd51b87a540490ac90cf01f41bdf1cd4
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-04-29 15:43:47 -0400

    Time: 55 ms (71.13%), Space: 41.5 MB (98.75%) - LeetHub

--- 0319-bulb-switcher/0319-bulb-switcher.js
+++ 0319-bulb-switcher/0319-bulb-switcher.js
@@ -1,7 +1,13 @@
-
-var bulbSwitch = function(num) {
+var bulbSwitch = function(num) {    
     
-    // you only get an on-light on the powers
-    return Math.floor(Math.pow(num, 0.5))
+    // you only get an on-light (odd number of factors) on the powers
+    
+    let sqrt =Math.pow(num, 0.5)
+    return Math.floor(sqrt)
 }
 
+console.log(bulbSwitch(4), 2, "bulb 1 and 4 are on")
+
+// console.log(bulbSwitch(1), 1, "1st lightbulb is always on")
+// console.log(bulbSwitch(8), 2, "bulbs 1 and 4 still")
+// console.log(bulbSwitch(9), 3, "bulbs [1, 4, 9] are on ")
