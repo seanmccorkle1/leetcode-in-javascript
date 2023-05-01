563e271f46a9cd5932efa240d2b0074814246255
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-05-01 15:15:53 -0400

    Time: 66 ms (15.98%), Space: 41.9 MB (50.29%) - LeetHub

--- 0394-decode-string/0394-decode-string.js
+++ 0394-decode-string/0394-decode-string.js
@@ -1,20 +1,12 @@
 var decodeString = function(string) {
-
-    // ([a-z]+) is (acc) in 1 group, (acc).repeat(3)
-    //([a-z])+ is [a-z][a-z]([a-z]), only goes on the last group
     
     const regex = /(\d+)\[([a-zA-Z]+)\]/g        
     const replaceFunction = (match,number,letter) => letter.repeat(number)
     
-    while (string.includes("]")){    
-        string
-
+    while (string.includes("]")){
+        
         let shortened = string.replace(regex, replaceFunction)
         string = shortened
-
-        // string.replace(regex, (match,number,letter) => letter.repeat(number))
-
-        // string = string.replace(/(\d+)\[([a-zA-Z]+)\]/g, (match,number,letter) => letter.repeat(number) )
     }
     
     return string
