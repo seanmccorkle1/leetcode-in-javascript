43a40ef8b8c7468f73c5b60f8bc705b4ddfe9327
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-05-02 01:18:13 -0400

    Time: 62 ms (45.78%), Space: 41.9 MB (64.00%) - LeetHub

---
+++ 0413-arithmetic-slices/0413-arithmetic-slices.js
@@ -0,0 +1,60 @@
+var numberOfArithmeticSlices = function (array) {
+
+
+    let consecutiveSliceCount = 0
+    let totalSlices = 0
+
+
+    for (let index = 0; index < array.length - 2; index++) {
+
+
+        consecutiveSliceCount
+        array
+
+
+        // in [0,1,2], this compares [0, 1]
+        const difference1 = array[index + 1] - array[index]
+
+        // compares [1, 2]
+        const difference2 = array[index + 2] - array[index + 1] 
+
+
+        // those 2 makes [0, 1, 2]
+        // the count gets +2 at once if theyre consecutive
+
+
+        // [0, 1, 2] - 1 slice
+        // [0,1,2,3] - 2 new slices  - [1,2,3] and [0,1,2,3]
+
+        //[+1, +2, +3, +4] for consecutiveSliceCount
+
+        difference1
+        difference2
+        
+        if (difference1 == difference2) {
+            
+            consecutiveSliceCount
+            totalSlices
+            
+            consecutiveSliceCount += 1
+            totalSlices += consecutiveSliceCount
+        }
+        
+        else if (difference1 != difference2) {
+            consecutiveSliceCount = 0
+        }
+        
+        totalSlices
+    }
+
+    return totalSlices
+}
+
+console.log(numberOfArithmeticSlices([3, 7, 11, 15, 0, 0, 3, 7, 11]), 4, "the first half is 3 slices, the second is 1 slice")
+
+//console.log(numberOfArithmeticSlices([0, 1, 2]), 1)
+
+// console.log(numberOfArithmeticSlices([0, 1, 2, 3]), 3, "3 slices, [0,1,2], [1,2,3], [0,1,2,3]")
+// console.log(numberOfArithmeticSlices([0, 1, 2, 3, 4]), 6, "3 more, [2,3,4], [1,2,3,4], [0,1,2,3,4]")
+
+// console.log(numberOfArithmeticSlices([0, 1, 4, 6, 7, 10]), 0, "[1, 4, 7] and [4,7,10] are both non-contiguous, they arent slices")
