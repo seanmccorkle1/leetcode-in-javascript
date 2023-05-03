5ee77f38e7edc7646cee67986b48519a83582635
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-05-03 17:43:09 -0400

    Time: 104 ms (68.11%), Space: 46.4 MB (86.65%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.js
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.js
@@ -8,25 +8,37 @@ var findMedianSortedArrays = function(nums1, nums2) {
   while (idx1 + idx2 <= totalLen/2) {
       
       if (typeof curr == "number") {
-      last = curr;
+          last = curr
       }
-    
+      
       let elOne = nums1[idx1];
       let elTwo = nums2[idx2];
-
+      
       if (elOne === undefined) {
           curr = elTwo;
       idx2++;
-    } else if (elTwo === undefined) {
-      curr = elOne;
-      idx1++;
-    } else if (elOne < elTwo) {
-      curr = elOne;
-      idx1++;
-    } else {
-      curr = elTwo;
-      idx2++;
-    }
+    } 
+      
+      else if (elTwo === undefined) {
+          curr = elOne;
+          idx1++;
+    } 
+      
+      else if (elOne < elTwo) {
+          curr = elOne;
+          idx1++;
+    } 
+      
+      else {
+      curr = elTwo      
+          idx2++
+      }
   }
-  return totalLen % 2 === 0 ? (last + curr) / 2 : curr;
-};
+    if ((totalLen % 2) == 0){
+        return (curr + last) / 2
+    }
+    else if ((totalLen % 2) == 1) {
+        return curr
+    }
+    // return totalLen % 2 === 0 ? (last + curr) / 2 : curr;
+}
