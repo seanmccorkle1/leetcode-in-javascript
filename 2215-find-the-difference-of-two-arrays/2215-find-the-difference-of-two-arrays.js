68a2cd9f88bd4fca58772a5452a4b13d582f77c6
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-05-02 22:42:01 -0400

    Time: 92 ms (73.23%), Space: 49.1 MB (43.69%) - LeetHub

--- 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.js
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.js
@@ -1,7 +1,7 @@
 var findDifference = function(nums1, nums2) {
     
     const set = new Set(nums1)
-    nums2.forEach(nums2Value=>{set.delete(nums2Value)})
+    nums2.forEach(nums2Value => set.delete(nums2Value))
     
     const set2 = new Set(nums2)
     nums1.forEach(num1Value => set2.delete(num1Value))    
