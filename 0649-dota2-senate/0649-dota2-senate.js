45353bb9df3e45d40fba7b3002a7dba3cd6ac7c9
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-05-03 20:52:04 -0400

    Time: 81 ms (43.40%), Space: 45.7 MB (83.02%) - LeetHub

---
+++ 0649-dota2-senate/0649-dota2-senate.js
@@ -0,0 +1,21 @@
+var predictPartyVictory = function(senate) {
+    var map={}, m=senate.length, q1=[], q2=[];
+
+    for(var i=0; i<m; i++){
+        if(senate.charAt(i) == 'R') q1.push(i)
+        else{
+            q2.push(i);
+        }
+    }
+    while(q1.length>0 &&q2.length>0){
+        let v1=q1[0]
+        q1.shift()
+        let v2=q2.shift();
+    
+        if(v1<v2) q1.push(v1+m)
+        else{
+            q2.push(v2+m);
+        }
+    }
+    return q1.length> q2.length? "Radiant": "Dire"
+};
