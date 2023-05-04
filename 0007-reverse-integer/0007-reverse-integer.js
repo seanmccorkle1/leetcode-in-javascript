147465cbc5d8cce5b717b667fc72305e5dedaf03
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-05-04 12:14:09 -0400

    Time: 66 ms (81.18%), Space: 44.4 MB (15.57%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.js
@@ -0,0 +1,31 @@
+var reverse = function (number) {
+    
+    const max = 2 ** 31 - 1
+    const min = 2 ** 31 * -1
+    
+    let reversedNum;
+    
+    if (number <= -1) {
+        
+        reversedNum = number.toString().slice(1).split("").reverse().join("")
+        reversedNum = reversedNum * -1
+        
+        if (reversedNum < min) {
+            return 0 
+        }
+        
+        return reversedNum
+    }
+    
+    else if (number >= 0) {                
+        
+        reversedNum= Number(number.toString().split("").reverse().join(""))
+        
+        if (reversedNum > max) {
+            return 0
+        }
+        
+        return reversedNum
+    }
+}
+console.log(reverse(123),  321)
