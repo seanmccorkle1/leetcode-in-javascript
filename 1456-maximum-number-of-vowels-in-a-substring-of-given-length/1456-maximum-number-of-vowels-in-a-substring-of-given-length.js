777469b1424df922462827e1e03e82c51c031439
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-05-05 05:33:34 -0400

    Time: 91 ms (76.04%), Space: 45.6 MB (56.94%) - LeetHub

---
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.js
@@ -0,0 +1,17 @@
+const maxVowels = (s, k) => {
+    
+    const VOWEL = new Set(['a','e','i','o','u']);
+
+    let max = 0;
+  
+    for (let i = 0; i < k; ++i) {
+    VOWEL.has(s[i]) && ++max;
+  }
+
+    for (let i = 0, cur = max; i < s.length - k; i++) {    
+        VOWEL.has(s[i]) && --cur
+        VOWEL.has(s[i + k]) && ++cur
+    cur > max && (max = cur);
+  }
+  return max;
+};
