62ad40714c5afcb97f7a7915315f69963b1b9094
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-05-05 08:40:38 -0400

    Time: 57 ms (60.91%), Space: 41.8 MB (62.44%) - LeetHub

--- 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.js
+++ 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.js
@@ -3,15 +3,23 @@ var average = function(salaries) {
     salaries
     
     let min = Math.min(...salaries)
-    salaries.splice(salaries.indexOf(min), 1)
+    // salaries.splice(salaries.indexOf(min), 1)
     
     let max = Math.max(...salaries)
-    salaries.splice(salaries.indexOf(max), 1)    
+    // salaries.splice(salaries.indexOf(max), 1)    
     
     // `salaries` has 2 less elements now
     // salaries.length was changed
     
-    let avg= salaries.reduce((sum, curr) => sum + curr, 0) / salaries.length
+    let sum = 0
+    sum-=min
+    sum-=max
+    for (const salary of salaries){
+        sum += salary
+    }
+    sum /= (salaries.length-2)
+    return sum
+    let avg= salaries.reduce((sum, curr) => sum + curr, 0) / (salaries.length-2)
     return avg
 }
 
