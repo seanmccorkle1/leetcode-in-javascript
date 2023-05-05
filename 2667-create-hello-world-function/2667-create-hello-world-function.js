63a35ae26178be743010b7d157b6b3d1b2c70f88
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-05-05 10:43:47 -0400

    Time: 59 ms (100.00%), Space: 42 MB (100.00%) - LeetHub

--- 2667-create-hello-world-function/2667-create-hello-world-function.js
+++ 2667-create-hello-world-function/2667-create-hello-world-function.js
@@ -1,4 +1,4 @@
-var createHelloWorld =p=>() => "Hello World"
+var createHelloWorld =p=>() => "hello world".split(" ").map(word => word[0].toUpperCase() +word.slice(1)).join(" ")
 
 const f = createHelloWorld()
 f(); // "Hello World"
