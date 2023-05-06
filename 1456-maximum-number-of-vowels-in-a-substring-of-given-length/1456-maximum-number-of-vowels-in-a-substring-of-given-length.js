858bbc8fb7b50bf6b1deade7e3d77ecd94bbb8f4
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-05-06 05:33:11 -0400

    Time: 95 ms (63.67%), Space: 45.9 MB (42.56%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.js
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.js
@@ -1,31 +1,48 @@
-const maxVowels = (s, k) => {
+const maxVowels = (string, k) => {
     
-    const VOWEL = new Set(['a','e','i','o','u']);
+    let max = 0    
+    const r=/[aeiou]/
+    
+    const VOWEL = new Set(['a','e','i','o','u'])    
 
-    let max = 0
+    if (k == 1 &&string.match(r)) {
+        return 1
+    }
     
-    for (let i = 0; i < k; ++i) {
+    for (let index = 0;  index < k; index++) {
+
+        const letter=string[index]
+        const letterIsVowel = VOWEL.has(letter)
         
-        if (VOWEL.has(s[i])){
+        if (letterIsVowel) {
             max++
         }
         
   }
     
-    for (let i = 0, cur = max; i < s.length - k; i++) {    
+    string
+    
+    let cur = max
+    
+    for (let index = 0; index < string.length - k; index++) {    
         
-        if (VOWEL.has(s[i])) {
+        const letter = string[index]
+        const rightLetter =string[index + k] 
+        
+        if (VOWEL.has(letter)) {
             cur--
         }
         
-        if (VOWEL.has(s[i + k])) {
+        if (VOWEL.has(rightLetter)) {
             cur++
         }
         
+        
+        max = Math.max(cur, max)
         if (cur== k){
             return cur
         }
-        max = Math.max(cur, max)
+
     }
     
     return max
