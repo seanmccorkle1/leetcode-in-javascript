8747b4771bf682257bad69a11da0ffd7abc41161
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-05-06 04:40:38 -0400

    Time: 53 ms (79.44%), Space: 42.1 MB (35.65%) - LeetHub

--- 2620-counter/2620-counter.js
+++ 2620-counter/2620-counter.js
@@ -1,14 +1,13 @@
 /**
- * @param {number} n
+ * @param {number} num
  * @return {Function} counter
  */
 
 // the return value is a function
 var createCounter = num => () => num++
 
-/** 
- * const counter = createCounter(10)
- * counter() // 10
- * counter() // 11
- * counter() // 12
- */
+const counter = createCounter(10)
+
+counter() // 10
+counter() // 11
+counter() // 12
