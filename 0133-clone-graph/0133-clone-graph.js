72c3fbab37a54ab8f3ba1a695279e1b1b7e65e4e
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-05-07 17:29:46 -0400

    Time: 64 ms (48.25%), Space: 43.8 MB (51.38%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.js
+++ 0133-clone-graph/0133-clone-graph.js
@@ -12,19 +12,20 @@
  */
 
 var cloneGraph = function(node) {
-    
-    if (node === null) {
-        return null
-    }    
-    const map = new Map();
-    
-    const clone = root => {
+        const clone = root => {
         
         if (!map.has(root.val)) {
             map.set(root.val, new Node(root.val));
             map.get(root.val).neighbors = root.neighbors.map(clone);
         }
+        
         return map.get(root.val)
     }
+    if (node === null) {
+        return null
+    }    
+    const map = new Map();
+    
+
     return clone(node)
 }
