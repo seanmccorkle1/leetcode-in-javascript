b2d47be49750a9e69ea3444d614d22dab0b47af3
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-05-06 21:44:00 -0400

    Time: 100 ms (45.19%), Space: 44.8 MB (96.45%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.js
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.js
@@ -1,44 +1,44 @@
-const maxVowels = (string, k) => {
+var maxVowels = function(s, k) {
     
-    let max = 0    
+    const vowels = ['a', 'e', 'i', 'o', 'u']
 
-    const VOWEL = new Set(['a','e','i','o','u'])        
+                    let maxCount = 0;
+    let start = 0; // the left edge of the window
+    let count = 0; // count of vowels for current substring
     
-    for (let index = 0;  index < k; index++) {
+    // expanding the right edge of the window one character at a time
 
-        const letter=string[index]
-        const letterIsVowel = VOWEL.has(letter)
+    for (let end = 0; end < s.length; end++) {
         
-        if (letterIsVowel) {
-            max++
+        if (vowels.includes(s[end])) {
+            count++
         }
         
-  }
-    
-    string
-    
-    let cur = max
-    
-    for (let index = 0; index < string.length - k; index++) {    
+        // if substring is longer than K, let's shrink the window by moving left edge
         
-        const letter = string[index]
-        const rightLetter =string[index + k] 
-        
-        if (VOWEL.has(letter)) {
-            cur--
+        if (end - start + 1 > k) {
+            
+            // reduce the current count by one 
+            // if the character on the left edge is vowe
+            
+            if (vowels.includes(s[start])) {
+                count--
+            }
+            
+            //shrinking the left edge of the window
+            start++
         }
         
-        if (VOWEL.has(rightLetter)) {
-            cur++
-        }
+		// checking if current count is larger
+        // than current maximum count
         
+        maxCount = Math.max(count, maxCount)
         
-        max = Math.max(cur, max)
-        if (cur== k){
-            return cur
+        // if maxCount is equal to K, no need to check further, it is the max possible count
+        
+        if (maxCount == k) {
+            return maxCount
         }
-
     }
-    
-    return max
+    return maxCount
 }
