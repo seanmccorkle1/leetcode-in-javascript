2915ccd937f665759bc4586678d9b357ec4979ba
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-05-06 21:51:20 -0400

    Time: 64 ms (70.46%), Space: 44.5 MB (74.53%) - LeetHub

--- 2665-counter-ii/2665-counter-ii.js
+++ 2665-counter-ii/2665-counter-ii.js
@@ -11,7 +11,7 @@ var createCounter = function(init) {
         return c
     }
     
-    const functionObj= {increment, decrement, reset}
+    const functionObj= {reset,increment, decrement}
     return functionObj
 }
 
