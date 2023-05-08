fe8f5ef0f2d1aad5fa5f5ce3b2eee753996e4205
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-05-07 23:01:17 -0400

    Time: 63 ms (27.39%), Space: 42.3 MB (24.84%) - LeetHub

--- 1880-check-if-word-equals-summation-of-two-words/1880-check-if-word-equals-summation-of-two-words.js
+++ 1880-check-if-word-equals-summation-of-two-words/1880-check-if-word-equals-summation-of-two-words.js
@@ -3,16 +3,9 @@ var isSumEqual = function(firstWord, secondWord, targetWord) {
     let firstWordDigitString = ""
     let secondWordDigitString = ""
     let targetDigitString = ""
-
-    let array = new Array(10).fill("a").map(e => [])
-    array = array.map((subarray, index) => [String.fromCharCode(97+index), index])
-
-    const obj=array.reduce((sum, curr) => ({...sum, [curr[0]] : curr[1]}), {})
-
-    // const o = array.reduce((obj, curr) => ({...obj, [curr[0]]: curr[1]}),{})
-    // const obj = {"a" : '0', "b" : '1', "c" : '2', "d" : '3', "e" : '4', "f" : '5', "g" : '6', "h" : '7', 'i' : '8', "j" : '9'}
-      
-      
+    
+    const obj = {"a" : '0', b : '1', "c" : '2', "d" : '3', "e" : '4', "f" : '5', "g" : '6', "h" : '7', 'i' : '8', "j" : '9'}
+    
       const greaterLength= Math.max(firstWord.length, secondWord.length, targetWord.length)
       
       for (let index=0; index < greaterLength; index++) {
@@ -23,21 +16,20 @@ var isSumEqual = function(firstWord, secondWord, targetWord) {
           
           let stringDigit1 = obj[firstWordLetter] ?? ""
           let stringDigit2 = obj[secondWordLetter] ?? ""
-          let targetDigit = obj[targetLetter] ?? ""
+          let targetDigit =obj[targetLetter] ?? ""
           
           firstWordDigitString += stringDigit1
           secondWordDigitString+= stringDigit2
           targetDigitString += targetDigit
       }
-      
+          
       let twoWordSum = Number(firstWordDigitString) + Number(secondWordDigitString)
+      
       const targetSum = Number(targetDigitString)
-  
+      
       if (twoWordSum == targetSum){
           return true
       }
-      return false
-      
-  }
-  
-  console.log(isSumEqual("acb", "cba", "cdb"), true)
+          return false
+}
+console.log(isSumEqual("acb", "cba", "cdb"), true)
