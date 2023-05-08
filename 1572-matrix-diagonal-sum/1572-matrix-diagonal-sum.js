eb552511b1ef2610481f287f51db8bc89d1073ae
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-05-07 21:55:06 -0400

    Time: 47 ms (99.22%), Space: 44.1 MB (33.80%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.js
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.js
@@ -3,7 +3,16 @@ var diagonalSum = function(matrix) {
     let backIndex = matrix.length - 1
     
     let diagonalSum = 0
+
+        const matrixIsOdd = (matrix.length % 2) == 1
     
+    let midIndex = Math.floor(matrix.length / 2)
+    let midValue = matrix[midIndex][midIndex]
+    
+    if (matrixIsOdd) {
+        diagonalSum -= midValue
+    }
+
     for (let index = 0; index < matrix.length; index++) {
 
         const primaryDiagonalValue = matrix[index][index]
@@ -14,21 +23,11 @@ var diagonalSum = function(matrix) {
 
         backIndex--
     }
-
-    const matrixIsOdd = (matrix.length % 2 == 1) ? true : undefined ?? ""
     
     // if its an odd-length matrix, 
+    
     // 5 was repeated as [5, 5]
     // remove it once to just get 5
-    
-    // let midIndex = Math.floor(matrix.length / 2)
-    
-    let midIndex = matrix.length / 2 -.5
-
-    if (matrixIsOdd) {
-        const midValue = matrix[midIndex][midIndex]
-        diagonalSum -= midValue
-    }
     
     return diagonalSum
 }
