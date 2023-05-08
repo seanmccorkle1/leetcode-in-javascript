27c155f9c2dc7587b1ddd132d579db9e193c1e91
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-05-07 20:57:28 -0400

    Time: 55 ms (87.29%), Space: 44.4 MB (12.40%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.js
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.js
@@ -1,28 +1,34 @@
-var diagonalSum = function(mat) {
-    
+var diagonalSum = function(matrix) {
+
     // side length
-    let n = mat.length;
-    
     // mid point index
-    let mid = Math.floor( n / 2 );
-    
+    let mid = Math.floor(matrix.length / 2)
+
     // summation of diagonal element
-    let summation = 0;
-    
-    for( let i = 0 ; i < n ; i++ ){
-        
+    let summation = 0
+    let backIndex = matrix.length - 1
+
+    for (let index = 0; index < matrix.length; index++) {
+
+        const currValue = matrix[index][index]
+
         // primary diagonal
-        summation += mat[i][i];
-        
+        summation += currValue
+
         // secondary diagonal
-        summation += mat[n-1-i][i];
+        summation += matrix[backIndex][index]
+        backIndex--
     }
-    
-    if( n % 2 == 1 ){
-        
+
+    if( matrix.length % 2 == 1 ){
+
         // remoive center element (repeated) on odd side-length case
-        summation -= mat[mid][mid];
+        summation -= matrix[mid][mid];
     }
-    
+
     return summation;
-};
+}
+console.log(diagonalSum(
+    [[1,2,3],
+    [4,5,6],
+    [7,8,9]]), 25)
