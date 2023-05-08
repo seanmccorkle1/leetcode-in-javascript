635f60ba03f724815f151c71832aef68b21f3047
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-05-07 22:54:03 -0400

    Time: 52 ms (87.26%), Space: 42.7 MB (8.28%) - LeetHub

--- 1880-check-if-word-equals-summation-of-two-words/1880-check-if-word-equals-summation-of-two-words.js
+++ 1880-check-if-word-equals-summation-of-two-words/1880-check-if-word-equals-summation-of-two-words.js
@@ -1,43 +1,42 @@
 var isSumEqual = function(firstWord, secondWord, targetWord) {
-    
-    const obj = {
-        "a" : '0', "b" : '1', "c" : '2', "d" : '3', "e" : '4', "f" : '5', "g" : '6', "h" : '7', 'i' : '8', 
-        "j" : '9'
-    }
-    
-    let first = ""
-    let second = ""
-    let target = ""
-    
-    firstWord = firstWord.toLowerCase()
-    secondWord=secondWord.toLowerCase()
-    targetWord=targetWord.toLowerCase()
-    
-    const greaterLength= Math.max(firstWord.length, secondWord.length, targetWord.length)
-    
-    for (let index=0; index < greaterLength; index++) {
-        
-        let firstWordLetter = firstWord[index]
-        let secondWordLetter = secondWord[index]
-        let targetLetter = targetWord[index]
-        
-        let stringDigit1 = obj[firstWordLetter] ?? ""
-        let stringDigit2 = obj[secondWordLetter] ?? ""
-        let targetDigit = obj[targetLetter] ?? ""
-        
-        first += stringDigit1
-        second+= stringDigit2
-        target += targetDigit
-    }
-    
-    let twoWordSum = Number(first) + Number(second)
-    const targetSum = Number(target)
+  
+    let firstWordDigitString = ""
+    let secondWordDigitString = ""
+    let targetDigitString = ""
 
-    if (twoWordSum == targetSum){
-        return true
-    }
-    return false
-    
-}
+    let array = new Array(10).fill("a").map(e => [])
+    array = array.map((subarray, index) => [String.fromCharCode(97+index), index])
 
-console.log(isSumEqual("acb", "cba", "cdb"), true)
+    const obj=array.reduce((sum, curr) => ({...sum, [curr[0]]: curr[1]}), {})
+    
+    // const obj = {"a" : '0', "b" : '1', "c" : '2', "d" : '3', "e" : '4', "f" : '5', "g" : '6', "h" : '7', 'i' : '8', "j" : '9'}
+      
+      
+      const greaterLength= Math.max(firstWord.length, secondWord.length, targetWord.length)
+      
+      for (let index=0; index < greaterLength; index++) {
+          
+          let firstWordLetter = firstWord[index]
+          let secondWordLetter = secondWord[index]
+          let targetLetter = targetWord[index]
+          
+          let stringDigit1 = obj[firstWordLetter] ?? ""
+          let stringDigit2 = obj[secondWordLetter] ?? ""
+          let targetDigit = obj[targetLetter] ?? ""
+          
+          firstWordDigitString += stringDigit1
+          secondWordDigitString+= stringDigit2
+          targetDigitString += targetDigit
+      }
+      
+      let twoWordSum = Number(firstWordDigitString) + Number(secondWordDigitString)
+      const targetSum = Number(targetDigitString)
+  
+      if (twoWordSum == targetSum){
+          return true
+      }
+      return false
+      
+  }
+  
+  console.log(isSumEqual("acb", "cba", "cdb"), true)
