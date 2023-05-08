9af89768d4c20c5a5c9d67d36a4f90b85f1036ea
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-05-07 20:41:16 -0400

    Time: 56 ms (66.24%), Space: 42.5 MB (16.56%) - LeetHub

---
+++ 1880-check-if-word-equals-summation-of-two-words/1880-check-if-word-equals-summation-of-two-words.js
@@ -0,0 +1,52 @@
+var isSumEqual = function(firstWord, secondWord, targetWord) {
+    
+    const obj = {
+        'a' : '0',
+        "b" : '1',
+        "c" : '2',
+        "d" : '3',
+        "e" : '4',
+        'f' : '5',
+        'g' : '6',
+        "h" : '7',
+        'i' : '8', 
+        "j" : '9'
+    }
+    
+    let first = ""
+    let second = ""
+    let target = ""
+    
+    const greaterLength= Math.max(firstWord.length, secondWord.length, targetWord.length)
+    
+    for (let index=0; index < greaterLength; index++) {
+        
+        let firstWordLetter = (firstWord[index] ?? "")
+        let secondWordLetter = (secondWord[index] ?? "")
+        let targetLetter = targetWord[index] ?? ""
+        
+        let stringDigit1 = obj[firstWordLetter] ?? ""
+        let stringDigit2 = obj[secondWordLetter] ?? ""
+        let targetDigit = obj[targetLetter] ?? ""
+        
+        first += stringDigit1
+        second+= stringDigit2
+        target += targetDigit
+    }
+    
+    //     for(let char of firstWord){        
+    
+//         first += obj[char]
+//     }
+//     for(let char of secondWord){
+//         second += obj[char]
+//     }
+    
+    // for(let char of targetWord){
+    //     target += obj[char]
+    // }
+    
+    return parseInt(first) + parseInt(second) === parseInt(target)
+}
+
+isSumEqual("acb", "cba", "cdb")
