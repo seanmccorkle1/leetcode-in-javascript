75d3b3e38ba972322fc3405803ab8c5e8f6d7c6e
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-05-08 15:21:29 -0400

    Time: 114 ms (14.45%), Space: 42.4 MB (98.79%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.js
+++ 0069-sqrtx/0069-sqrtx.js
@@ -1,15 +1,17 @@
 
 var mySqrt = function(num) {
+    
     if (num == 0){
         return 0
     }
+    
     let increment = 1
     
     while(Math.pow(increment, 2) <= num) {
         increment += 1
     }
     
-    return increment - 1
+    return increment -1
 }
 
 console.log(mySqrt(8), 2, "the square root is 2.8, truncate it to 2")
