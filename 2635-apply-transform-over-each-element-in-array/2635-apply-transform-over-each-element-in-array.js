e86c4874dd75e986ee14425e3ae90da8d1d2aa6b
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-05-07 23:26:20 -0400

    Time: 67 ms (9.40%), Space: 41.6 MB (90.84%) - LeetHub

--- 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
+++ 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
@@ -6,14 +6,16 @@ var map = function(array, mapFn) {
         
         // index just gets left out if "num" is the only argument
         
-        const mappedNum = mapFn(array[index], index)
-
-        mappedArray[index] = mappedNum
+        const num = array[index]
+        const numAfterFunction = mapFn(num, index)
+        
+        mappedArray[index] = numAfterFunction
   }
+    
     return mappedArray
 }
 
-const mapFunction = (num)=> {return num + 1}
-console.log(map([1,2,3], mapFunction),  [1,3,5])
+const mapFunction = (num, index)=> {return num + index}
+console.log(map([1,2,3], mapFunction),  [2,3,4])
 
 map([1,2,3], (num)=> {return num + 1})
