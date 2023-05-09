2e185f2306138e5cc44a2afb33b17efa7b4ba8ba
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-05-08 20:46:44 -0400

    Time: 64 ms (22.80%), Space: 41.8 MB (65.45%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.js
+++ 0054-spiral-matrix/0054-spiral-matrix.js
@@ -5,33 +5,38 @@ var spiralOrder = function(matrix) {
     while (matrix.length != 0) {
         
         const firstArray = matrix.shift()
-
-        // array.push(...[1,2]) to keep it a single array 
+        
+        // use ... to keep it as a single array 
         array.push(...firstArray)
-
+        
         for (let subarray of matrix) {
             
             // pop modifies the subarrays
-            let pop = subarray.pop()
-
-            if (pop !== undefined) {
+            if (subarray.length >= 1){
+                let pop = subarray.pop()
                 array.push(pop)
             }
+
+            // if (pop != undefined){
+            //     array.push(pop)
+            // }
             
             // reverse is destructive
-            subarray = subarray.reverse()
+            subarray.reverse()
     }
-        matrix =matrix.reverse()
+        
+        matrix.reverse()
   }
-  return array
+    return array
 }
- 
+
 // push [8,7] to the top to get it firstArray on the next iteration
- 
+
 // [8,7]
 // [5,4]
 
-console.log(spiralOrder([
+console.log(
+    spiralOrder([
     [1,2,3],
     [4,5,6],
     [7,8,9]]), [1,2,3,6,9,8,7,4,5])
