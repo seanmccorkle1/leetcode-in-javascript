fd44fcb9949c51ef66bd44c4375618158e8f88f0
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-05-09 22:07:28 -0400

    Time: 57 ms (80.08%), Space: 42.3 MB (68.42%) - LeetHub

--- 2626-array-reduce-transformation/2626-array-reduce-transformation.js
+++ 2626-array-reduce-transformation/2626-array-reduce-transformation.js
@@ -2,12 +2,20 @@ var reduce = function(nums, reduceFn, initialValue) {
     
     let currSum = initialValue
 
-    for (let index = 0; index < nums.length; index++) {
+    const arrayLength=nums.length
+
+    for (let index = 0; index < arrayLength; index++) {
         const currNum = nums[index]
+        
+        // if currSum is on BOTH sides, dont use +=
+        // this is an = case
+        
         currSum = reduceFn(currSum, currNum)
     }
+    
     return currSum
 }
 
 let initValue= 1
-console.log(reduce([2,3,4,5], (sum, curr) => sum + curr,  initValue),   15)
+
+console.log(reduce([2,3,4,5], (sum, curr) => sum + curr, 12),    26)
