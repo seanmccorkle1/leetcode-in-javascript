f3ee7edfd80f4859788fe7b147a9f431a44c6ba2
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-05-10 20:15:53 -0400

    Time: 55 ms (76.13%), Space: 42 MB (53.46%) - LeetHub

--- 0059-spiral-matrix-ii/0059-spiral-matrix-ii.js
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.js
@@ -2,49 +2,53 @@ var generateMatrix = function(n) {
     
     let output = new Array(n).fill(0).map((zero) => new Array(n).fill(0))
     
-    let count = 0;    
-
-    let size = n * n;
+    let count = 0
+    let size = n * n
     
     let left = 0;
+    let right = n - 1
     
-    let right = n - 1;
+    let top = 0
     
-    let top = 0;
-    
-    let bottom = n -1;
+    let bottom = n -1
     
     while(count < size){
         
         //going left
+        
         for(let i = left; i <= right; i++){
             count++;
             output[top][i] = count;
         }
-        top++;
-                
+
+        top++
+        
         // going down
-        for(let i = top; i <= bottom; i++){
-            count++;
-            output[i][right] = count;
-        }
-        right--;
         
+        for(let i = top; i <= bottom; i++) {
+            count++
+            output[i][right] = count
+        }
+
+        right--
+
         //going left
-        for(let i = right; i >= left; i--){
-            count++;
-            output[bottom][i] = count;
+
+        for (let i = right; i >= left; i--){
+            count++
+            output[bottom][i] = count
         }
-        bottom--;
-        
+
+        bottom--
+
         //going up
-        for(let i = bottom; i >= top; i--){
-            count++;
-            output[i][left] = count;
+        
+        for(let i = bottom; i >= top; i--) {
+            count++
+            output[i][left] = count
         }
-        left++;
+        left++
     }
     
-    return output;
-    
+    return output    
 }
