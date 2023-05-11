04fdb8529bd4dd9bf299133ab170460a139a1b71
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-05-11 16:40:40 -0400

    Time: 63 ms (49.32%), Space: 41.7 MB (87.06%) - LeetHub

--- 0050-powx-n/0050-powx-n.js
+++ 0050-powx-n/0050-powx-n.js
@@ -1,39 +1,39 @@
 var myPow = function (num, exp) {
     
+    const recursivePower = (x, exponent) => {
+    
+        if (exponent == 0) {
+            return 1
+        }
+    
+        if (x == 0) {
+            return 0
+        }
+    
+        const exponentIsEven = (exponent % 2) == 0
+
+        if (exponentIsEven) {
+        
+            const returnValue = recursivePower(x, exponent / 2) 
+            return Math.pow(returnValue, 2)
+        }
+        
+        else if (!exponentIsEven) {
+            exponent = (exponent - 1) / 2
+        
+            const pow = Math.pow(recursivePower(x, exponent), 2)
+            return pow * x
+        }
+        
+        
+    }
+    
     const absExp = Math.abs(exp)
     const poweredNum = recursivePower(num, absExp)
-    
+
     if (exp <= -1) {
         return Math.abs(1/poweredNum) // abs not needed
     }
-    
+
     return poweredNum
-}
-
-var recursivePower = (x, exponent) =>{
-    
-    if (exponent == 0) {
-        return 1
-    }
-    
-    if (x == 0) {
-        return 0
-    }
-    
-    const exponentIsEven = (exponent % 2) == 0
-
-    if (exponentIsEven) {
-        
-        const returnValue = recursivePower(x, exponent / 2) 
-        return Math.pow(returnValue, 2)
-  }
-
-    else if (!exponentIsEven) {
-        
-        exponent = (exponent - 1) / 2
-        
-        const pow = Math.pow(recursivePower(x, exponent), 2)
-        return pow * x
-        
-    }
 }
