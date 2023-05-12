dca2d02fce65b011b631efb831027431f31d4714
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-05-12 18:27:14 -0400

    Time: 106 ms (56.19%), Space: 48.4 MB (23.57%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.js
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.js
@@ -1,28 +1,14 @@
-
-
 var firstUniqChar = function (string) {
     
     const array = [...string]
     
-    let firstFind = array.find(letter => string.indexOf(letter )== string.lastIndexOf(letter))
+    let firstUniqueLetter = array.find(letter => string.indexOf(letter )== string.lastIndexOf(letter))
     
-    return firstFind ? array.indexOf(firstFind) : -1
-    if (string.length == 1){
-        return 0
-    }
-    
-    for (let index = 0; index < string.length; index++) {
-
-        const letter= string[index]
-        
-        const stringOnlyHas1Index = string.indexOf(letter) == string.lastIndexOf(letter)
-        
-        if (stringOnlyHas1Index) {
-            return index
-        }
+    if (firstUniqueLetter){
+        let indexOfFirstUnique = array.indexOf(firstUniqueLetter)
+        return indexOfFirstUnique
     }
     
     return -1
 }
-
 console.log(firstUniqChar("leitcode"), 0, "'l' is the first unique")
