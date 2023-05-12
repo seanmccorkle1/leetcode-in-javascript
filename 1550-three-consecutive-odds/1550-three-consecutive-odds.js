5caa551b7cdcfa43cc178fbee64907a7fc5e5bf1
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-05-12 18:04:40 -0400

    Time: 62 ms (34.71%), Space: 41.4 MB (93.53%) - LeetHub

--- 1550-three-consecutive-odds/1550-three-consecutive-odds.js
+++ 1550-three-consecutive-odds/1550-three-consecutive-odds.js
@@ -1,5 +1,8 @@
 
 var threeConsecutiveOdds = function(inputArray) {
+    if (inputArray.filter(num => num%2==1).length <= 2){
+        return false
+    }
     
     for (let index = 2; index < inputArray.length; index++) {
         
