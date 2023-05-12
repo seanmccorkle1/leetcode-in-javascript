e265851388861fc5049912c559202bad1a6f9b80
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-05-12 14:46:55 -0400

    Time: 52 ms (93.47%), Space: 44.2 MB (28.42%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.js
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.js
@@ -1,39 +1,33 @@
 var diagonalSum = function(matrix) {
     
-    let backIndex = matrix.length - 1
-    
     let diagonalSum = 0
+    let backIndex = matrix.length - 1
 
-        const matrixIsOdd = (matrix.length % 2) == 1
+    const matrixIsOdd = (matrix.length % 2) == 1 ? true : false
     
     let midIndex = Math.floor(matrix.length / 2)
-    let midValue = matrix[midIndex][midIndex]
+    let midCell = matrix[midIndex][midIndex]
     
-    if (matrixIsOdd) {
-        diagonalSum -= midValue
+    if (matrixIsOdd){
+        diagonalSum -= midCell
     }
+    
+    for (let index=0; index<  matrix.length; index++){
+        let primaryDiagonalValue =  matrix[index][index]
+        let secondary = matrix[backIndex][index]
 
-    for (let index = 0; index < matrix.length; index++) {
-
-        const primaryDiagonalValue = matrix[index][index]
-        const secondaryDiagonalValue = matrix[backIndex][index]
-
-        diagonalSum += primaryDiagonalValue
-        diagonalSum += secondaryDiagonalValue        
-
+        
+        diagonalSum+=primaryDiagonalValue
+        diagonalSum+=secondary
+        
         backIndex--
     }
-    
-    // if its an odd-length matrix, 
-    
-    // 5 was repeated as [5, 5]
-    // remove it once to just get 5
-    
     return diagonalSum
 }
 
-console.log(diagonalSum(
-   [[1,2,3],
-    [4,5,6],
-    [7,8,9]])
-    , 25)
+console.log(
+    diagonalSum(
+        [[1,2,3],
+         [4,5,6],
+         [7,8,9]])
+         , 25)
