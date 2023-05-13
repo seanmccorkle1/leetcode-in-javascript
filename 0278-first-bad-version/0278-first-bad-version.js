12c7d644acc7280f811b3dbdc8bbb4010848545e
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-05-13 16:51:57 -0400

    Time: 47 ms (95.58%), Space: 41.6 MB (65.89%) - LeetHub

---
+++ 0278-first-bad-version/0278-first-bad-version.js
@@ -0,0 +1,25 @@
+var solution = function(isBadVersion) {
+    
+    return function(n) {
+        
+        let start = 1
+        let end = n
+        
+        while(start < end){
+            
+            let mid = Math.floor(start + (end - start) / 2)
+            
+            // go down to the FIRST bad version
+            
+            if (isBadVersion(mid)){
+                end = mid
+           }
+            
+            // if its a good version, go right to the first bad version
+            else{
+                start = mid + 1
+           }
+       }
+       return start
+    }
+}
