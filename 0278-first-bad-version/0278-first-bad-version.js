b32a56bd06c98030b3e02799c4f9e0e58b54f308
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-05-13 16:57:58 -0400

    Time: 60 ms (48.94%), Space: 41.6 MB (76.18%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.js
+++ 0278-first-bad-version/0278-first-bad-version.js
@@ -1,19 +1,17 @@
 var solution = function(isBadVersion) {
     
-    return function xd(num) {
-
+    return function(num) {
+        
         // num isnt an index, 
         // so use [1, num] as left and right bounds
-
+        
         let leftBound = 1
         let rightBound = num
-
+        
         while(leftBound < rightBound){
             
-            // let midIndex = Math.floor(leftBound + (rightBound - leftBound) / 2)
-            
             let midIndex = Math.floor((leftBound + rightBound) / 2)
-
+            
             // go down to the FIRST bad version
             
             if (isBadVersion(midIndex)){
@@ -21,10 +19,13 @@ var solution = function(isBadVersion) {
            }
 
             // if its a good version, go right to the first bad version
-            else {
+            // the asymmetric (left + 1) has to be on left
+            
+            else {                 
                 leftBound = midIndex + 1
            }
        }
-       return leftBound
+       // return leftBound
+        return rightBound
     }
 }
