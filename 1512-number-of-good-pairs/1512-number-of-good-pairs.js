9538bf6491cf31be1f6991f5c720f2f7f9d33dd8
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-05-13 18:24:16 -0400

    Time: 68 ms (10.97%), Space: 41.7 MB (82.17%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.js
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.js
@@ -1,28 +1,33 @@
+
+
 var numIdenticalPairs = function (array) {
 
-    const map = new Map()
+    const obj = {}
     let count = 0
 
-    for (let number of array){ 
-        
-        const valueExists = map[number] ? true : false
-        
+    for (let number of array){
+
+        const valueExists = obj[number] ? true : false
+        const frequency = obj[number]
+
         if (valueExists) {
-            count += map[number]
-            map[number] += 1
-            
-            map
+
+            count += frequency
+
+            // update the data structure after the counter
+            obj[number] += 1 
+
+            obj
         }
-        
-        else if (!valueExists) {            
-            map[number] = 1
-            map
+
+        else if (!valueExists) {
+            obj[number] = 1
+            obj
         }
     }
-    map        
 
+    obj
     return count
-    
 }
 
 console.log(numIdenticalPairs([1,2,3,1,1,3]),   4, "(1, 1) (1, 1) (1, 1) and (3, 3)")
