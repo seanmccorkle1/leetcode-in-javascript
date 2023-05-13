44493fc2ac32e8ad4e37c82148928578f37be4e6
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-05-13 00:31:40 -0400

    Time: 107 ms (19.79%), Space: 43.8 MB (59.92%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.js
+++ 0069-sqrtx/0069-sqrtx.js
@@ -1,20 +1,12 @@
 var mySqrt = function(num) {
     
-    if (num == 0){
-        return 0
+    let base = 0
+    
+    while (Math.pow(base, 2) <= num){
+        base++
     }
-    
-    let increment = 1
-    
-    while(Math.pow(increment, 2) <= num) {
-        increment += 1
-    }
-    
-    // do one extra iteration with (pow <= num)
-    // then cancel it with increment - 1
-    
-    return increment - 1
+    return base - 1
 }
 
-console.log(mySqrt(8), 2, "the square root is 2.8, truncate it to 2")
-console.log(mySqrt(0), 0)
+console.log(mySqrt(8), 2, "the square root of 8 is 2.8, truncate it to 2")
+// console.log(mySqrt(0), 0)
