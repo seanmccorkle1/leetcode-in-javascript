3be9ba2c4d5a98efae721a9a3cb874f872418fde
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-05-12 20:33:23 -0400

    Time: 68 ms (100.00%), Space: 44.3 MB (100.00%) - LeetHub

--- 2677-chunk-array/2677-chunk-array.js
+++ 2677-chunk-array/2677-chunk-array.js
@@ -1,24 +1,14 @@
 var chunk = function(array, size) {
-
+    
     const chunkedArray = []
-    let index = 0
     
-    const a = [] 
-    while (array.length != 0){
+    array
+    size
 
+    while (array.length != 0){        
         let chunk = array.splice(0, size)
-        a.push(chunk)
+        chunkedArray.push(chunk)
     }
-    return a
-    while (index < array.length) {
-      
-      let endIndex = index + size 
-      array.slice(index, endIndex)
-      
-      chunkedArray.push(array.slice(index, index + size));
-      index += size
-  }
     
-  return chunkedArray;
+    return chunkedArray
 }
-
