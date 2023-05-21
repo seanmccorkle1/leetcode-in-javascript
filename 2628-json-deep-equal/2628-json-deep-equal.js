b002692cac5e5fb6c614dd4f840cdaddf981600e
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-05-20 20:41:10 -0400

    Time: 79 ms (51.13%), Space: 47.8 MB (35.79%) - LeetHub

--- 2628-json-deep-equal/2628-json-deep-equal.js
+++ 2628-json-deep-equal/2628-json-deep-equal.js
@@ -6,18 +6,37 @@ var areDeeplyEqual = function(o1, o2) {
     if (typeof o1 != 'object' || typeof o2 != 'object') return false;
 
     // Both of them should be objects or arrays
-    if (Array.isArray(o1) !== Array.isArray(o2)) return false;
+    
+    const arraysDontMatch = Array.isArray(o1) !== Array.isArray(o2)
+    
+    if (arraysDontMatch) {
+        return false
+    }
     
     // Both should have same keys, in case of indexes this will return indexes
-    if (Object.keys(o1).length != Object.keys(o2).length) return false;
-
+    
+    const lengthsDontMatch = Object.entries(o1).length != Object.entries(o2).length
+    
+    if (lengthsDontMatch){
+        return  false
+    }
+    // if (Object.keys(o1).length != Object.keys(o2).length) {
+    //     return false
+    // }
+    
     // Check if all values against keys of o1 and o2 are deeply equal.
     // If number of keys are same, then at a different key in objects would mean at least
     // 1 value against the key of o2 will be undefined
-    for (const key in o1) {
-        if (areDeeplyEqual(o1[key], o2[key])== false) return false;
-    }
 
-    // All checks passed
-    return true;
-};
+    for (const key in o1) {
+        
+        const keysArentEqual = !areDeeplyEqual(o1[key], o2[key])
+        
+        if (keysArentEqual) {
+            return false
+        }
+    }
+    
+    // All false checks passed
+    return true
+}
