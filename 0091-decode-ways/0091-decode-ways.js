76f3e874c652f97a44ee2c8b597a0d88c80b3df9
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-05-23 11:55:12 -0400

    Time: 99 ms (13.95%), Space: 46.2 MB (13.06%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.js
+++ 0091-decode-ways/0091-decode-ways.js
@@ -1,44 +1,33 @@
 var numDecodings = function (stringnum) {
     
-  // if (stringnum == undefined || stringnum === "") {
-  //     return 0
-  // }
-    
     if (stringnum[0] === '0') {
         return 0
     }
     
-    // only 1 way to decode a single-digit number
+    const array = new Array(stringnum.length + 1).fill(0)
+    
+    array[0] = 1
+    array[1] = 1
+    
+    for (let index = 2; index <= stringnum.length; index++) {
 
-    if (stringnum.length==1) {
-        return 1 
+        let singleDigit = Number(stringnum.slice(index - 1, index))
+
+        if (singleDigit != 0) {
+            array[index] += array[index - 1]
+        }
+        
+        let twoDigits = Number(stringnum.slice(index - 2, index))
+
+        if (twoDigits >= 10 && twoDigits <= 26) {
+            array[index] += array[index - 2]
+        }
     }
-    
-  const array = new Array(stringnum.length + 1).fill(0)
 
-  array[0] = 1
-  array[1] = 1
-    
-  
-  for (let index = 2; index <= stringnum.length; index++) {
-      
-      let singleDigit = Number(stringnum.slice(index - 1, index))
-    
-      if (singleDigit >= 1 && singleDigit <= 9) {
-          array[index] += array[index - 1]
-      }
-    
-      let twoDigits= Number(stringnum.slice(index - 2, index))
-      
-      if (twoDigits >= 10 && twoDigits <= 26) {
-          array[index] += array[index - 2]
-      }
-  }
-  
-  console.log(array)
-  return array.at(-1)
+    console.log(array)
+    return array.at(-1)
 }
 
-console.log(numDecodings("12"), 2,  '"12" could be decoded as "AB" (1 2) or "L" (12)')
+console.log(numDecodings("12"), 2, '"12" could be decoded as "AB" (1 2) or "L" (12)')
 
 // console.log(numDecodings("05"),  0, "starting with a 0 is invalid, ending with a 0 (eg. 20) is fine")
