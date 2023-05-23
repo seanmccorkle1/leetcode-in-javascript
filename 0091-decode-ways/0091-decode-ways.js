ec10b2c0fb62f17683680c1c6e9f4c9dfd4b2cad
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-05-23 14:13:35 -0400

    Time: 89 ms (16.17%), Space: 46.2 MB (13.35%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.js
+++ 0091-decode-ways/0091-decode-ways.js
@@ -7,30 +7,30 @@
 
 var numDecodings = function (stringnum) {
 
-    if (stringnum[0] === '0') {
+    if (stringnum[0] == '0') {
         return 0
     }
     
     // its not a direct "map" like the spells and potions problem,
     // also its a DP problem that uses array.at(-1) at the end
     // which is why its length + 1    
-
+    
     const array = new Array(stringnum.length + 1).fill(0)
     
     // "A is the lowest unit and starts at 1, 0 isnt valid"    
     array[0] = 1
     array[1] = 1
-
     
     // index IS slice's endIndex, so using < would mean the last letter gets cut off
+    // always start this index at lastInitIndex + 1
     
     for (let index = 2; index <= stringnum.length; index++) {
-
+        
         let singleDigitNum = Number(stringnum.slice(index - 1, index))
 
         // A is the lowest and starts at 1, 0 isnt valid
         if (singleDigitNum != 0) {
-            array[index] += array[index - 1]
+            array[index] =array[index - 1]
         }
         
         let twoDigitNum = Number(stringnum.slice(index - 2, index))
