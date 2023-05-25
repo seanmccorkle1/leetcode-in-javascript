122e3fb54da22238d92d80e2546fd59003a60533
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-05-24 22:16:38 -0400

    Time: 60 ms (60.00%), Space: 41.6 MB (92.31%) - LeetHub

--- 1694-reformat-phone-number/1694-reformat-phone-number.js
+++ 1694-reformat-phone-number/1694-reformat-phone-number.js
@@ -11,9 +11,16 @@ var reformatNumber = function(phoneNumString) {
     while (index < phoneNumString.length){
       
       let threeDigits = phoneNumString.slice(index, index + 3)
-      
+
       if (threeDigits.length == 1){
-        return returnString.slice(0, returnString.length - 2) + "-" + returnString.at(-2)+threeDigits
+          let secondToLastDigit =returnString.at(-2)
+          let lastDigit = threeDigits
+          
+          let firstBlock = returnString.slice(0, returnString.length - 2)
+          let lastBlock  = secondToLastDigit + lastDigit
+          
+          return `${firstBlock}-${lastBlock}`
+          return returnString.slice(0, returnString.length - 2) + "-" +secondToLastDigit + lastDigit 
       }
       returnString += (threeDigits+"-")
       index += 3
