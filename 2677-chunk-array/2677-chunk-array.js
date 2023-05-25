6ab83e2ccf885c8bc387f53fbef104eaf15abe5c
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-05-24 20:43:37 -0400

    Time: 72 ms (19.36%), Space: 44.3 MB (78.31%) - LeetHub

--- 2677-chunk-array/2677-chunk-array.js
+++ 2677-chunk-array/2677-chunk-array.js
@@ -5,10 +5,15 @@ var chunk = function(array, size) {
     array
     size
 
+    // array length cant get to -1 or below
+    // so array.length != 0 is a stronger condition than you think
+
     while (array.length != 0){        
         let chunk = array.splice(0, size)
         chunkedArray.push(chunk)
     }
-    
+    chunkedArray.slice(1)
     return chunkedArray
 }
+
+console.log(chunk([1,9,6,3,2], 3), [[1,9,6],[3,2]])
