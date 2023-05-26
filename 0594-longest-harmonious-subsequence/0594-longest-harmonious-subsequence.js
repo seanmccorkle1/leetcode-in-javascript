ec25cf07a6f27f541264958a59ff1684cde74594
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-05-25 23:10:16 -0400

    Time: 654 ms (14.89%), Space: 66.4 MB (5.67%) - LeetHub

--- 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.js
+++ 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.js
@@ -1,34 +1,39 @@
 var findLHS = function(array) {
     
     const obj = {}
-    let LHSLength = 0
+    let longestSubsequenceThatDiffersBy1 = 0
     
     for (let num of array){
       let frequency = ~~obj[num]
       obj[num] = frequency + 1
     }
-    
+  
   console.log(obj)
-    
-  for (let [stringnum, frequencyOfNum1] of Object.entries(obj)) {
+
+
+  
+  for (let num in obj){
       
-      let num = Number(stringnum)
+      num = Number(num)
       const nextElementIsHarmonious = obj[num + 1] ? true : false
       
       console.log(nextElementIsHarmonious)
       
+      
       if (nextElementIsHarmonious) {
-          
-          let frequencyOfNum2 = obj[num + 1]
-          let combinedFrequencies = frequencyOfNum1 +frequencyOfNum2
-          
-          LHSLength = Math.max(combinedFrequencies, LHSLength)
-          console.log(`longest: ${LHSLength}`)
+        let frequencyOfNum1 = obj[num]
+        let frequencyOfNum2 = obj[num + 1]
+        
+        let combinedFrequencies = frequencyOfNum1 +frequencyOfNum2
+        
+        
+        longestSubsequenceThatDiffersBy1 = Math.max(combinedFrequencies, longestSubsequenceThatDiffersBy1)
+        console.log(`longest: ${longestSubsequenceThatDiffersBy1}`)
       }
       
     }
   
-  return LHSLength
+  return longestSubsequenceThatDiffersBy1
 }
 
-console.log(findLHS([1,3,2,2,5,2,3,7]),  5)
+console.log(findLHS([1,3,2,2,5,2,3,7]),  5, "the longest harmonious subsequence is [3,2,2,2,3]")
