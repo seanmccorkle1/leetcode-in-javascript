3fc5176e4666ebe169df38531121f6f1f5517ca7
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-05-28 20:47:51 -0400

    Time: 59 ms (78.01%), Space: 44.8 MB (23.56%) - LeetHub

--- 2562-find-the-array-concatenation-value/2562-find-the-array-concatenation-value.js
+++ 2562-find-the-array-concatenation-value/2562-find-the-array-concatenation-value.js
@@ -8,19 +8,18 @@ var findTheArrayConcVal = function(nums) {
     const array=[]
     let realSum = 0
     
-    while (nums.length != 0){
+    while (nums.length != 0) {
         
         let firstNum=nums.length ? nums.shift() : ""
         let lastNum=nums.length ? nums.pop() : ""
         
         let concatString = String(firstNum) + String(lastNum)
         realSum += Number(concatString)
-        // array.push(Number(concatString))
     }
-
-    return realSum  
-    return array.reduce((a,b)=>a+b,0)
+    
+    return realSum
 }
+    
 
 // [7, 52, 2, 4] 
 // "74"
