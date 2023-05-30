d53d6a6478a4331d7f8396de36838c2ede2e3fce
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-05-30 10:46:33 -0400

    Time: 176 ms (33.24%), Space: 52.7 MB (53.35%) - LeetHub

--- 0705-design-hashset/0705-design-hashset.js
+++ 0705-design-hashset/0705-design-hashset.js
@@ -1,39 +1,20 @@
+// Use the inherent `this` object as the hash map
+
+// this.hash_map = {}
+
 var MyHashSet = function() {
-  
-    // Really you should just 
-    // Make your own object, but instead
-    // we have attached ourself to the 
-    // `this` object which then becomes our hashmap.
-    
-    // What you should instead do is this:
-    // this.hash_map = {}
-    // And then update our following functions
-};
-
-MyHashSet.prototype.add = function(key) {
-    
-    // Constant Time
-    // Linear Space | To the size of the input key
-    // You can access objects using array notation
-
-    this[key] = "asd"
+    this.hashSet= {}
 }
 
+MyHashSet.prototype.add = function(key) {
+    this.hashSet[key] = "asd"
+}
+
+// explicit reference for objects (including arrays)
 MyHashSet.prototype.remove = function(key) {
-    
-    // Constant Time
-    // Constant Space
-    // You can access objects using array notation
-    // Here we use the delete keyword.
+    delete this.hashSet[key] 
+}
 
-    delete this[key]
-};
-
-MyHashSet.prototype.contains = function(key) {
-    
-    // Constant Time
-    // Constant Space
-    // This just asks if the property exists
-
-    return this.hasOwnProperty(key)
-};
+MyHashSet.prototype.contains = function(key) {    
+    return this.hashSet.hasOwnProperty(key)
+}
