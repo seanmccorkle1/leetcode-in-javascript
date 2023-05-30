802dc75a2166eea6f175e94eba7414d2a8d93725
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-05-30 10:55:58 -0400

    Time: 73 ms (68.26%), Space: 44.3 MB (85.43%) - LeetHub

---
+++ 2553-separate-the-digits-in-an-array/2553-separate-the-digits-in-an-array.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var separateDigits = function(nums) {
+    
+    return nums.join("").split("").map(e => Number(e))
+    // nums = nums.map(num => String(num))
+    
+    
+    for (let stringnum of nums){
+        
+        if (stringnum.length >= 2){
+            
+        }
+        
+    }
+}
