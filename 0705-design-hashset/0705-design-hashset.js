c0fa636787f71a1458318010989a2403e2545475
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-05-30 18:22:47 -0400

    Time: 171 ms (36.75%), Space: 52.7 MB (52.71%) - LeetHub

--- 0705-design-hashset/0705-design-hashset.js
+++ 0705-design-hashset/0705-design-hashset.js
@@ -6,11 +6,13 @@ MyHashSet.prototype.add = function(key) {
     this.hashSet[key] = 0
 }
 
-// explicit reference for objects (including arrays)
+// explicit reference for updating and deleting objects 
+// (including arrays)
+
 MyHashSet.prototype.remove = function(key) {
     delete this.hashSet[key] 
 }
 
-MyHashSet.prototype.contains = function(key) {    
-    return this.hashSet.hasOwnProperty(key)
+MyHashSet.prototype.contains = function (key) {    
+    return this.hashSet[key] !== undefined
 }
