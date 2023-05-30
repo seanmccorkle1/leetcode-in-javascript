34a74849da4d1b26258582e40d9c092c276d3272
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-05-30 10:23:25 -0400

    Time: 67 ms (9.09%), Space: 41.5 MB (81.82%) - LeetHub

--- 2396-strictly-palindromic-number/2396-strictly-palindromic-number.js
+++ 2396-strictly-palindromic-number/2396-strictly-palindromic-number.js
@@ -1,11 +1,2 @@
-/**
- * @param {number} n
- * @return {boolean}
- */
-var isStrictlyPalindromic = function(n) {
-   
-    if (n <= 3){
-        return true
-    }
-    return false
-}
+ 
+const isStrictlyPalindromic =num=>{return false}
