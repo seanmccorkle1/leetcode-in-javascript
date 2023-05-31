2c5afac9ff04e55e53e51d59ab80eb8b8cda3ef6
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-05-30 21:19:21 -0400

    Time: 248 ms (31.25%), Space: 60.2 MB (32.81%) - LeetHub

--- 1396-design-underground-system/1396-design-underground-system.js
+++ 1396-design-underground-system/1396-design-underground-system.js
@@ -1,19 +1,34 @@
 class UndergroundSystem {
-  customer = new Map();
-  avg = new Map();
-  
+    
+  customer = new Map()
+  avg = new Map()
+
   checkIn(id, stationName, t) {
-    this.customer.set(id, { stationName, t });
+      this.customer.set(id, { stationName, t })
   }
-  checkOut(id, stationName, t) {
+checkOut(id, stationName, t) {
     const checkIn = this.customer.get(id);
-    if (!checkIn) throw new Error(`Customer ${id} didn't checked in`);
-    const key = `${checkIn.stationName}-${stationName}`;
-    const { sum, count } = this.avg.get(key) ?? { sum: 0, count: 0 };
+
+    if (!checkIn) {
+        throw new Error(`Customer ${id} didn't checked in`)
+    }
+
+    const key = `${checkIn.stationName}-${stationName}`
+    let { sum, count } = this.avg.get(key) ?? { sum: 0, count: 0 }
+    
     this.avg.set(key, { sum: sum + (t - checkIn.t), count: count + 1 });
   }
+  
   getAverageTime(startStation, endStation) {
     const { sum, count } = this.avg.get(`${startStation}-${endStation}`) ?? { sum: 0, count: 0 };
     return sum / count;
   }
 }
+
+/** 
+ * Your UndergroundSystem object will be instantiated and called as such:
+ * var obj = new UndergroundSystem()
+ * obj.checkIn(id,stationName,t)
+ * obj.checkOut(id,stationName,t)
+ * var param_3 = obj.getAverageTime(startStation,endStation)
+ */
