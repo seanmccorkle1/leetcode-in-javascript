8f3e227f209642288c0ca0955ceb7edbf7baa25c
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-06-01 19:23:47 -0400

    Time: 65 ms (26.82%), Space: 42.5 MB (21.09%) - LeetHub

--- 0059-spiral-matrix-ii/0059-spiral-matrix-ii.js
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.js
@@ -1,90 +1,73 @@
 var generateMatrix = function (num) {
 
-    const spiralMatrix = new Array(num).fill(0).map((zero) => new Array(num).fill(0))
+    // 3 new arrays
+    const spiralMatrix = new Array(num).fill(0).map((three) => new Array(num).fill(0))
 
     spiralMatrix
 
-    let increment = 0
     const numOfMatrixElements = num * num
-    
+
     // left:top
     // right:bottom
-    
+
     let leftIndex = 0
     let rightIndex = num - 1
-    
+
     let topIndex = 0
     let bottomIndex = num - 1
-    
+
     // use < with 0
     // use <= with 1
+    let increment = 1
 
-    while (increment < numOfMatrixElements) {
-        
-        // do many small increments of 1 
-        // than one  big increment of 4
+    while (increment <= numOfMatrixElements) {
 
         let refreshedLeftIndex = leftIndex
 
-        // left-to-right, row doesnt change
-        // chained to the top ROW
-
         while (refreshedLeftIndex <= rightIndex) {
-            increment += 1
             spiralMatrix[topIndex][refreshedLeftIndex] = increment
 
-            spiralMatrix
             refreshedLeftIndex++
+            increment+=1
         }
-
+        
         topIndex += 1
         let refreshedTopIndex = topIndex
 
-        // top-to-bottom, row changes
-        // chained to the right ELEMENT (2nd access specifier)
 
-        while (refreshedTopIndex <= bottomIndex) {
+        while (refreshedTopIndex  <= bottomIndex){
 
-            increment += 1
             spiralMatrix[refreshedTopIndex][rightIndex] = increment
-
-            spiralMatrix
             refreshedTopIndex++
+            increment +=1
         }
 
         rightIndex -= 1
         let refreshedRightIndex = rightIndex
 
-        // right-to-left, row doesnt change
-        // chained to the bottom ROW
+        while (refreshedRightIndex >= leftIndex){
 
-        while (refreshedRightIndex >= leftIndex) {
-            increment += 1
             spiralMatrix[bottomIndex][refreshedRightIndex] = increment
 
-            spiralMatrix
             refreshedRightIndex--
+            increment++
         }
 
-        bottomIndex -= 1
+        bottomIndex  -=  1
         let refreshedBottomIndex = bottomIndex
 
-        increment
-
-        // bottom-to-top, row changes
-        // chained to the left ELEMENT
-        
         while (refreshedBottomIndex >= topIndex) {
 
-            increment += 1
             spiralMatrix[refreshedBottomIndex][leftIndex] = increment
-
             refreshedBottomIndex--
-        }
+            increment++
+        } 
 
         leftIndex += 1
+
+        spiralMatrix
+
     }
-    
     return spiralMatrix
 }
 
