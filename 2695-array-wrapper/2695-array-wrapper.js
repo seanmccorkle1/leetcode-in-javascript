adfdd5777ff6537c39c54d6bf376939bcee9d202
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-05-31 20:12:18 -0400

    Time: 66 ms (24.04%), Space: 43.8 MB (61.85%) - LeetHub

--- 2695-array-wrapper/2695-array-wrapper.js
+++ 2695-array-wrapper/2695-array-wrapper.js
@@ -11,10 +11,8 @@ ArrayWrapper.prototype.toString = function() {
     return `[${stringWithCommas}]`
 }
 
-/**
- * const obj1 = new ArrayWrapper([1,2]);
- * const obj2 = new ArrayWrapper([3,4]);
- * obj1 + obj2; // 10
- * String(obj1); // "[1,2]"
- * String(obj2); // "[3,4]"
- */
+const obj1 = new ArrayWrapper([1,2]);
+const obj2 = new ArrayWrapper([3,4]);
+obj1 + obj2; // 10
+String(obj1); // "[1,2]"
+String(obj2); // "[3,4]"
