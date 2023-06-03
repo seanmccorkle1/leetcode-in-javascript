2a92b77bb47d8742aeedd1d25831c36ba3b5e3a9
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-06-02 20:09:09 -0400

    Time: 64 ms (44.15%), Space: 44 MB (48.64%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.js
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.js
@@ -1,34 +1,35 @@
-var diagonalSum = function(matrix) {
-    
+var diagonalSum = function (matrix) {
+
     let diagonalSum = 0
     let backIndex = matrix.length - 1
 
-    const matrixIsOdd = (matrix.length % 2) == 1 ? true : false
-    
-    let midIndex = Math.floor(matrix.length / 2)
-    let midCell = matrix[midIndex][midIndex]
-    
+    const matrixIsOdd = (matrix.length % 2) == 1
+
+    let midIndex = ~~(matrix.length / 2)
+    let midValue = matrix[midIndex][midIndex]
+
     if (matrixIsOdd){
-        diagonalSum -= midCell
+        diagonalSum -= midValue
     }
-    
-    for (let index=0; index<  matrix.length; index++){
         
-        let primaryDiagonalValue =  matrix[index][index]
-        let secondary = matrix[backIndex][index]
+    let index = 0
 
+    while (backIndex >= 0) {
+
+        diagonalSum += matrix[index][index]
+        diagonalSum += matrix[backIndex][index]
+
+        index++
         backIndex--
-
-        diagonalSum+=primaryDiagonalValue
-        diagonalSum+=secondary
-        
     }
+
+
     return diagonalSum
 }
 
 console.log(
     diagonalSum(
-        [[1,2,3],
-         [4,5,6],
-         [7,8,9]])
+        [[1, 2, 3],
+         [4, 5, 6],
+         [7, 8, 9]])
          , 25)
