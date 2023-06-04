4eadd6e73af7e6c54165a8dc65385c6babe57d8d
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-06-03 20:30:07 -0400

    Time: 70 ms (59.71%), Space: 44.5 MB (79.96%) - LeetHub

---
+++ 0547-number-of-provinces/0547-number-of-provinces.js
@@ -0,0 +1,33 @@
+function findCircleNum(M) {
+    // visited set
+    const visited = new Set();
+    // friend circles count
+    let circles = 0;
+	
+    // iterate thru matrix
+    for (let i = 0; i < M.length; i++) {
+        // check if this friend has been visited before
+        if (!visited.has(i)) {
+            // start dfs for this friend
+            dfs(i);
+            // this is another friend circle
+            circles++;
+        }
+    }
+	
+    return circles;
+	
+    // helper method to do dfs traversal thru M
+    function dfs(i) {
+        // go thru this friend's friends
+        for (let j = 0; j < M.length; j++) {
+            // check if this is a friend, and not visited before
+            if (M[i][j] === 1 && !visited.has(j)) {
+                // add as visited
+                visited.add(j);
+                // call dfs
+                dfs(j);
+            }
+        }
+    }
+}
