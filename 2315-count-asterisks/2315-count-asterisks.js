79d8827f760645615630e137ee0e3dd4c7a23282
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-06-05 09:24:32 -0400

    Time: 67 ms (23.97%), Space: 43.5 MB (19.83%) - LeetHub

---
+++ 2315-count-asterisks/2315-count-asterisks.js
@@ -0,0 +1,29 @@
+var countAsterisks = function(s) {
+    
+    s= s.split("|")
+    
+    let index = 2
+    let count = 0
+    
+    if (s[0].includes("*")){
+        count += s[0].match(/[\*]/g).length
+    }
+    
+    while (index < s.length){
+        
+        let string = s[index]
+        let array=string.match(/[\*]/g)
+        
+        console.log(array)
+        if (array !== null){
+            count += array.length
+        }
+        
+        index += 2
+    }
+    return count
+}
+
+// console.log(countAsterisks("l|*e*et|c**o|*de|"))
+
+console.log(countAsterisks("yo|uar|e**|b|e***au|tifu|l"))
