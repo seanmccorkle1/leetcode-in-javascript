9cc542e8e533b32040ba88bdec6d99f30e89450c
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-06-05 09:34:10 -0400

    Time: 66 ms (20.98%), Space: 41.8 MB (73.17%) - LeetHub

---
+++ 2103-rings-and-rods/2103-rings-and-rods.js
@@ -0,0 +1,19 @@
+var countPoints = function(rings) {
+
+    let rods = '0123456789'
+    let count = 0
+    
+    // looping over the variable, rods
+
+    for (let rod of rods) {     
+    
+        const ringWithAllColorsFound = rings.includes(`R${rod}`) && 
+          rings.includes(`G${rod}`) && rings.includes(`B${rod}`)
+        
+        if (ringWithAllColorsFound) {
+            count++
+        }         
+    } 
+    
+    return count
+}
