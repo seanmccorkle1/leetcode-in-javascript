07399ff91360837d2f5c44c8a8bc6334c8b3df3b
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-06-06 14:44:02 -0400

    Time: 67 ms (25.89%), Space: 42.6 MB (52.79%) - LeetHub

--- 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.js
+++ 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.js
@@ -1,26 +1,11 @@
 var findFinalValue = function(array, originalNum) {
-    array.sort((a,b)=>a-b)
-    
-    let backIndex = array.length - 1
-    
+    // array.sort((a,b)=>a-b)    
     let currNum = originalNum
     
     while (array.includes(currNum)){
         array.splice(array.indexOf(currNum), 1)
         currNum *= 2
     }
-        
-    return currNum
     
-    if (!array.includes(originalNum)){
-        return originalNum
-    }
-    while (backIndex >= 0){
-        const num = array[backIndex]
-        if (num % originalNum == 0){
-            return num * 2
-        }
-        backIndex--
-    }
-    return originalNum
+    return currNum
 }
