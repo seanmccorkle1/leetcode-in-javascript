ccbd845cbbb188ef891e6461a7f5fd7fa38b4f08
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-06-07 00:39:13 -0400

    Time: 66 ms (38.25%), Space: 43.8 MB (28.01%) - LeetHub

---
+++ 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.js
@@ -0,0 +1,15 @@
+var findGCD = function(array) {
+    array.sort((a,b)=>a-b)
+    
+    let divisor = array[0]
+    
+    while (divisor >= 0){
+
+        if (array[0] % divisor == 0 && array.at(-1) % divisor == 0){
+            return divisor
+        }
+        
+        divisor--
+    }
+    return divisor
+}
