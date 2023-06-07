3e339307c0a73b8c2f63193a7bec5be05b2a88db
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-06-07 10:02:54 -0400

    Time: 325 ms (87.50%), Space: 77.7 MB (81.62%) - LeetHub

---
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.js
@@ -0,0 +1,29 @@
+var pivotArray = function(array, pivot) {
+
+    let smallerArray = []
+    let greaterArray = []
+    let equalArray = []
+
+    for (let index = 0; index < array.length; index++){
+
+        const num = array[index]
+
+        if (num < pivot) {
+            smallerArray.push(num)
+        }
+
+        else if (num > pivot) {
+            greaterArray.push(num)
+        }
+        
+        else if (num == pivot)  {
+            equalArray.push(num)
+        }
+        
+    }
+    
+    return smallerArray.concat(equalArray).concat(greaterArray)
+    // return smallerArray.concat(equalArray, greaterArray)
+}
+
+console.log(pivotArray([9,12,5,10,14,3,10], 10), [9,5,3,10,10,12,14])
