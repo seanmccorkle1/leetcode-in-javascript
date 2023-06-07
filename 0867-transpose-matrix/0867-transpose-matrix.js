ae353ad4244aca51ff81540d6cb9dcc4f12a93a8
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-06-07 12:40:57 -0400

    Time: 67 ms (80.18%), Space: 44.6 MB (62.14%) - LeetHub

--- 0867-transpose-matrix/0867-transpose-matrix.js
+++ 0867-transpose-matrix/0867-transpose-matrix.js
@@ -1,18 +1,11 @@
-// origin_matrix  colIndex        origin_col_arr
-
 var transpose = function(matrix) {
-
-    let firstRow=matrix[0]
-
-    // `row` acts as the index for the row
-    // turn each number (n) into an array (return value of map())
-
-    // [2, 4, -1][0]
-    // [-10, 5, 11][1]
-    // [18, -7, 6][2]
-
     
-    return firstRow.map((n,  index) => matrix.map(row => row[index]))
+    // turn each number (n) into an array 
+    
+    const firstRow=matrix[0]
+
+    // nested map function
+    return firstRow.map((n, index) => matrix.map(row => row[index]))
 }
 
 console.log(
