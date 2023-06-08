6b7b1a6b63756885f7fe2260c90533e189f63c97
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-06-08 08:04:38 -0400

    Time: 64 ms (29.94%), Space: 42 MB (48.59%) - LeetHub

--- 0476-number-complement/0476-number-complement.js
+++ 0476-number-complement/0476-number-complement.js
@@ -2,12 +2,12 @@ var findComplement = function(num) {
 
     let numAsBitstring= num.toString(2)
 
-    let complementBitstring = numAsBitstring.replace(/[^]/g, digit=> digit == "1" ? "0" : "1")
+    let complementAsBitstring = numAsBitstring.replace(/[^]/g, digit=> digit == "1" ? "0" : "1")
     
     // the pattern is (binaryString, 2) not (binaryString, 10)
     // keep the arguments consistent
     
-    let complementAsDecimal=  parseInt(complementBitstring, 2)
+    let complementAsDecimal=  parseInt(complementAsBitstring, 2)
     return complementAsDecimal
 }
 
