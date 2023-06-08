21f190129a9e9832e8776ced102279e9cd3377c0
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-06-08 08:59:36 -0400

    Time: 53 ms (92.89%), Space: 44.4 MB (31.41%) - LeetHub

--- 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.js
+++ 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.js
@@ -1,26 +1,25 @@
 var checkStraightLine = function(coords) {
     
-    for (let index = 0; index < coords.length - 2; index++) {
-        
-        let num1 = coords[index][0] * coords[index + 1][1] + 
-            coords[index + 1][0] * coords[index + 2][1] +
-            coords[index + 2][0] * coords[index][1]
-        
-        let num2 = coords[index][1] * coords[index + 1][0] + 
-            coords[index + 1][1] * coords[index + 2][0] + 
-            coords[index + 2][1] * coords[index][0]
-        
-        const area = 0.5 * (num1 - num2)     
-        
-        const notAStraightLine =  (area != 0) ? true : false
-        
-        if (notAStraightLine) {
-            return false
-        }
-    }
-    
+   for (let i=0; i < coords.length - 2; i++){
+       
+       let [x1,y1]=coords[i]
+       let [x2,y2]=coords[i+1]
+       let [x3,y3]=coords[i+2]
+       
+       if (!x3){
+           break
+       }
+       
+       let area=Math.abs(0.5 * ((x1*y2 + x2*y3 + x3*y1)-(y1*x2 + y2*x3 + y3*x1)))
+       
+       const lineNotStraight = (area != 0) ? true : false
+
+       if (lineNotStraight) {
+           return false
+       }
+   }
     return true
 }
 
 console.log(checkStraightLine([[1,2],[2,3],[3,4],[4,5],[5,6],[6,7]]), true)
-// console.log(checkStraightLine( [[1,1],[2,2],[3,4],[4,5],[5,6],[7,7]]), false)
+// console.log(checkStraightLine([[1,1],[2,2],[3,4],[4,5],[5,6],[7,7]]), false)
