77a91b988d1a04307ebb842972ee05e6c7663c18
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-06-08 08:11:09 -0400

    Time: 70 ms (6.94%), Space: 41.8 MB (73.61%) - LeetHub

---
+++ 1037-valid-boomerang/1037-valid-boomerang.js
@@ -0,0 +1,16 @@
+var isBoomerang = function(points) {
+ 
+    let x1Difference = points[0][0] - points[1][0]
+    let x2Difference = points[0][0] - points[2][0]
+    
+    
+    let y1Difference = points[0][1] - points[1][1]
+    let y2Difference=  points[0][1] - points[2][1]
+    
+    let sum =( x1Difference * y2Difference) - (x2Difference *y1Difference)
+    
+    if (sum == 0){
+        return false
+    }
+    return true
+}
