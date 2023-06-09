358a92243669fa7fc7bb8327acf8f82cc7c6c917
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-06-09 17:26:56 -0400

    Time: 73 ms (6.43%), Space: 44.4 MB (5.26%) - LeetHub

---
+++ 2727-is-object-empty/2727-is-object-empty.js
@@ -0,0 +1,15 @@
+/**
+ * @param {Object | Array} obj
+ * @return {boolean}
+ */
+var isEmpty = function(obj) {
+ 
+    if (Array.isArray(obj)){
+        if (obj.length == 0){
+            return true
+        }
+        return false
+    }
+    
+    return Object.entries(obj).length == 0 ? true : false
+}
