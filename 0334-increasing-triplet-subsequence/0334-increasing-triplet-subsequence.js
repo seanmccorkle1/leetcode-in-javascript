6ec04ab27b01663a3184ff24d1afdb11f03dc7cd
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-06-09 20:11:01 -0400

    Time: 76 ms (93.27%), Space: 61.5 MB (26.27%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.js
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.js
@@ -4,16 +4,19 @@ var increasingTriplet = function(nums) {
     let secondNumber = Infinity
     
     for (let currentNumber of nums) {
-        if (currentNumber > secondNumber && currentNumber > firstNumber) {
-            return true;
-        }
-        if (currentNumber <= firstNumber) {
-            firstNumber = currentNumber;
+        
+        if (currentNumber > firstNumber && currentNumber > secondNumber) {
+            return true
         }
         
         else if (currentNumber > firstNumber) {
             secondNumber = currentNumber;
         }
+        
+        else if (currentNumber <= firstNumber) {
+            firstNumber = currentNumber;
+        }
+        
     }
     return false
 }
