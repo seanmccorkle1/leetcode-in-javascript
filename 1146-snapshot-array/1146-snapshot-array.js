cf287bdabb14735281ae3152c7201bd42a9bddbb
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-06-10 20:43:05 -0400

    Time: 1285 ms (14.00%), Space: 131.9 MB (6.00%) - LeetHub

---
+++ 1146-snapshot-array/1146-snapshot-array.js
@@ -0,0 +1,40 @@
+class SnapshotArray {
+
+    constructor(length) {
+        
+        this.elements = Array(length).fill(null).map(e => new Map());   // max O(n + s)
+        
+        this.snapId = -1
+    }
+    
+    set(index, val) {   // O(1)
+        this.elements[index].set(this.snapId +1, val)
+    }
+    
+    snap() {    // O(1)
+        ++this.snapId
+        return this.snapId;
+    }
+    
+    get(index, snap_id) {   // O(log(s) + s)
+        
+        // O(s)
+        const element = Array.from(this.elements[index].keys())
+        let id = -1
+        
+        let left = 0
+        let right = element.length - 1
+        let mid
+
+        while(left <= right) {
+            mid = left + Math.floor((right - left) / 2);
+            if(element[mid] <= snap_id) {
+                id = element[mid];
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return id === -1 ? 0 : this.elements[index].get(id);
+    }
+}
