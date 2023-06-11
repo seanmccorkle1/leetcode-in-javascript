4a9795590e457205e65ca1edc053c184eeefac36
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-06-11 08:57:38 -0400

    Time: 1338 ms (8.00%), Space: 132.2 MB (6.00%) - LeetHub

--- 1146-snapshot-array/1146-snapshot-array.js
+++ 1146-snapshot-array/1146-snapshot-array.js
@@ -1,38 +1,37 @@
 class SnapshotArray {
 
     constructor(length) {
-        this.array = new Array(length).fill(null).map(e => new Map()); // max O(n + s)
+        this.mapArray = new Array(length).fill(null).map(e => new Map()) // max O(n + s)
         this.snapId = -1
     }
 
     set(index, val) { // O(1)
-        this.array[index].set(this.snapId + 1, val)
+        this.mapArray[index].set(this.snapId + 1, val)
     }
 
     snap() { // O(1)
         ++this.snapId
-        return this.snapId;
+        return this.snapId
     }
 
     get(index, snap_id) { // O(log(s) + s)
 
         // O(s)
-        const element = Array.from(this.array[index].keys())
+        const keyArray = Array.from(this.mapArray[index].keys())
         let id = -1
 
         let left = 0
-        let right = element.length - 1
+        let right = keyArray.length - 1
         let mid
 
         while (left <= right) {
 
             // mid = left + Math.floor((right - left) / 2)
             mid = ~~((left+right)/2)
-                        
-            if (element[mid] <= snap_id) {
-                id = element[mid];
-                left = mid + 1;
-                // break
+
+            if (keyArray[mid] <= snap_id) {
+                id = keyArray[mid]
+                left = mid + 1
             } 
             
             else {
@@ -40,14 +39,23 @@ class SnapshotArray {
             }
         }
 
-        return id === -1 ? 0 : this.array[index].get(id);
+        // the exact ID wasn't found, 
+        // otherwise it would have hit the equals in the <= condition
+
+        if (id == -1) {
+            return 0
+        }
+
+        let map=this.mapArray[index]
+        return map.get(id)
+        // return this.mapArray[index].get(id)
     }
 }
 
-const snapshotArr = new SnapshotArray(3); // set the length to be 3
+const snapshotArr = new SnapshotArray(3) // set the length to be 3
 
-snapshotArr.set(0, 5); // Set array[0] = 5
-snapshotArr.snap(); // Take a snapshot, return snap_id = 0
+snapshotArr.set(0, 5) // Set array[0] = 5
+snapshotArr.snap() // Take a snapshot, return snap_id = 0
 
-snapshotArr.set(0, 6);
-snapshotArr.get(0, 0); // Get the value of array[0] with snap_id = 0, return 5
+snapshotArr.set(0, 6)
+snapshotArr.get(0, 0) // Get the value of array[0] with snap_id = 0, return 5
