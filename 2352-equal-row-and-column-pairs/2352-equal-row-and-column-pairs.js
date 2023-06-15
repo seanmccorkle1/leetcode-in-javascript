2038f4cd78f506166b028862a124baab8b246444
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-06-15 10:57:41 -0400

    Time: 250 ms (24.27%), Space: 51.9 MB (62.87%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.js
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.js
@@ -1,11 +1,48 @@
-var equalPairs = function(grid) {
-    const rows = grid.map(arr => arr.join())
-    const columns = grid[0].map((col, i) => grid.map(row => row[i]).join())
+var equalPairs = function (matrix) {
+
+    matrix
+
+    if (matrix.length == 1) {
+        return 1
+    }
+
+    const rowArray = matrix.map(row => row.join())
+    const columnArray = matrix[0].map((col, i) => matrix.map(row => row[i]).join())
+
+    rowArray
+    columnArray
+
+    // let array = rowArray.concat(columnArray)
+    // array = array.filter(string => array.indexOf(string) != array.lastIndexOf(string))
+
+
     let count = 0
-    for (let row of rows) {
-        for (let column of columns) {
-            if (row === column) count++
+
+    for (let row of rowArray) {
+
+        for (let column of columnArray) {
+
+            if (row == column) {
+                ++count
+            }
+
         }
     }
+
     return count
-};
+}
+
+// equalPairs([[3,2,1],[1,7,6],[2,7,7]])
+
+console.log(
+    equalPairs([
+        [13,13],
+        [13,13]]),  4)
+
+// console.log(
+//     equalPairs([
+//         [3, 2, 1],
+//         [1, 7, 6],
+//         [2, 7, 7]
+//     ]),
+//     1, "[2,7,7] is the pair")
