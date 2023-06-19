7d86c0a22bd3ee2d89f29ab7bd31737f6c41ef49
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-06-18 22:19:56 -0400

    Time: 66 ms (17.09%), Space: 42.5 MB (11.22%) - LeetHub

---
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.js
@@ -0,0 +1,11 @@
+const largestAltitude = gain => {    
+    let previous = 0;
+    let biggest = 0;
+    
+    for (let i = 0; i < gain.length; i += 1) {
+        previous += gain[i];
+        if (previous > biggest) biggest = previous;
+    }
+    
+    return biggest;
+}
