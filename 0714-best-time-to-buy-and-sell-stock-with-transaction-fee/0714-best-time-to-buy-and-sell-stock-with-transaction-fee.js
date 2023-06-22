0f24eddd2f2284bd835f0f5121f0231a0afb1d0a
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-06-21 22:25:30 -0400

    Time: 72 ms (95.15%), Space: 49.3 MB (67.96%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.js
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.js
@@ -3,9 +3,6 @@ var maxProfit = function(priceArray, fee) {
     let maxProfit = 0
     let selling = -priceArray[0]
 
-    if (selling >= 1){
-        return 
-    }
     for (let index = 1; index < priceArray.length; index++) {
 
         const currPrice = priceArray[index]
