af12962876fd98f507ba5f8f6ef7cdcfaac28f23
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-06-23 10:43:41 -0400

    Time: 217 ms (51.85%), Space: 72.5 MB (44.44%) - LeetHub

---
+++ 2433-find-the-original-array-of-prefix-xor/2433-find-the-original-array-of-prefix-xor.js
@@ -0,0 +1,32 @@
+
+
+var findArray = function(array) {
+ 
+    5^2
+    3^1
+
+    const returnArray = new Array(array.length)
+    returnArray[0] = array[0]
+
+    returnArray
+
+    for (let index=1; index < array.length; index++) {
+
+        let rightNum= array[index]
+        let leftNum= array[index - 1]
+
+        returnArray[index] = (leftNum ^ rightNum) 
+    }
+    
+    return returnArray
+}
+
+console.log(findArray([5,2,0,3,1]), [5,7,2,3,2])
+
+
+// From the array [5,7,2,3,2]:
+// - array[0] = 5.
+// - array[1] = 5 ^ 7 = 2.
+// - array[2] = 5 ^ 7 ^ 2 = 0.
+// - array[3] = 5 ^ 7 ^ 2 ^ 3 = 3.
+// - array[4] = 5 ^ 7 ^ 2 ^ 3 ^ 2 = 1.
