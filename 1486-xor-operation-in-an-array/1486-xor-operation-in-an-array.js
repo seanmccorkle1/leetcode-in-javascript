147229d5262320a178e0f55ece08bbeda9452e57
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-06-22 23:17:15 -0400

    Time: 46 ms (95.98%), Space: 42.5 MB (6.32%) - LeetHub

--- 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.js
+++ 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.js
@@ -3,20 +3,10 @@ var xorOperation = function(num, startNum) {
     const array = new Array(num)
     array[0]=startNum
     
-    let sum =  startNum
-    let index = 1
-    
-    while (index < num){
-        let currNum = startNum + (index * 2)
-        sum ^= currNum
-        ++index
-    }
-    return sum
-    
     for (let index = 1; index < array.length; index++) {
         array[index]  = startNum + (index * 2)
     }
-
+    
     array
     return array.reduce((sum, curr) => sum ^ curr)
 }
