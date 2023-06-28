43b5336ed2e4273534ec14314a4b99c54460e564
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-06-27 20:12:04 -0400

    Time: 111 ms (53.28%), Space: 57.2 MB (71.53%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.js
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.js
@@ -3,10 +3,9 @@ var findCenter = function(edges) {
     const [p1, p2] = edges[0]
     const [p3, p4] = edges[1]
     
+    
     if (p1 == p3 || p1 == p4){
         return p1
-    } else if (p2 == p3 || p2 == p4){
-        return p2
-    }
-    // ? p1 : p2
+    } 
+    return p2
 }
