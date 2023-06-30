dab93141eda058e99e334f9bfe7945c27013d63c
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-06-30 18:53:52 -0400

    Time: 48 ms (97.22%), Space: 42.4 MB (18.59%) - LeetHub

--- 0050-powx-n/0050-powx-n.js
+++ 0050-powx-n/0050-powx-n.js
@@ -1,37 +1,36 @@
 var myPow = function (num, exp) {
-    
+
     num
     exp
-    
 
     const recursivePower = (x, exponent) => {
-        
+
         if (x == 0) {
             return 0
         }
-        
+
         if (exponent == 0) {
             return 1
         }
-        
+
         const exponentIsEven = (exponent % 2) == 0
-        
+
         if (exponentIsEven) {
-        
-            const returnValue = recursivePower(x, exponent / 2) 
+
+            const returnValue = recursivePower(x, exponent / 2)
             return Math.pow(returnValue, 2)
         }
-        
+
         else if (!exponentIsEven) {
-            
+
             exponent = (exponent - 1) / 2
-            
+
             const pow = Math.pow(recursivePower(x, exponent), 2)
 
             return x * pow
         }
     }
-    
+
     const absExp = Math.abs(exp)
     const poweredNum = recursivePower(num, absExp)
     
@@ -39,7 +38,9 @@ var myPow = function (num, exp) {
         return 1/poweredNum
     }
     
+    return Number(poweredNum.toFixed(5))
     return poweredNum
 }
 
-myPow(2.00000, 10) // 1024
+console.log(myPow(2.00000, 10), 1024)
+console.log(myPow(2.10000, 3),  9.261) 
