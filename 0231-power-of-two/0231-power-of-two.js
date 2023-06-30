5d5a3d6b5d3bb9e038e42b667a7ba19eb2e40e3c
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-06-29 23:39:43 -0400

    Time: 75 ms (36.19%), Space: 43.5 MB (67.34%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.js
@@ -0,0 +1,19 @@
+var isPowerOfTwo = function(num) {
+    
+    if (num <=0)return false
+    
+    let bitwise = (num) & (num - 1) 
+        
+        if (bitwise === 0) {
+            return true
+        }
+        else if (bitwise !== 0) {
+            return false
+        }
+    }
+    
+
+    // else if ( num <= 0) return false
+
+console.log(isPowerOfTwo(32), true, "2 ^ 5 = 32")
+// console.log(isPowerOfTwo(20), false, “[1, 2, 4, 8, 16, 32], 20 is not a power”)
