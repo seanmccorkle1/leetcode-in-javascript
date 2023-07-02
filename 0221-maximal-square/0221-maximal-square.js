f78ad9b6ceb8fbe497318f4367bc67c256bb8248
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-07-02 13:17:05 -0400

    Time: 102 ms (58.46%), Space: 50.5 MB (29.85%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.js
+++ 0221-maximal-square/0221-maximal-square.js
@@ -1,26 +1,53 @@
-var maximalSquare = function(matrix) {
-    
+var maximalSquare = function (matrix) {
+
     if (!matrix.length) {
         return 0
     }
-    
-    let dp = new Array(matrix.length + 1).fill(0)
-    
-    dp = dp.map((n)=>new Array(matrix[0].length+1).fill(0))
-    
-    let max = 0
-    
-    for (let r=1;r<dp.length;r++) { 
 
-        for (let c=1;c<dp[0].length;c++) {
-        
-            if (matrix[r-1][c-1]!=0) {
-                dp[r][c] = Math.min(dp[r][c-1], dp[r-1][c], dp[r-1][c-1])+1;
-                max = Math.max(dp[r][c], max);
+    matrix
+
+    let dp = new Array(matrix.length + 1).fill(0)
+    // let dp = new Array(matrix[0].length).fill(0)
+
+    dp = dp.map((n) => new Array(matrix[0].length + 1).fill(0))
+    dp
+
+    let max = 0
+
+    let numOfRows = dp.length
+    let numOfColumns = dp[0].length
+
+    for (let rowIndex = 1; rowIndex < numOfRows; rowIndex++) {
+
+        for (let columnIndex = 1; columnIndex < numOfColumns; columnIndex++) {
+
+            const matrixNum = Number(matrix[rowIndex - 1][columnIndex - 1])
+
+            let leftNum = dp[rowIndex][columnIndex - 1]
+            let topNum = dp[rowIndex - 1][columnIndex]
+            let currNum = dp[rowIndex - 1][columnIndex - 1]
+
+            if (matrixNum != 0) {
+
+                dp[rowIndex][columnIndex] = Math.min(currNum, leftNum, topNum)
+                dp[rowIndex][columnIndex] += 1
+
+                dp
+                max = Math.max(dp[rowIndex][columnIndex], max)
             }
         }
     }
     
+    dp
     max
+
+    // "square"
     return Math.pow(max, 2)
 }
+
+maximalSquare([
+    ["1", "0", "1", "0", "0"],
+    ["1", "0", "1", "1", "1"],
+    ["1", "1", "1", "1", "1"],
+    ["1", "0", "0", "1", "0"]
+]) // 4
