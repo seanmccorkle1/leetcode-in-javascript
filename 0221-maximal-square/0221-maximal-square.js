ca0cd0c170bb0889632e1f4db357dee0d39c5962
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-07-02 13:16:50 -0400

    Time: 104 ms (54.48%), Space: 50.6 MB (29.85%) - LeetHub

---
+++ 0221-maximal-square/0221-maximal-square.js
@@ -0,0 +1,26 @@
+var maximalSquare = function(matrix) {
+    
+    if (!matrix.length) {
+        return 0
+    }
+    
+    let dp = new Array(matrix.length + 1).fill(0)
+    
+    dp = dp.map((n)=>new Array(matrix[0].length+1).fill(0))
+    
+    let max = 0
+    
+    for (let r=1;r<dp.length;r++) { 
+
+        for (let c=1;c<dp[0].length;c++) {
+        
+            if (matrix[r-1][c-1]!=0) {
+                dp[r][c] = Math.min(dp[r][c-1], dp[r-1][c], dp[r-1][c-1])+1;
+                max = Math.max(dp[r][c], max);
+            }
+        }
+    }
+    
+    max
+    return Math.pow(max, 2)
+}
