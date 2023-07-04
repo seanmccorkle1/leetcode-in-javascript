b1ba964b5709816a9ee0adf10c23ecdf47a05184
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-07-04 11:26:21 -0400

    Time: 72 ms (37.45%), Space: 44.1 MB (37.07%) - LeetHub

--- 0859-buddy-strings/0859-buddy-strings.js
+++ 0859-buddy-strings/0859-buddy-strings.js
@@ -1,28 +1,66 @@
-var buddyStrings = function(A, B) {
-    if (A.length != B.length) {
-        return false;
-    }
-    
-    if (A == B) {
-        let s = new Set();
-        
-        for (let i = 0; i < A.length; i++) {
-            s.add(A.charAt(i));
-        }
-        
-        if (s.size < A.length){
-            return true
-        }
+var buddyStrings = function (string, goalString) {
+
+    if (string.length != goalString.length) {
         return false
     }
-    
-    let diff = []
-    
-    for (let i = 0; i < A.length; i++) {
-        if (A.charAt(i) != B.charAt(i)) {
-            diff.push(i);
+
+    const twoIndexArray = []
+
+    for (let index = 0; index < string.length; index++) {
+
+        let letter = string[index]
+        let goalStringLetter = goalString[index]
+
+        if (letter != goalStringLetter) {
+            twoIndexArray.push(index)
+            twoIndexArray
+        }
+
+        // return check at the end of the loop
+        if (twoIndexArray.length >= 3) {
+            return false
         }
     }
-    
-    return diff.length == 2 && A.charAt(diff[0]) == B.charAt(diff[1]) && A.charAt(diff[1]) == B.charAt(diff[0]);
-};
+
+    const stringsAreTheSame = twoIndexArray.length == 0 ? true : false
+
+    if (stringsAreTheSame) {
+
+        let set = [...new Set(string)]
+
+        string
+        set
+
+        const duplicateLettersExist = string.length != [...new Set(string)].length ? true : false
+
+        if (duplicateLettersExist) {
+            return true
+        }
+
+        return false
+
+        // if (string.length != new Set(string).size) {
+        //     return true
+        // }
+
+    }
+
+    let [index, index2] = twoIndexArray
+    twoIndexArray
+
+    string
+    goalString
+
+    // check only two letters
+    if (string[index] == goalString[index2] && string[index2] == goalString[index]) {
+        return true
+    }
+
+    return false
+}
+
+// console.log(buddyStrings("ab", "ba"), true)
+// console.log(buddyStrings("ab", "ab"), false)
+
+console.log(buddyStrings("aa", "aa"), true)
+// console.log(buddyStrings("ab", "ab"), false)
