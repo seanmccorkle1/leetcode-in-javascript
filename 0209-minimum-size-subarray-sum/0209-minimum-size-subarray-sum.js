64f527677365043eab503551074aac25f5734fb2
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-07-06 13:19:25 -0400

    Time: 68 ms (55.45%), Space: 46.2 MB (54.51%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.js
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.js
@@ -1,15 +1,12 @@
-/**
- * @param {number} target
- * @param {number[]} nums
- * @return {number}
- */
-
 var minSubArrayLen = function (targetNum, array) {
+
     let left = 0
     let right = 0
 
     let sum = 0
     let minArrayLength = Infinity
+
+    // contiguous means you CANT sort it
     array
 
     while (right < array.length) {
@@ -23,26 +20,34 @@ var minSubArrayLen = function (targetNum, array) {
         sum += array[right]
 
         while (sum >= targetNum) {
-            let length = (right - left) + 1
-        
-            minArrayLength
 
-            // if (length < minArrayLength) {
-                minArrayLength = Math.min(length, minArrayLength)
-            // }
-            
-            array[right]
-            array[left]
-            sum
+            let length = (right - left) + 1
+
+            minArrayLength = Math.min(length, minArrayLength)
+            minArrayLength
 
             sum -= array[left]
             left++
-        } // end of nested while
+        }
 
         right++
     }
+
     left
     right
+
+    // if the sum was never greater
+
+    if (minArrayLength == Infinity) {
+        
+    // if (sum < targetNum){
+        return 0
+    }
     
-    return minArrayLength === Infinity ? 0 : minArrayLength
+    return minArrayLength
 }
+
+console.log(minSubArrayLen(213, [12, 28, 83, 4, 25, 26, 25, 2, 25, 25, 25, 12]), 8)
+
+// console.log(minSubArrayLen(8, [1, 1, 1, 5, 1, 2]), 3, "[5, 1, 2] is contiguous and is >= 8")
+// console.log(minSubArrayLen(15,[1,1,1,1]), 0, "the array cant sum to 15, return 0.")
