2fa6537c2df08b1cd14a87bde4340472641b5be8
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-07-09 20:28:15 -0400

    Time: 242 ms (14.94%), Space: 98.8 MB (65.48%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.js
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.js
@@ -7,25 +7,38 @@
  * }
  */
 
-/**
- * @param {TreeNode} root
- * @return {number}
- */
-
 var minDepth = function(root) {
-    
-    if (root === null) {
-        return 0
-    }
-    
-    if (root.left === null) {
-        return minDepth(root.right) + 1
-    }
 
-    if (root.right === null) {
-        return minDepth(root.left) + 1
+    // Base case...
+    // If the subtree is empty i.e. root is NULL, return depth as 0...
+    
+    if(root == null)  return 0;
+    
+    // If the both subtrees are empty...
+    if(root.left == null && root.right == null){    
+        return 1
     }
     
-    let n =  Math.min( minDepth(root.left), minDepth(root.right) )
-    return n +1
+    // If the left subtree is empty,
+    // return the depth of the right subtree after adding 1 to it...
+
+    if (root.left == null){
+        return 1 + minDepth(root.right)
+    }
+    
+    // If the right subtree is empty, 
+    // return the depth of left subtree after adding 1 to it...
+    
+    if (root.right == null) {
+        return 1 + minDepth(root.left)
+    }
+    
+    // When the two child function return its depth...
+    // Pick the minimum out of these two subtrees and return this value after adding 1 to it...
+
+    // Adding 1 is the current node which is the parent of the two subtrees...
+    
+    let minNumOfEdges = Math.min(minDepth(root.left), minDepth(root.right)) + 1;
+    return minNumOfEdges
 }
+
