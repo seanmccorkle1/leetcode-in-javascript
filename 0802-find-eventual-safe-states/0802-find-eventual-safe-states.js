77ef4be6b51672b592f5fa4096c59687c16bda10
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-07-11 20:38:01 -0400

    Time: 127 ms (71.92%), Space: 51.3 MB (75.34%) - LeetHub

---
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.js
@@ -0,0 +1,35 @@
+var eventualSafeNodes = function(graph) {
+
+    let results = [];
+    let visited = {};
+    
+    for(let i = 0; i < graph.length; i++) {        
+        if (isNodeSafe(graph, i, visited)) {
+            results.push(i);
+        }
+    }
+    return results.sort((a, b) => a - b);
+}
+
+
+function isNodeSafe(graph, node, visited) {
+    if (visited[node] === true) {
+        return false;
+    }    
+
+    if (visited[node] === "safe") {
+        return true;
+    }
+    
+    visited[node] = true;
+
+    for(let i = 0; i < graph[node].length; i++) {        
+        if(!isNodeSafe(graph, graph[node][i], visited)) {
+            return false;
+        }        
+    }
+    visited[node] = "safe";   
+    return true;
+}
+
+
