303b4d3b5a2d2ff5e4faac1f7b342e16753a6479
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-07-19 18:31:58 -0400

    Time: 265 ms (38.52%), Space: 75.1 MB (54.26%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.js
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.js
@@ -1,24 +1,25 @@
-var eraseOverlapIntervals = function(array) {
+var eraseOverlapIntervals = function (array) {
     
     // sort by earliest finish time
     array.sort((a, b) => a[1] - b[1])
+
+    let prevInterval = array[0]
+    let removeCount = 0
     
-    let prev = array[0]
-    let remove = 0
-    
-    for(let index = 1; index < array.length; index++) {
+    for (let index = 1; index < array.length; index++) {
         
         const currInterval = array[index]
         
-        if (currInterval[0] < prev[1]) {
-            remove++
+        if (currInterval[0] < prevInterval[1]) {
+            removeCount++
         }
-        
+
         else {
-            prev = array[index]
+            prevInterval = array[index]
         }
     }
     
-    return remove
+    return removeCount
 }
 
+console.log(eraseOverlapIntervals([[1,2],[2,3],[3,4],[1,3]]), 1)
