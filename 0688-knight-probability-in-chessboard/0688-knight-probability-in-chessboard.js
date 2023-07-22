ed4f30d64edfba4d8e3cacb5f9ff59c63895551b
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-07-21 20:46:35 -0400

    Time: 186 ms (10.94%), Space: 63.5 MB (6.25%) - LeetHub

---
+++ 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.js
@@ -0,0 +1,31 @@
+var knightProbability = function(N, K, r, c) {
+    
+    const  findProbs = function(k, r, c) {
+        const key = `${k}#${r}#${c}`;
+        
+        // base case
+        if (isOutOfBound(r, c)) return 0.0; // .0 is for completeness sake. Don't really need it in JavaScript
+        if (k === 0) return 1.0;
+        if (memo.has(key)) return memo.get(key);
+        
+        let prob = 0;
+        
+        for (const [dirX, dirY] of dirs) {
+            prob += 0.125 * findProbs(k - 1, r + dirX, c + dirY);
+        }
+        
+        memo.set(key, prob);
+        
+        return prob;
+        
+    }
+    const dirs = [[2, 1], [2, -1], [-2, 1], [-2, -1], [1, 2], [1, -2], [-1, 2], [-1, -2]];
+    const memo = new Map();
+    
+    return findProbs(K, r, c);
+    
+    
+    function isOutOfBound(row, col) {
+        return row < 0 || col < 0 || row >= N || col >= N;
+    }
+};
