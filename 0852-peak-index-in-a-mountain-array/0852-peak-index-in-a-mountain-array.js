afd58fd45d494de84b54b2f1faede8aa52bde1d1
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-07-24 20:14:15 -0400

    Time: 69 ms (83.13%), Space: 51.4 MB (32.10%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.js
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.js
@@ -1,7 +1,10 @@
 const peakIndexInMountainArray = (A, low = 0, high = A.length - 1) => {
-    if (!A.length) 
-        return -1
     
+    if (!A.length){
+        return -1
+    }
+    
+    // bitwise or
     const mid = ((low + high) / 2) | 0
 	
     if (A[mid] > A[mid - 1] && A[mid] > A[mid + 1])
