485c9fcc728aa3eb05103fe3b7bff23dec90ca20
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-07-28 07:22:10 -0400

    Time: 3010 ms (5.08%), Space: 49.1 MB (7.04%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.js
+++ 0010-regular-expression-matching/0010-regular-expression-matching.js
@@ -5,24 +5,29 @@ var isMatch = function (string, patternString) {
     // returns true when string and patternString are empty
     // returns false when string contains chars with empty patternString
 
-    if (!patternString) {
-        return !string
+    if (patternString == "" && string == "") {
+        return true
+    }
+
+    if (patternString == "" && string.length != "") {
+        return false
     }
     
-    // if (patternString.includes(".*") && !patternString.includes("\.*")){
-    //     return true
-    // }
-    
+    if (patternString.includes(".*") && !patternString.includes("\.*")){
+        return true
+    }
+
     patternString
     string
 
     const firstLetterOfSliceMatches  = (patternString[0] == string[0]) || (patternString[0] == ".")
     const firstLetterMatches = firstLetterOfSliceMatches && Boolean(string)
-        
+
     // Track when the next character * is next in line in the patternString
 
     // if next patternString match (after *) is fine with current string, then proceed with it (s, p+2).  That's because the current patternString may be skipped.
     // Otherwise check firstLetterMatches. That's because if we want to proceed with the current patternString, we must be sure that the current patternString char matches the char
+
 
     // If firstLetterMatches is true, then do the recursion with next char and current patternString (s+1, p).
     // That's because current char matches the patternString char.
