382621f2c17c33e89b7c3a005079d13c35688db3
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-07-28 08:07:53 -0400

    Time: 2668 ms (5.08%), Space: 49.7 MB (5.09%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.js
+++ 0010-regular-expression-matching/0010-regular-expression-matching.js
@@ -16,6 +16,14 @@ var isMatch = function (string, patternString) {
         return false
     }
     
+    if (string == "" && patternString.replace(/\*/g, "") == "" &&string.length >= 3){
+        return true
+    }
+    if (string == "" && patternString.replace(/\*/g, "") != "" && string.length>=3){
+        return false
+    }
+    
+    
     patternString
     string
     
@@ -23,7 +31,7 @@ var isMatch = function (string, patternString) {
     const catchall = patternString[0] == "."
     const stringExists = Boolean(string)
     
-    const firstLetterMatches = lettersMatch || catchall && stringExists
+    const firstLetterMatches = (lettersMatch || catchall) && stringExists
     
     // Track when the next character * is next in line in the patternString
 
