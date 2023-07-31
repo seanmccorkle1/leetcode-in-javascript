cdf5d18c9657ee460cf6d1f131074bc89ec229ec
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-07-31 12:11:25 -0400

    Time: 64 ms (82.20%), Space: 46.1 MB (23.31%) - LeetHub

---
+++ 2500-delete-greatest-value-in-each-row/2500-delete-greatest-value-in-each-row.js
@@ -0,0 +1,34 @@
+var deleteGreatestValue = function (grid) {
+
+    grid
+
+    // push highest values to the back for pop()
+    grid.forEach(row => row.sort((a, b) => a - b))
+
+    let sum = 0
+    grid[0]
+
+    let firstRow = grid[0]
+
+    while (firstRow.length != 0) {
+
+        const currColumn = []
+
+        for (const row of grid) {
+            let greatestNum = row.pop()
+            currColumn.push(greatestNum)
+        }
+
+        currColumn
+
+        let greatestOfTwo = Math.max(...currColumn)
+        sum += greatestOfTwo
+    }
+    
+    return sum
+}
+
+console.log(deleteGreatestValue([
+    [1, 2, 4],
+    [3, 3, 1]
+]), 8, "4 + 3 + 1 = 8")
