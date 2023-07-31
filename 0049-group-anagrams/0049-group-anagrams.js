674f117f2f8877d76cd144fa925c8d2b7960a1c8
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-07-31 12:03:01 -0400

    Time: 116 ms (89.05%), Space: 53.5 MB (60.46%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.js
+++ 0049-group-anagrams/0049-group-anagrams.js
@@ -1,27 +1,29 @@
-var groupAnagrams = function(stringArray){
-    
+var groupAnagrams = function(array){
+
     // take a normal array and return a double array = use an object
     const object = {}
-    
-    for (let originalString of stringArray){
-        
-        let anagramString = originalString.split("").sort().join("")
-        const anagramArray = object[anagramString]
-        
+
+    array.length
+
+    for (let string of array){
+
+        let anagram = string.split("").sort().join("")
+        const anagramArray = object[anagram]
+
         // group it ON the anagram
         // means make the anagram the key
-        
-        anagramArray ? anagramArray.push(originalString) : object[anagramString]   = [originalString]
-        
-//         if (!anagramArray) { 
-//             object[anagramString] = [originalString] // so you can push into the array
-//         }
-        
-//         else {
-//             anagramArray.push(originalString)
-//         }
-        
+
+        anagramArray
+            ? anagramArray.push(string)
+            : object[anagram]   = [string];
+
         object
     }
-    return Object.values(object)
+
+    object
+    
+    return Object.values(object).sort((a,b) => a.length - b.length)
 }
+
+groupAnagrams(["eat","tea","tan","ate","nat","bat"]) // [["bat"],["nat","tan"],["ate","eat","tea"]]
+// groupAnagrams([""])
