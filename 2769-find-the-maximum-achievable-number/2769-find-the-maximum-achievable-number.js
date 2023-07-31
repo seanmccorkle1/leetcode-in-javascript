17d2d0b03d3f408a28902a890a8173296bef8a7d
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-07-31 12:26:22 -0400

    Time: 87 ms (92.46%), Space: 46.1 MB (18.92%) - LeetHub

---
+++ 2769-find-the-maximum-achievable-number/2769-find-the-maximum-achievable-number.js
@@ -0,0 +1,20 @@
+
+var theMaximumAchievableX = function(num, t) {
+    if (t ==1){
+        return num + 2
+    }
+    if (num == 1){
+        return (t*2) + 1
+    }
+
+    // if (num == t){
+    //     return Math.pow(num, t)
+    // }
+    
+    return num +( t * 2 )
+}
+
+theMaximumAchievableX(2, 2) // 6
+theMaximumAchievableX(3, 2) // 7
+
+// theMaximumAchievableX(3, 3) // 9
