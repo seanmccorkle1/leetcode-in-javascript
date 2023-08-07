aa9137b87fbd6c814b53eca71ffa813eaf522985
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-08-06 20:19:00 -0400

    Time: 49 ms (90.59%), Space: 42.4 MB (28.19%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.js
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.js
@@ -1,18 +1,31 @@
 var searchMatrix = function(matrix, target) {
     
-    return matrix.flat(10).includes(target)
     if (!matrix.length || !matrix[0].length) {
         return false
     }
     
-  let row = 0;
-  let col = matrix[0].length - 1
+  let row = 0
+  
+  // index
+  let col = matrix[0].length - 1 
   
   while (col >= 0 && row <= matrix.length - 1) {
-      if (matrix[row][col] === target) return true;
-      else if (matrix[row][col] > target) col--;
-      else if (matrix[row][col] < target) row++;
+      
+      let currNum = matrix[row][col]
+      
+      if (currNum == target) {
+          return true
+      }
+      
+      else if (currNum < target) {
+          row+= 1
+      }
+      
+      else if (currNum > target){
+          col -= 1
+      }      
   }
-
-  return false;
+    // opposite of "true" in the loop
+    return false
 }
+
