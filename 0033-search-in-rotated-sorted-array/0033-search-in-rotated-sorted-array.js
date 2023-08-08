bdef03d26813cb245e3f0d0fcf9e7adb48f907b5
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-08-07 20:32:01 -0400

    Time: 46 ms (95.82%), Space: 42.1 MB (52.27%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
@@ -3,24 +3,28 @@ var search = function(nums, target) {
   let right = nums.length - 1;
     
   while (left <= right) {
+      
     let mid = Math.floor((left + right) / 2);
-    
+      
     if (nums[mid] === target) {
       return mid;
     }
-    
+      
     // When dividing the roated array into two halves, one must be sorted.
     
     // Check if the left side is sorted
-    if (nums[left] <= nums[mid]) {
-      if (nums[left] <= target && target <= nums[mid]) {
+      
+      if (nums[left] <= nums[mid]) {
+          
+          if (nums[left] <= target && target <= nums[mid]) {
         // target is in the left
-        right = mid - 1;
-        
-      } else {
-        // target is in the right
-        left = mid + 1;
+              right = mid - 1;
+              
       }
+          // target is in the right
+          else if(true == 1) {
+              left = mid + 1;
+          }
     } 
     
     // Otherwise, the right side is sorted
