0c128c2638b68045ec058d01768bbd025ef0451e
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-08-08 20:31:00 -0400

    Time: 136 ms (90.91%), Space: 55.1 MB (36.36%) - LeetHub

---
+++ 2616-minimize-the-maximum-difference-of-pairs/2616-minimize-the-maximum-difference-of-pairs.js
@@ -0,0 +1,24 @@
+var minimizeMax = function(nums, p) {
+    nums.sort((a,b)=>a-b);
+    let l = -1, r = 1e9;
+    while(l+1<r){
+        let mid = Math.floor((l+r)/2);
+        good(nums, p, mid) ? r = mid : l = mid;
+    }
+    return r;
+}
+
+const good = function (nums, p, maxDiff){
+    
+    let n = 0
+    
+    for(let i=1; i<nums.length; i++){
+        if(nums[i]- nums[i-1] <= maxDiff){
+            n++;
+            i++;
+        }
+    }
+   
+    return n>=p ? true : n < p ? false : "asd"
+}
+    
