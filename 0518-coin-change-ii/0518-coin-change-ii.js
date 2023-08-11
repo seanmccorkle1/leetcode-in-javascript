e886d1a51beb14effe16e1284dcb8b38c0103c2e
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-08-10 21:21:30 -0400

    Time: 57 ms (92.86%), Space: 42.2 MB (77.78%) - LeetHub

---
+++ 0518-coin-change-ii/0518-coin-change-ii.js
@@ -0,0 +1,24 @@
+var change = function(amount, coins) {
+    
+    let dp = new Array(amount + 1).fill(0)
+    dp[0] = 1
+    
+    for (let i=0; i<coins.length;i++) { // iterate the types of coin
+        
+        for (let j=1; j<=amount; j++) {
+            
+            i
+            j
+            
+            // dp[j] is the combinations for amount j for not using the current coin
+            // dp[j - coins[i]] is is the combinations for using the current coin
+            
+            if (j >= coins[i]) {
+                // dp[j] = dp[j] + dp[j - coins[i]];
+                dp[j] += dp[j - coins[i]]
+            }
+        }
+    }
+    
+    return dp[amount]
+}
