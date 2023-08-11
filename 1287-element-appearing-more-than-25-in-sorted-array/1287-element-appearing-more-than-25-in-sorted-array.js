1809f363b3684a53ff5cc22faceb94075d9836a0
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-08-11 15:56:25 -0400

    Time: 58 ms (73.79%), Space: 42.8 MB (67.96%) - LeetHub

--- 1287-element-appearing-more-than-25-in-sorted-array/1287-element-appearing-more-than-25-in-sorted-array.js
+++ 1287-element-appearing-more-than-25-in-sorted-array/1287-element-appearing-more-than-25-in-sorted-array.js
@@ -1,13 +1,15 @@
 var findSpecialInteger = function (array) {
 
-    const quarter = ~~(array.length / 4)
     if (array.length == 1){
         return array[0]
     }
+    
     if (array.length <= 3){
         return array[1]
     }
-    
+
+    const quarter = ~~(array.length / 4)
+
     array.length
     array
 
@@ -15,6 +17,7 @@ var findSpecialInteger = function (array) {
 
         let currNum = array[index]
         let firstNumOfNextQuartile = array[index + quarter]
+
 
         // spans 2
         if (currNum == firstNumOfNextQuartile) {
@@ -25,21 +28,11 @@ var findSpecialInteger = function (array) {
 
 }
 
-// [1, 1] [2, 2] [3, 3] [3, 3]
-// only 3 spans two quartiles
-
-// findSpecialInteger([1, 1, 2, 2, 3, 3, 3, 3]) // 3
-
-// findSpecialInteger([1,2,3,3]) // 3
-// findSpecialInteger([1,2,2,3]) // 2
-// findSpecialInteger([1,1,2,3]) // 1
-
-findSpecialInteger([1,2,2]) // 2
-findSpecialInteger([1,1,2]) // 1
+findSpecialInteger([1, 1, 2, 2, 3, 3, 3, 3]) // 3
 
 // findSpecialInteger([15]) // 15
 
-// findSpecialInteger([1,2,3,4,5,6,7,8,9,10,11,12,12,12,12]) // 12
+// findSpecialInteger([1,2,2]) // 2
+// findSpecialInteger([1,1,2]) // 1
 
-// findSpecialInteger([1,2,2,6,6,6,6,7,10]) // 6
-findSpecialInteger([1, 1]) // 1
+// findSpecialInteger([1,2,3,4,5,6,7,8,9,10,11,12,12,12,12]) // 12
