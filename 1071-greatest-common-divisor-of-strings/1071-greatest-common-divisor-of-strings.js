7e1096ad2c3875bfc33f67afbdcd8df7dde5f83f
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-08-12 14:00:15 -0400

    Time: 55 ms (77.32%), Space: 42 MB (81.56%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.js
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.js
@@ -1,8 +1,14 @@
 const gcd = (string1Length, string2Length) => {
 
+    string1Length
+    string2Length
+
+    // mod = 0 means a perfect fit, which is necessary for a GCD
+    // mod will always reach 0, otherwise it wouldve returned ""
+
     if (string2Length == 0) {
         return string1Length
-    } 
+    }
 
     else if (string2Length != 0) {
         let mod = string1Length % string2Length
@@ -26,10 +32,13 @@ var gcdOfStrings = function (string1, string2) {
     string1.length
     string2.length
 
-    let endLength = gcd(string1.length, string2.length)
-    return string1.slice(0, endLength)
-    
+    let gcdNum = gcd(string1.length, string2.length)
+    // return string1.slice(0, gcdNum)
+    return string2.slice(0, gcdNum)
 }
 
-console.log(gcdOfStrings("ABCABC", "ABC"), "ABC")
+// console.log(gcdOfStrings("ABCABC", "ABC"), "ABC")
+console.log(gcdOfStrings("ABABAB", "ABAB"), "AB", "GCD of 6 and 4 is 2, not 4.")
+
+// console.log(gcdOfStrings("ABCDABCD", "ABCD"), "ABCD")
 // console.log(gcdOfStrings("ASD", "XYZ"), "")
