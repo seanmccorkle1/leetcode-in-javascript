68ae9eb1c4b5cc2544f1d473cbf8393091c82f99
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-08-12 15:14:21 -0400

    Time: 56 ms (78.87%), Space: 42.9 MB (38.59%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.js
+++ 0063-unique-paths-ii/0063-unique-paths-ii.js
@@ -1,20 +1,85 @@
-var uniquePathsWithObstacles = function(OG) {
-    
-    if (OG[0][0]) {
-        return 0
-    }
-    
-    let m = OG.length, n = OG[0].length
-    const dp = Array.from({length: m}, el => new Uint32Array(n))
-    
-    dp[0][0] = 1
-    
-    for (let i = 0; i < m; i++)
+// only down and right are allowed
+var uniquePathsWithObstacles = function (matrix) {
 
-        for (let j = 0; j < n; j++)
+    if (matrix[0][0] == 1) {
+        return 0 // blocked
+    }
+
+    let continues = 0
+
+    let numOfRows = matrix.length // rows
+    let numOfColumns = matrix[0].length
+
+    const selfArray = new Array(numOfRows).fill("a").map(a => new Array(numOfColumns).fill(0))
+
+    selfArray[0][0] = 1
+    selfArray
+
+    for (let rowIndex = 0; rowIndex < numOfRows; rowIndex++) {
+
+        selfArray
+
+        for (let colIndex = 0; colIndex < numOfColumns; colIndex++) {
+
+            let currCell = matrix[rowIndex][colIndex]
+
+            const isFirstMove = rowIndex == 0 && colIndex == 0
+            const cellIsObstacle = currCell == 1
+
+            if (cellIsObstacle || isFirstMove) {
+                ++continues
+                continue // go right without adding
+            }
+
+            // otherwise its array[-1]
+
+            rowIndex
+            colIndex
+
+            if (rowIndex >= 1) {
+                let cellAbove = selfArray[rowIndex - 1][colIndex]
+                selfArray[rowIndex][colIndex] += cellAbove
+            }
+
+            if (colIndex >= 1) {
+                let leftCell = selfArray[rowIndex][colIndex - 1]
+                selfArray[rowIndex][colIndex] += leftCell
+            }
+
+            selfArray
+        }
         
-            if (OG[i][j] || (!i && !j)) continue
-            else dp[i][j] = (i ? dp[i-1][j] : 0) + (j ? dp[i][j-1] : 0)
-    
-        return dp[m-1][n-1]
+        rowIndex
+        selfArray
+    }
+
+    // [1, 1, 0],
+    // [0, 0, 0]
+
+    // [1, 1, 1],
+    // [0, 0, 0]
+
+    // [1, 1, 1],
+    // [1, 0, 0]
+
+    // [1, 1, 1],
+    // [1, 2, 0]
+
+    // [1, 1, 1],
+    // [1, 2, 3]
+
+    selfArray
+    return selfArray[numOfRows - 1][numOfColumns - 1]
 }
+
+uniquePathsWithObstacles([
+    [0, 0, 0],
+    [0, 0, 0],
+]) // 3
+
+
+// uniquePathsWithObstacles([
+//     [0, 0, 0],
+//     [0, 1, 0],
+//     [0, 0, 0]
+// ]) // 2
