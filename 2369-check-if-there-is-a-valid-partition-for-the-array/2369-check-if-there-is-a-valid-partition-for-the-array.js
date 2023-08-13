e91a7f8431530395ad9f52a62f8318a081b3af70
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-08-12 20:05:58 -0400

    Time: 84 ms (85.71%), Space: 55.2 MB (85.71%) - LeetHub

--- 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.js
+++ 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.js
@@ -6,11 +6,18 @@ var validPartition = a => {
     for (let i = 2; i <= n; i++) {
 	    // check dp[i-2] = true dp[i-3] = true for previous array can be partion
     
-        if (dp[i - 2] && a[i - 2] == a[i - 1]) dp[i] = true; // condition 1
+        if (dp[i - 2] && a[i - 2] == a[i - 1]) {
+            dp[i] = true
+        }
         
-        if (i - 3 >= 0 && dp[i - 3] && a[i - 1] == a[i - 2] && a[i - 2] == a[i - 3]) dp[i] = true; // condition 2
+        if (i - 3 >= 0 && dp[i - 3] && a[i - 1] == a[i - 2] && a[i - 2] == a[i - 3]) {
+            dp[i] = true
+        }
         
-        if (i - 3 >= 0 && dp[i - 3] && a[i - 1] - a[i - 2] == 1 && a[i - 2] - a[i - 3] == 1) dp[i] = true; // condition 3
+        if (i - 3 >= 0 && dp[i - 3] && a[i - 1] - a[i - 2] == 1 && a[i - 2] - a[i - 3] == 1) {
+            dp[i] = true
+        }
     }
-    return dp[n];
-};
+    
+    return dp[n]
+}
