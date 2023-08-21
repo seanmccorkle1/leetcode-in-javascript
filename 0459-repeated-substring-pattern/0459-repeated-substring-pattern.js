fce9740f58167796df9ac7a114f94ffe98f0a16e
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-08-21 16:47:52 -0400

    Time: 55 ms (87.38%), Space: 44.4 MB (88.93%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.js
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.js
@@ -1,9 +1,15 @@
-
-
 var repeatedSubstringPattern = function(string) {
     
     let doubleString = string.repeat(2)
     doubleString = doubleString.slice(1,-1)
     
+    string
+    doubleString
+    
     return doubleString.includes(string) ? true: false
 }
+
+console.log(repeatedSubstringPattern("leetleet"), true)
+
+// console.log(repeatedSubstringPattern("LeetaLeet"), true)
+// console.log(repeatedSubstringPattern("leetASD"), false)
