c719e4357d98f11f0cecc147acc140fe987b65a4
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-08-23 21:37:51 -0400

    Time: 41 ms (97.22%), Space: 41.5 MB (86.31%) - LeetHub

--- 0068-text-justification/0068-text-justification.js
+++ 0068-text-justification/0068-text-justification.js
@@ -1,41 +1,41 @@
-var fullJustify = function(words, maxWidth) {
+// The constraints of the input parameters are:
+// 1 <= words.length <= 300
+// 1 <= words[i].length <= 20
+// words[i] consists of only English letters and symbols.
+// 1 <= maxWidth <= 100
+// words[i].length <= maxWidth
 
-    const res = [];
+var fullJustify = function(words, maxWidth) {
     
-    let buf = [];
+    const res = []
+    
+    let buf = []
     let width = maxWidth
     
     // Greedy pack each line
     // when that fails add the line to the result with the added padding
     // and start a new line
-
     words.forEach(word => {
         // Check if the word fits in the current line
         // A word fits if theres enough room for the word and
         // a space between it and the word to the left
-    
         if (word.length <= (width - buf.length)) {
             buf.push(word);
             width -= word.length;
-        }
-
-        else {
-
+        } else {
             // The word did not fit on the line, send this line for padding
-            addWordToResult(res, buf.slice(), maxWidth)
+            addWordToResult(res, buf.slice(), maxWidth);
             
             // Start a new line with the current word
             buf = [word];
-
             // reset the current line width
             width = maxWidth - word.length;
         }
-    })
-
+    });
+    
     // This is the final lines processing
     // According the rules this should only be left justified
     // so add all padding to the right not between the words
-    
     if (buf.length) {
         let str = buf.join(' ');
         str += ' '.repeat(maxWidth - str.length);
@@ -43,14 +43,13 @@ var fullJustify = function(words, maxWidth) {
     }
     
     return res;
-}
+};
 
 // Max words are on each line now pad them with spaces
 
+//
 function addWordToResult(res, buf, maxWidth) {
-    
     // How many spaces are needed
-
     let spaces = maxWidth - buf.reduce((acc, cur) => cur.length + acc, 0);
     
     // If there is only one word on the line
@@ -72,18 +71,20 @@ function addWordToResult(res, buf, maxWidth) {
     
     // If there are spaces to distribute, distribute them
     // evenly looping back to the beginning of the buffer
-
-    while (spaces-- > 0) {
+    
+    while (spaces  > 0) {
         buf[index] += ' ';
-        
-        index = (index + 1) % end;
+        index = (index + 1) % end
+        spaces--
     }
     
     res.push(buf.join(' '))
 }
 
-fullJustify(["Science","is","what","we","understand","well","enough","to","explain","to","a","computer.","Art","is","everything","else","we","do"], 20)
-            
+// end of program
+fullJustify(["Science","is","what","we","understand","well","enough","to","explain","to","a","computer." , "Art","is","everything","else","we","do"], 20)
+
+/** Output is
 [
   "Science  is  what we",
   "understand      well",
@@ -91,4 +92,4 @@ fullJustify(["Science","is","what","we","understand","well","enough","to","expla
   "a  computer.  Art is",
   "everything  else  we",
   "do                  "
-]
+]*/
