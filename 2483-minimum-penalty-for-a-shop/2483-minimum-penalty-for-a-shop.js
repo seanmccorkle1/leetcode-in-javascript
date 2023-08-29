86ec62735a13c3954d7c7ebb18900fdc41163347
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-08-29 19:40:08 -0400

    Time: 63 ms (73.08%), Space: 44.6 MB (78.85%) - LeetHub

--- 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.js
+++ 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.js
@@ -1,12 +1,19 @@
 var bestClosingTime = function(customers) {
-    let max = 0;
-    let balance = 0;
+    
+    let max = 0
+    let balance = 0
     let answer = 0
-    for(var i = 0;i < customers.length;i++){
+    
+    for (var i = 0;i < customers.length;i++){
+        
         if(customers[i] === "Y"){
             balance++
         }
-        else balance--
+        
+        else if (customers[i] != "Y") {
+            balance--
+        }
+        
         if(balance > max){
             max = balance;
             answer = i + 1
