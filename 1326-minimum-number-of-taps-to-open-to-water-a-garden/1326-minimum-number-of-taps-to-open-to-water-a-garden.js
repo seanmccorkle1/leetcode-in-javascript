2e9d893bb5fe4a51d679b412be2732345e6b7a41
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-08-30 20:20:00 -0400

    Time: 65 ms (100.00%), Space: 43.8 MB (76.47%) - LeetHub

---
+++ 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.js
@@ -0,0 +1,31 @@
+var minTaps = function(n, ranges) {
+    
+    const dp = new Array(n + 1).fill(1e6);
+    dp[0] = 0
+    
+    for (let i = 0; i < ranges.length; i++) {
+        
+        let leftCover = Math.max(0, i - ranges[i])        
+        let rightCover = Math.min(n, i + ranges[i])
+        
+        let start = leftCover
+        
+        while (start <= rightCover){
+            dp[start] = Math.min(dp[leftCover] + 1, dp[start])
+            start++
+        }
+        
+        // for (let k = leftCover; k <= rightCover; k++) {
+            // dp[k] = Math.min(dp[k], 1 + dp[leftCover]);
+        // }
+    }
+    
+    // whole garden cant be watered
+    if (dp.at(-1) == 1e6) {
+        return -1
+    }
+    
+
+    return dp[n]
+}
+
