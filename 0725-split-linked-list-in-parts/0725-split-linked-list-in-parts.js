8ddc6fb1ec839b3ae9450493a7d41c6433ff2479
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-09-05 20:32:47 -0400

    Time: 55 ms (89.90%), Space: 44.6 MB (30.30%) - LeetHub

---
+++ 0725-split-linked-list-in-parts/0725-split-linked-list-in-parts.js
@@ -0,0 +1,56 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} k
+ * @return {ListNode[]}
+ */
+
+var splitListToParts = function(root, k) {
+
+    const res = new Array(k).fill(null); // will contain split linked list 
+    let n = 0; // total # of nodes in the link list
+    
+    let curr = root;
+    
+    // count the # of nodes
+    while (curr != null) {
+
+        curr = curr.next;
+
+        n++;
+    }
+    
+    const width = Math.floor(n / k); // initial width of each linked list parts
+    let rem = n % k; // remaining nodes
+    
+    let head = root;
+    let tail = root;
+    
+    for (let i = 0; head != null; i++) {
+        let m = width;
+        
+        if (rem > 0) {
+            m += 1;
+            rem--;
+        }
+        
+        let tail = head;
+        
+        for (let j = 1; j < m; j++) {
+            tail = tail.next;
+        }
+        
+        const next = tail.next;
+        tail.next = null;
+        res[i] = head;
+        head = next;
+    }
+    
+    return res;
+};
