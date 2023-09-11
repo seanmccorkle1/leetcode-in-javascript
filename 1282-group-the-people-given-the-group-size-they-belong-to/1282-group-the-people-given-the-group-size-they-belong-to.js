f05593351ff768ce5a60bdcf122fa785a5a0a493
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-09-10 21:39:36 -0400

    Time: 80 ms (65.59%), Space: 46.3 MB (74.73%) - LeetHub

---
+++ 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.js
@@ -0,0 +1,23 @@
+var groupThePeople = function(group) {
+
+    const result = []
+  const hash = {}
+    
+  for (let i = 0; i < group.length; i++) {
+    const num = group[i]
+    
+    if (hash[num]) {
+      hash[num].push(i)
+    } 
+      
+      else {
+          hash[num] = [i]
+    }
+      
+    if (hash[num].length === num) {
+      result.push(hash[num])
+      delete hash[num]
+    }
+  }
+  return result
+}
