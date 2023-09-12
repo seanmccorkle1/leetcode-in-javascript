9d0d99dd518ee35ae0d9d4d2f94dc5b03b188d7e
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-09-11 21:57:19 -0400

    Time: 75 ms (96.51%), Space: 46.5 MB (91.86%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.js
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.js
@@ -8,9 +8,9 @@ var minDeletions = function(s) {
         arr[index]++
     }
     
-    // arr.sort((a,b)=>b-a)
-    arr.sort((a,b) => a - b).reverse()
-    for(let i=1; i<26; i++){
+    arr.sort((a,b)=>b - a)
+    
+    for (let i=1; i<26; i++){
 
         while(arr[i] && arr[i] >= arr[i-1]){
             arr[i]--
