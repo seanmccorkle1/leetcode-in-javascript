a075781a7a4df1b2062bfe1899fe04e7a984db08
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-10-19 22:40:44 -0400

    Time: 16 ms (15.52%), Space: 14.1 MB (24.14%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2824-count-pairs-whose-sum-is-less-than-target/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2824-count-pairs-whose-sum-is-less-than-target.swift
@@ -0,0 +1,18 @@
+class Solution {
+    func countPairs(_ nums: [Int], _ target: Int) -> Int {
+        var nums = nums.sorted() // sort the vector nums
+        var count = 0 // variable to store the count
+        var left = 0 // variable to store the left
+        var right = nums.count-1 // variable to store the right
+        while(left < right){ // loop until left is less than right
+            if(nums[left] + nums[right] < target){ // if nums[left] + nums[right] is less than target
+                count += right-left // update the count
+                left += 1 // increment the left
+            }
+            else{ // else
+                right -= 1 // decrement the right
+            }
+        }
+        return count // return the count
+    }
+}
