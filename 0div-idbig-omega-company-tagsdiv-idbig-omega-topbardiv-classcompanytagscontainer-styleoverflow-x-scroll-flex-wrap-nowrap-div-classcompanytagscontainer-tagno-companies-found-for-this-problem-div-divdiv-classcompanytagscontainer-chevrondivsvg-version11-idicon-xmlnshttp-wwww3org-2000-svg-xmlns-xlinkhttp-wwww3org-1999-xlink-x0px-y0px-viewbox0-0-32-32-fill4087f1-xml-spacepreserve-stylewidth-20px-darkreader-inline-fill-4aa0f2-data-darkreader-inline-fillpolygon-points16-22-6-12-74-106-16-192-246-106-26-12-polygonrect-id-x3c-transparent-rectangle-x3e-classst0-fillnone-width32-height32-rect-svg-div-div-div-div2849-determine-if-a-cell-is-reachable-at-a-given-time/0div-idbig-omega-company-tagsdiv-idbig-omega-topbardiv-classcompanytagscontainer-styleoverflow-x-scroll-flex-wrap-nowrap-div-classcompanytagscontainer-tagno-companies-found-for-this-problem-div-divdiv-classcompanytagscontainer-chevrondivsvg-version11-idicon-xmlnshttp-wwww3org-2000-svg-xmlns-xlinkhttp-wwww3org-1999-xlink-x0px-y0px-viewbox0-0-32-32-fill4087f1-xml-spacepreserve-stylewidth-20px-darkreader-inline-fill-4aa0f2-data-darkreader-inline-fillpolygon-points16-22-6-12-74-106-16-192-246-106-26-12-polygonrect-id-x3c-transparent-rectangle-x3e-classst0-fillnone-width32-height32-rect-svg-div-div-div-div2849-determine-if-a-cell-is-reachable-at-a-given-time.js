f7e49e1823c02dba18a2f552578035e8d4a29970
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-11-07 19:42:53 -0500

    Time: 60 ms (37.56%), Space: 43.7 MB (32.13%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2849-determine-if-a-cell-is-reachable-at-a-given-time/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2849-determine-if-a-cell-is-reachable-at-a-given-time.js
@@ -0,0 +1,16 @@
+var isReachableAtTime = function(sx, sy, fx, fy, t) {
+    // Calculate the Manhattan distance between the start and target cells
+    let xDistance = Math.abs(sx - fx);
+    let yDistance = Math.abs(sy - fy);
+    
+    // Calculate the minimum distance required to reach the target
+    let minDist = Math.min(xDistance, yDistance) + Math.abs(yDistance - xDistance);
+    
+    // If the minimum distance is 0, it's not possible to reach within 1 second
+    if (minDist === 0) {
+        return t !== 1;
+    }
+    
+    // Check if the given time is greater than or equal to the minimum distance
+    return t >= minDist;
+};
