67dc439e53bee7b6f7531399556e0a52fd2da2dc
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-11-20 19:54:42 -0500

    Time: 96 ms (96.00%), Space: 51.3 MB (92.00%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-darkreader-inline-bgcolor-rgba0-8-26-005-data-darkreader-inline-bgcolordivuber-divdiv-classcompanytagscontainer-tagoccurence3-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1814-count-nice-pairs-in-an-array/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-darkreader-inline-bgcolor-rgba0-8-26-005-data-darkreader-inline-bgcolordivuber-divdiv-classcompanytagscontainer-tagoccurence3-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1814-count-nice-pairs-in-an-array.js
@@ -0,0 +1,33 @@
+function countNicePairs(nums) {
+    const counts = new Map();
+    
+    for (const num of nums) {
+        const rev = reverse(num);
+        const diff = rev - num;
+        
+        const count = counts.get(diff);
+        
+        counts.set(diff, count ? count + 1 : 1);
+    }
+    
+    let sum = 0;
+    
+    for (const [key, value] of counts) {
+        sum += (value - 1) * value / 2;
+    }
+    
+    return sum % 1000000007;
+}
+
+function reverse(num) {
+    let n = 0;
+    
+    while (num > 0) {
+        const modulo = num % 10;
+        
+        n = n * 10 + modulo;
+        num = (num - modulo) / 10;
+    }
+    
+    return n;
+}
