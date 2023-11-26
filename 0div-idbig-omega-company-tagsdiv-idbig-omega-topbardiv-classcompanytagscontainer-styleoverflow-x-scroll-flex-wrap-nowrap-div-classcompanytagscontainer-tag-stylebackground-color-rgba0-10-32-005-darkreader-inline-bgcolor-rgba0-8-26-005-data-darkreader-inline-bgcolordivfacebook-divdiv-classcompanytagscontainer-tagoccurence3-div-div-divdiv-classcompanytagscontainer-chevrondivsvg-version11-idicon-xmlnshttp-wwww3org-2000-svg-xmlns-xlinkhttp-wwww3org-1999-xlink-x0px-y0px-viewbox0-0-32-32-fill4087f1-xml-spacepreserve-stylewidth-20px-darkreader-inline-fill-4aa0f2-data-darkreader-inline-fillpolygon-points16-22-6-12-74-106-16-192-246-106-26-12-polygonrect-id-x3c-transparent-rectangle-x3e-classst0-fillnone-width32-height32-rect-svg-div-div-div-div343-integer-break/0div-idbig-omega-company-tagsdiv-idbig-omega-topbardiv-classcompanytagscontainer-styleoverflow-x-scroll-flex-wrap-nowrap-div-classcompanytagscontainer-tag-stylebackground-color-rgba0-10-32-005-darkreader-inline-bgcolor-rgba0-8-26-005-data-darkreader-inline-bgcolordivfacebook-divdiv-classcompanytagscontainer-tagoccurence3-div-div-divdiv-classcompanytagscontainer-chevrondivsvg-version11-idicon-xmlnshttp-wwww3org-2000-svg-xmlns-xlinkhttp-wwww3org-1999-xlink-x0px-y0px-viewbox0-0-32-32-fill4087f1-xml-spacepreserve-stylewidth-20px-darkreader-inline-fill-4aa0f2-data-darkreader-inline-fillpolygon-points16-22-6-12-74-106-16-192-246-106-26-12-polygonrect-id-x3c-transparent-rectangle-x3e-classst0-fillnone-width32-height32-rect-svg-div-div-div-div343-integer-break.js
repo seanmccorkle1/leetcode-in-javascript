40568ca4de81b2ec059b25f0fdc3078d4db3d66f
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-11-26 11:29:28 -0500

    Time: 46 ms (85.38%), Space: 41.5 MB (87.16%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-darkreader-inline-bgcolor-rgba0-8-26-005-data-darkreader-inline-bgcolordivfacebook-divdiv-classcompanytagscontainer-tagoccurence3-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div343-integer-break/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-darkreader-inline-bgcolor-rgba0-8-26-005-data-darkreader-inline-bgcolordivfacebook-divdiv-classcompanytagscontainer-tagoccurence3-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div343-integer-break.js
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-darkreader-inline-bgcolor-rgba0-8-26-005-data-darkreader-inline-bgcolordivfacebook-divdiv-classcompanytagscontainer-tagoccurence3-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div343-integer-break/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-darkreader-inline-bgcolor-rgba0-8-26-005-data-darkreader-inline-bgcolordivfacebook-divdiv-classcompanytagscontainer-tagoccurence3-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div343-integer-break.js
@@ -1,62 +1,37 @@
-// Why not 4s?
-//  Anytime we have a 4, we can always split it into two 2s,
-// which will ultimately give an  equal product.
-
 var integerBreak = function (num) {
 
-    num
-
     // 2 is the lowest in "constraints"
-
     if (num == 2) {
         return 1 // 1x1 = 1
     }
 
+    // there must be at least 2 numbers, X and Y,
+    // you cant break it into one number
+
     if (num === 3) {
-        return 2 //  2x1 = 2
+        return 2
     }
 
-    num
+    let countOf3s = Math.floor(num/3)
+    let remainder =  num % 3
 
-    // math floor over ~~ for edge conditions
-    let countOf3s = Math.floor(num / 3)
+    if (remainder == 0){
+        return Math.pow(3,countOf3s)
 
-    num
-    let remainder = num % 3
-
-    countOf3s
-
-    // perfect fit, no -1s
-    if (remainder == 0) {
-        return Math.pow(3, countOf3s)
     }
-
-    else if (remainder === 1) {
-
-        countOf3s
-        countOf3s - 1
-
-        // order matters for Math.pow()
-        const  pow = Math.pow(3, (countOf3s - 1))
-
-        // avoid useless 27x1
-        return pow * 2 * 2
+    if (remainder == 1){
+        return Math.pow(3, countOf3s - 1)  * 4
     }
-
-    else if (remainder == 2) {
-
-        // pow redefined in its own bracket
-        let pow = Math.pow(3, countOf3s)
-
-        pow
-        return pow * 2
+    if (remainder == 2){
+        return Math.pow(3, countOf3s) * 2
     }
 }
 
-// integerBreak(9)  // 3 x 3 x3  = 27
-integerBreak(10) // 36  | 3 + 3 + 4 = 10 |  (3 × 3 × 4) =  36 or (3 x 3 x2x2) = 36
+// console.log(integerBreak(9), 27, " 3 x 3 x3 = 27, this also adds up to 9 | 3+3+3=9")
 
-// integerBreak(11) // 3 x 3 x 3 x 2 = 54
+console.log(integerBreak(10), 36, "10 is (3 + 3 + 4)| This sequence of integers, {3, 3, 4}, also gives the highest product, 36 | (3 × 3 × 4) = 36")
+
+console.log(integerBreak(11), 54, "(3 x 3 x 3 x 2) = (27 x 2) = 54")
 
 // integerBreak(2) // 1 + 1 = 2 | 1 × 1 = 1
 // integerBreak(3) // 2 x1 = 2 and 2+1 = 3
