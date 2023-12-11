b451c82e26666904b66e220aa994e112e8db9918
Sean Mccorkle <90057127+seanmccorkle1@users.noreply.github.com>
2023-12-10 19:14:07 -0500

    Time: 55 ms (66.49%), Space: 42.3 MB (85.11%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-darkreader-inline-bgcolor-rgba0-8-26-005-data-darkreader-inline-bgcolordivfacebook-divdiv-classcompanytagscontainer-tagoccurence5-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-darkreader-inline-bgcolor-rgba0-8-26-005-data-darkreader-inline-bgcolordivgoogle-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1287-element-appearing-more-than-25-in-sorted-array/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-darkreader-inline-bgcolor-rgba0-8-26-005-data-darkreader-inline-bgcolordivfacebook-divdiv-classcompanytagscontainer-tagoccurence5-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-darkreader-inline-bgcolor-rgba0-8-26-005-data-darkreader-inline-bgcolordivgoogle-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-darkreader-inline-fill-4aa0f2-data-darkreader-inline-fillpolygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1287-element-appearing-more-than-25-in-sorted-array.js
@@ -0,0 +1,38 @@
+var findSpecialInteger = function (array) {
+
+    if (array.length == 1){
+        return array[0]
+    }
+    
+    if (array.length <= 3){
+        return array[1]
+    }
+
+    const quarter = ~~(array.length / 4)
+
+    array.length
+    array
+
+    for (let index = 0; index < array.length; index++) {
+
+        let currNum = array[index]
+        let firstNumOfNextQuartile = array[index + quarter]
+
+
+        // spans 2
+        if (currNum == firstNumOfNextQuartile) {
+            return currNum
+        }
+
+    }
+
+}
+
+findSpecialInteger([1, 1, 2, 2, 3, 3, 3, 3]) // 3
+
+// findSpecialInteger([15]) // 15
+
+// findSpecialInteger([1,2,2]) // 2
+// findSpecialInteger([1,1,2]) // 1
+
+// findSpecialInteger([1,2,3,4,5,6,7,8,9,10,11,12,12,12,12]) // 12
